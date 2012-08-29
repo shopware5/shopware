7afe537ef1274d4a1a79e4c60f4b63983ba99e09
Heiner Lohaus <info@shopware.de>
2012-08-29 14:14:27 +0000

    SW-3619 - Add mysql version check

--- install/assets/php/Shopware_Install_Database.php
+++ install/assets/php/Shopware_Install_Database.php
@@ -95,8 +95,8 @@ class Shopware_Install_Database
         try {
             $sql = "SELECT VERSION()";
             $result = $this->database->query($sql)->fetchColumn(0);
-            if(version_compare($result, '4.1.0', '<')) {
-                $this->setError("Database-Error!: Your database server is running MySQL $result, but Shopware 4 requires at least MySQL 4.1.0.<br/>");
+            if(version_compare($result, '5.1.0', '<')) {
+                $this->setError("Database-Error!: Your database server is running MySQL $result, but Shopware 4 requires at least MySQL 5.1.0.<br/>");
                 return false;
             }
         } catch(PDOException $e) { }
