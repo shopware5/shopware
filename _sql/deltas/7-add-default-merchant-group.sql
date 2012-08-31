b4d262a10da008fbec0a3ab3bc2f5734ffe6a6c2
Heiner Lohaus <info@shopware.de>
2012-08-31 09:43:15 +0000

    SW-3652 - Add default merchant group

---
+++ _sql/deltas/7-add-default-merchant-group.sql
@@ -0,0 +1,9 @@
+-- //
+
+INSERT IGNORE INTO `s_core_customergroups` (`id`, `groupkey`, `description`, `tax`, `taxinput`, `mode`, `discount`, `minimumorder`, `minimumordersurcharge`) VALUES
+(2, 'H', 'Händler', 1, 0, 0, 0, 0, 0);
+
+-- //@UNDO
+
+
+-- //
