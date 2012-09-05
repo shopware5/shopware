22db38af4c01467af9905c15a0685aef7513c21e
Stefan Hamann <info@shopware.de>
2012-09-05 05:50:05 +0000

    Remove extjs-page-analyzer

--- extjs-page-analyzer/models/LayoutIdSummaryData.js
+++
@@ -1,9 +0,0 @@
-Ext.define('PageAnalyzer.models.LayoutIdSummaryData', {
-    extend: 'Ext.data.Model',
-    fields: [
-        { name: 'type', type: 'string' },
-        { name: 'duration', type: 'float', defaultValue: 0.0 },
-        { name: 'count', type: 'int', defaultValue: 0 }
-    ]
-
-});
