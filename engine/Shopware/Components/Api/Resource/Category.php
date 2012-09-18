a635929af227aa338fc152c4e5b8e6ce2fcc6d17
Daniel Nögel <info@shopware.de>
2012-09-18 12:21:59 +0000

    SW-3925 - Adds Category REST Resource

---
+++ engine/Shopware/Components/Api/Resource/Category.php
@@ -0,0 +1,217 @@
+<?php
+/**
+ * Shopware 4.0
+ * Copyright © 2012 shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+namespace Shopware\Components\Api\Resource;
+
+use Shopware\Components\Api\Exception as ApiException;
+use Shopware\Models\Category\Category as CategoryModel;
+
+
+/**
+ * Category API Resource
+ */
+class Category extends Resource
+{
+    /**
+     * @return \Shopware\Models\Category\Repository
+     */
+    public function getRepository()
+    {
+        return $this->getManager()->getRepository('Shopware\Models\Category\Category');
+    }
+
+
+    /**
+     * @param int $id
+     * @return array|\Shopware\Models\Category\Category
+     * @throws \Shopware\Components\Api\Exception\ParameterMissingException
+     * @throws \Shopware\Components\Api\Exception\NotFoundException
+     */
+    public function getOne($id)
+    {
+
+        $this->checkPrivilege('read');
+
+        if (empty($id)) {
+            throw new ApiException\ParameterMissingException();
+        }
+
+        $filters = array(array('property' => 'id','expression' => '=','value' => $id));
+        $query = $this->getRepository()->getListQuery($filters, array(), 1);
+
+        /** @var $category \Shopware\Models\Category\Category */
+        $category = $query->getOneOrNullResult($this->getResultMode());
+
+
+        if (!$category) {
+            throw new ApiException\NotFoundException("Category by id $id not found");
+        }
+
+        return $category;
+    }
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @param array $criteria
+     * @param array $orderBy
+     * @return array
+     */
+    public function getList($offset = 0, $limit = 25, array $criteria = array(), array $orderBy = array())
+    {
+        $this->checkPrivilege('read');
+
+        $query = $this->getRepository()->getListQuery($criteria, $orderBy, $limit, $offset);
+        $query->setHydrationMode($this->resultMode);
+
+        $paginator = new \Doctrine\ORM\Tools\Pagination\Paginator($query);
+
+        //returns the total count of the query
+        $totalResult = $paginator->count();
+
+        //returns the category data
+        $categories = $paginator->getIterator()->getArrayCopy();
+
+        return array('data' => $categories, 'total' => $totalResult);
+    }
+
+    /**
+     * @param array $params
+     * @return \Shopware\Models\Category\Category
+     * @throws \Shopware\Components\Api\Exception\ValidationException
+     * @throws \Exception
+     */
+    public function create(array $params)
+    {
+        $this->checkPrivilege('create');
+
+        $params = $this->prepareCategoryData($params);
+
+        $category = new \Shopware\Models\Category\Category();
+        $category->fromArray($params);
+
+        $violations = $this->getManager()->validate($category);
+        if ($violations->count() > 0) {
+            throw new ApiException\ValidationException($violations);
+        }
+
+        $this->getManager()->persist($category);
+        $this->flush();
+
+        return $category;
+    }
+
+    /**
+     * @param int $id
+     * @param array $params
+     * @return \Shopware\Models\Category\Category
+     * @throws \Shopware\Components\Api\Exception\ValidationException
+     * @throws \Shopware\Components\Api\Exception\NotFoundException
+     * @throws \Shopware\Components\Api\Exception\ParameterMissingException
+     * @throws \Shopware\Components\Api\Exception\CustomValidationException
+     */
+    public function update($id, array $params)
+    {
+        $this->checkPrivilege('update');
+
+        if (empty($id)) {
+            throw new ApiException\ParameterMissingException();
+        }
+
+        /** @var $category \Shopware\Models\Category\Category */
+        $category = $this->getRepository()->find($id);
+
+        if (!$category) {
+            throw new ApiException\NotFoundException("Category by id $id not found");
+        }
+
+        $params = $this->prepareCategoryData($params, $category);
+        $category->fromArray($params);
+
+        $violations = $this->getManager()->validate($category);
+        if ($violations->count() > 0) {
+            throw new ApiException\ValidationException($violations);
+        }
+
+        $this->flush();
+
+        return $category;
+    }
+
+    /**
+     * @param int $id
+     * @return \Shopware\Models\Category\Category
+     * @throws \Shopware\Components\Api\Exception\ParameterMissingException
+     * @throws \Shopware\Components\Api\Exception\NotFoundException
+     */
+    public function delete($id)
+    {
+        $this->checkPrivilege('delete');
+
+        if (empty($id)) {
+            throw new ApiException\ParameterMissingException();
+        }
+
+        /** @var $category \Shopware\Models\Category\Category */
+        $category = $this->getRepository()->find($id);
+
+        if (!$category) {
+            throw new ApiException\NotFoundException("Category by id $id not found");
+        }
+
+        $this->getManager()->remove($category);
+        $this->flush();
+
+        return $category;
+    }
+
+    private function prepareCategoryData($params, $category = null)
+    {
+        if(!isset($params['name'])) {
+            throw new ApiException\CustomValidationException("A name is required");
+        }
+
+        if (!empty($params['parent'])) {
+            $params['parent'] = Shopware()->Models()->getRepository('Shopware\Models\Category\Category')->find($params['parent']);
+            if (!$params['parent']) {
+                throw new ApiException\CustomValidationException(sprintf("Parent by id %s not found", $params['parent']));
+            }
+        } else {
+            unset($params['parent']);
+        }
+
+        if(!empty($params['attribute'])) {
+            foreach($params['attribute'] as $key=>$value) {
+                if(is_numeric($key)) {
+                    $params['attribute']['attribute'.$key] = $value;
+                    unset($params[$key]);
+                }
+            }
+        }
+
+
+        return $params;
+    }
+
+}
---
+++ engine/Shopware/Controllers/Api/Categories.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Shopware 4.0
+ * Copyright © 2012 shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ *
+ * @category   Shopware
+ * @package    Shopware_Controllers
+ * @subpackage Api
+ * @copyright  Copyright (c) 2012, shopware AG (http://www.shopware.de)
+ * @version    $Id$
+ * @author     Benjamin Cremer
+ */
+
+class Shopware_Controllers_Api_Categories extends Shopware_Controllers_Api_Rest
+{
+    /**
+     * @var Shopware\Components\Api\Resource\Category
+     */
+    protected $resource = null;
+
+    public function init()
+    {
+        $this->resource = \Shopware\Components\Api\Manager::getResource('category');
+    }
+
+    /**
+     * Get list of categories
+     *
+     * GET /api/categories/
+     */
+    public function indexAction()
+    {
+        $limit  = $this->Request()->getParam('limit', 1000);
+        $offset = $this->Request()->getParam('start', 0);
+        $sort   = $this->Request()->getParam('sort', array());
+        $filter = $this->Request()->getParam('filter', array());
+
+        $result = $this->resource->getList($offset, $limit, $filter, $sort);
+
+        $this->View()->assign($result);
+        $this->View()->assign('success', true);
+    }
+
+    /**
+     * Get one category
+     *
+     * GET /api/articles/{id}
+     */
+    public function getAction()
+    {
+        $id = $this->Request()->getParam('id');
+
+        $category = $this->resource->getOne($id);
+
+        $this->View()->assign('data', $category);
+        $this->View()->assign('success', true);
+    }
+
+    /**
+     * Create new category
+     *
+     * POST /api/category
+     */
+    public function postAction()
+    {
+        $category = $this->resource->create($this->Request()->getPost());
+
+        $location = $this->apiBaseUrl . 'categories/' . $category->getId();
+        $data = array(
+            'id'       => $category->getId(),
+            'location' => $location
+        );
+
+        $this->View()->assign(array('success' => true, 'data' => $data));
+        $this->Response()->setHeader('Location', $location);
+    }
+
+    /**
+     * Update category
+     *
+     * PUT /api/categories/{id}
+     */
+    public function putAction()
+    {
+        $id = $this->Request()->getParam('id');
+        $params = $this->Request()->getPost();
+
+        $category = $this->resource->update($id, $params);
+
+        $location = $this->apiBaseUrl . 'categories/' . $category->getId();
+        $data = array(
+            'id'       => $category->getId(),
+            'location' => $location
+        );
+
+        $this->View()->assign(array('success' => true, 'data' => $data));
+        $this->Response()->setHeader('Location', $location);
+    }
+
+    /**
+     * Delete article
+     *
+     * DELETE /api/articles/{id}
+     */
+    public function deleteAction()
+    {
+        $id = $this->Request()->getParam('id');
+
+        $this->resource->delete($id);
+
+        $this->View()->assign(array('success' => true));
+    }
+}
