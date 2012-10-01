920a275a7cd57342bf9514d2934804f98d9b9a03
Heiner Lohaus <info@shopware.de>
2012-10-01 09:02:12 +0000

    SW904 - Fix update shop delta

--- update/deltas/5-shop.sql
+++ update/deltas/5-shop.sql
@@ -1,16 +1,20 @@
 INSERT IGNORE INTO `s_core_templates` (`template`, `name`, `description`, `author`, `license`, `version`)
 SELECT
-  template,
-  CONCAT(UCASE(SUBSTRING(template, 1, 1)),LCASE(SUBSTRING(template, 2))) as name,
+  SUBSTRING_INDEX(template, '/', 2) as template,
+  CONCAT(UCASE(SUBSTRING(template, 11, 1)),LCASE(SUBSTRING(template, 12))) as name,
   NULL, 'shopware AG', 'New BSD', 1
 FROM backup_s_core_multilanguage
 GROUP BY template;
 
 TRUNCATE TABLE `s_core_shops`;
-REPLACE INTO `s_core_shops` (`id`, `main_id`, `name`, `host`, `hosts`, `template_id`, `document_template_id`, `category_id`, `locale_id`, `currency_id`, `customer_group_id`, `default`, `fallback_id`)
+REPLACE INTO `s_core_shops` (`id`, `main_id`, `name`, `host`, `hosts`, `template_id`, `document_template_id`, `category_id`, `locale_id`, `currency_id`, `customer_group_id`, `default`, `fallback_id`, `active`)
 SELECT
 	id,
-	(SELECT id FROM backup_s_core_multilanguage WHERE CONCAT('|', switchLanguages, '|') LIKE CONCAT('%|', m.id, '|%')) as main_id,
+	(
+	  SELECT IF(id=m.id, NULL, id) as id FROM backup_s_core_multilanguage
+	  WHERE CONCAT('|', switchLanguages, '|') LIKE CONCAT('%|', m.id, '|%')
+	  ORDER BY `default` DESC, id LIMIT 1
+	) as main_id,
 	name,
 	IF(`domainaliase`='', NULL, TRIM(SUBSTRING_INDEX(`domainaliase`, '\n', 1))) as host,
 	IF(`domainaliase`='', NULL, domainaliase) as hosts,
@@ -20,8 +24,9 @@ SELECT
 	locale as `locale_id`,
 	defaultcurrency as `currency_id`,
 	(SELECT id FROM s_core_customergroups WHERE groupkey=defaultcustomergroup) as `customer_group_id`,
-	`default`,
-    IF(fallback=0, NULL, fallback) as `fallback_id`
+	m.default,
+    (SELECT id FROM backup_s_core_multilanguage WHERE isocode=m.fallback) as `fallback_id`,
+    1 as active
 FROM backup_s_core_multilanguage m;
 UPDATE s_core_shops SET base_path = NULL, secure_base_path = NULL;
 UPDATE s_core_shops SET host = NULL, hosts = NULL WHERE main_id IS NOT NULL;
@@ -57,3 +62,32 @@ UPDATE `s_core_shops` SET `host` = TRIM(@value) WHERE `default`=1;
 SET @value = (SELECT REPLACE(`value`, @value, '') FROM `backup_s_core_config` WHERE `name` LIKE 'sBASEPATH');
 UPDATE `s_core_shops` SET `base_path` = TRIM(@value) WHERE `base_path` IS NULL AND `main_id` IS NULL;
 UPDATE `s_core_shops` SET `base_path` = NULL WHERE `base_path` = '';
+SET @value = (SELECT `value` FROM `backup_s_core_config` WHERE `name` LIKE 'sUSESSL');
+UPDATE `s_core_shops` SET `secure` = @value WHERE `default` = 1;
+
+
+INSERT IGNORE INTO `s_core_multilanguage` (
+  `id`, `isocode`, `locale`, `parentID`, `skipbackend`,
+  `name`, `defaultcustomergroup`, `template`, `doc_template`,
+  `domainaliase`,
+  `switchCurrencies`, `switchLanguages`,
+  `defaultcurrency`, `default`,
+  `fallback`
+)
+SELECT
+  s.id, s.id as isocode, s.locale_id, s.currency_id, s.default as skipbackend, s.name,
+  (SELECT groupkey FROM s_core_customergroups WHERE id=s.customer_group_id) as defaultcustomergroup,
+  (SELECT CONCAT('templates/', template) FROM s_core_templates WHERE id=m.template_id) as template,
+  (SELECT CONCAT('templates/', template) FROM s_core_templates WHERE id=m.document_template_id) as doc_template,
+  CONCAT(s.host, '\n', s.hosts) as hosts,
+  GROUP_CONCAT(d.currency_id SEPARATOR '|') as switchCurrencies,
+  (SELECT GROUP_CONCAT(id SEPARATOR '|') FROM s_core_shops WHERE id=m.id OR main_id=m.id)  as switchLanguages,
+  s.currency_id, s.default, s.fallback_id
+FROM `s_core_shops` s
+LEFT JOIN `s_core_shops` m
+ON m.id=s.main_id
+OR (s.main_id IS NULL AND m.id=s.id)
+LEFT JOIN `s_core_shop_currencies` d
+ON d.shop_id=m.id
+WHERE s.active=1
+GROUP BY s.id;
