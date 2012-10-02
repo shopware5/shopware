7a83dc33f3b8d230b78cdd41efbc9e6a08c00e45
Heiner Lohaus <info@shopware.de>
2012-10-02 09:02:11 +0000

    SW904 - Fix update blog entries

--- update/deltas/13-blog.sql
+++ update/deltas/13-blog.sql
@@ -2,7 +2,7 @@ INSERT IGNORE INTO `s_blog` (`id`, `title`, `author_id`, `active`, `short_descri
 SELECT a.id, name, NULL, active, description, description_long, 0, releasedate, ac.categoryID, template, NULL, NULL
 FROM backup_s_articles a, s_articles_categories ac
 WHERE a.mode=1
-AND ac.articleID = a.id
+AND ac.articleID = a.id;
 
 DELETE a, d, at, ac FROM s_articles a
 LEFT JOIN s_articles_details d
@@ -11,4 +11,4 @@ LEFT JOIN s_articles_attributes at
 ON at.articledetailsID = d.id
 LEFT JOIN s_articles_categories ac
 ON ac.articleID = a.id
-WHERE a.mode != 0
+WHERE a.mode != 0;
