f224838401fe4e8f73ea8ef85512a6d31f2b6971
Oliver Denter <info@shopware.de>
2012-10-23 12:17:17 +0000

    SW-4170 - Remove try catch block in the RouteStartUp event listener function of the default showpare router plugin

--- engine/Shopware/Plugins/Default/Core/Router/Bootstrap.php
+++ engine/Shopware/Plugins/Default/Core/Router/Bootstrap.php
@@ -107,18 +107,14 @@ class Shopware_Plugins_Core_Router_Bootstrap extends Shopware_Components_Plugin_
             return;
         }
 
-        try {
-            /** @var $repository Shopware\Models\Shop\Repository */
-            $repository = Shopware()->Models()->getRepository('Shopware\Models\Shop\Shop');
-            if (($shop = $request->getCookie('shop')) !== null) {
-                $shop = $repository->getActiveById($shop);
-            } if($shop === null) {
-                $shop = $repository->getActiveByRequest($request);
-            } if($shop === null) {
-                $shop = $repository->getActiveDefault();
-            }
-        } catch (Exception $e) {
-            return;
+        /** @var $repository Shopware\Models\Shop\Repository */
+        $repository = Shopware()->Models()->getRepository('Shopware\Models\Shop\Shop');
+        if (($shop = $request->getCookie('shop')) !== null) {
+            $shop = $repository->getActiveById($shop);
+        } if($shop === null) {
+            $shop = $repository->getActiveByRequest($request);
+        } if($shop === null) {
+            $shop = $repository->getActiveDefault();
         }
 
         if (!$shop->getHost()) {
