eadc97075b59217157ea1c4db247f46fd852c70a
Daniel Nögel <info@shopware.de>
2012-11-12 14:03:15 +0000

    SW-4396 - Replaces non-existing shopID for google product export

---
+++ _sql/deltas/2-fix-google-product-export.sql
@@ -0,0 +1,7 @@
+-- Set multishopID to 1 if the default ID does not exist //
+
+UPDATE `s_export` SET `multishopID`=1 WHERE `multishopID` NOT IN (SELECT `id` FROM `s_core_shops`) AND `name`="Google Produktsuche"
+
+-- //@UNDO
+
+-- //
