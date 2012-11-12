b4c846a0a8fbc8eec02cb0a04c39c297cd99499c
Daniel Nögel <info@shopware.de>
2012-11-12 14:14:57 +0000

    NTR - Fixes sql delta

--- _sql/deltas/2-fix-google-product-export.sql
+++ _sql/deltas/2-fix-google-product-export.sql
@@ -1,6 +1,6 @@
 -- Set multishopID to 1 if the default ID does not exist //
 
-UPDATE `s_export` SET `multishopID`=1 WHERE `multishopID` NOT IN (SELECT `id` FROM `s_core_shops`) AND `name`="Google Produktsuche"
+UPDATE `s_export` SET `multishopID`=1 WHERE `multishopID` NOT IN (SELECT `id` FROM `s_core_shops`) AND `name`='Google Produktsuche';
 
 -- //@UNDO
 
