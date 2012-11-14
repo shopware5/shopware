b9d88ac3633ea53df220f89c60698149fd33e7ff
Daniel Nögel <info@shopware.de>
2012-11-14 10:41:55 +0000

    SW-4418 - Adds variant's weight to configurator merge list

--- engine/core/class/sConfigurator.php
+++ engine/core/class/sConfigurator.php
@@ -376,6 +376,7 @@ class sConfigurator
         $articleData["suppliernumber"] = empty($selected['suppliernumber']) ? $articleData['suppliernumber'] : $selected['suppliernumber'];
         $articleData["stockmin"] = empty($selected['stockmin']) ? $articleData['stockmin'] : $selected['stockmin'];
         $articleData["stockmin"] = empty($selected['stockmin']) ? $articleData['stockmin'] : $selected['stockmin'];
+        $articleData["weight"] = empty($selected['weight']) ? $articleData['weight'] : $selected['weight'];
         $articleData["width"] = empty($selected['width']) ? $articleData['width'] : $selected['width'];
         $articleData["length"] = empty($selected['length']) ? $articleData['length'] : $selected['length'];
         $articleData["height"] = empty($selected['height']) ? $articleData['height'] : $selected['height'];
