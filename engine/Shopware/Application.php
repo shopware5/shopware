722896e20e8be2fe2f23df135559e27e753ddda6
Marcel Schmäing <info@shopware.de>
2012-11-14 16:21:44 +0000

    #NTR  update version and revision for 4.0.5

--- engine/Shopware/Application.php
+++ engine/Shopware/Application.php
@@ -37,9 +37,9 @@
  */
 class Shopware extends Enlight_Application
 {
-    const VERSION = '4.0.4';
+    const VERSION = '4.0.5';
     const VERSION_TEXT = '';
-    const REVISION = '6174';
+    const REVISION = '6615';
 
     protected $app     = 'Shopware';
     protected $appPath = 'engine/Shopware/';
