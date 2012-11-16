37ecaf5c740c81f59a933cef4153717daf72fbe8
Heiner Lohaus <info@shopware.de>
2012-11-16 12:28:51 +0000

    SW-4272 Improve backend search

--- engine/Shopware/Controllers/Backend/Search.php
+++ engine/Shopware/Controllers/Backend/Search.php
@@ -40,162 +40,150 @@
 class Shopware_Controllers_Backend_Search extends Shopware_Controllers_Backend_ExtJs
 {
 
-	/** @var int - The limit for all SQL queries */
-	public $searchLimit = 5;
+    /** @var int - The limit for all SQL queries */
+    public $searchLimit = 5;
 
-	/**
-	 * Sanitizes the passed term and queries the different areas of the search
-	 * @return mixed
-	 */
-	public function indexAction() {
-		if(!$this->Request()->isPost()) {
-			return;
-		}
+    /**
+     * Sanitizes the passed term and queries the different areas of the search
+     * @return mixed
+     */
+    public function indexAction()
+    {
+        if (!$this->Request()->isPost()) {
+            return;
+        }
 
-		// Sanitize and clean up the search parameter for later processing
-		$search =  $this->Request()->get('search');
-		$search = strtolower($search);
-		$search = trim($search);
+        // Sanitize and clean up the search parameter for later processing
+        $search = $this->Request()->get('search');
+        $search = strtolower($search);
+        $search = trim($search);
 
-		$search = preg_replace("/[^a-z0-9äöüß]/", " ", $search);
-		$search = trim(preg_replace('/\s+/', '%', $search), "%");
+        $search = preg_replace("/[^\\w0-9]+/u", " ", $search);
+        $search = trim(preg_replace('/\s+/', '%', $search), "%");
 
-		$articles = $this->getArticles($search);
-		$customers = $this->getCustomers($search);
-		$orders = $this->getOrders($search);
+        $articles = $this->getArticles($search);
+        $customers = $this->getCustomers($search);
+        $orders = $this->getOrders($search);
 
-		$this->View()->assign('searchResult', array(
-			'articles'  => $articles,
-			'customers' => $customers,
-			'orders'    => $orders
-		));
- 	}
+        $this->View()->assign('searchResult', array(
+            'articles' => $articles,
+            'customers' => $customers,
+            'orders' => $orders
+        ));
+    }
 
-	/**
-	 * Queries the articles from the database based on the passed search term
-	 *
-	 * @param $search
-	 * @return array
-	 */
-	public function getArticles($search) {
+    /**
+     * Queries the articles from the database based on the passed search term
+     *
+     * @param $search
+     * @return array
+     */
+    public function getArticles($search)
+    {
+        $search2 = Shopware()->Db()->quote("$search%");
+        $search = Shopware()->Db()->quote("%$search%");
 
-		$search = Shopware()->Db()->quote("%$search%");
-
-		$sql = "
+        $sql = "
 			SELECT DISTINCT
 				a.id,
 				a.name,
 				a.description_long,
 				a.description,
-				d.ordernumber
-			FROM
-				s_articles as a
-			INNER JOIN s_articles_details as d
-				ON a.id = d.articleID
+				IFNULL(d.ordernumber, m.ordernumber) as ordernumber
+			FROM s_articles as a
+			JOIN s_articles_details as m
+		    ON m.id = a.main_detail_id
+			LEFT JOIN s_articles_details as d
+		    ON a.id = d.articleID
+		    AND d.ordernumber LIKE $search2
 			LEFT JOIN s_articles_translations AS t
-				ON a.id=t.articleID
+            ON a.id=t.articleID
 			LEFT JOIN s_articles_supplier AS s
-			    ON a.supplierID=s.id
-			WHERE
-			    d.kind = 1
-		    AND
-				(
-						a.name LIKE $search
-					OR
-						d.ordernumber LIKE $search
-					OR
-						t.name LIKE $search
-				    OR
-				        s.name LIKE $search
-				)";
+            ON a.supplierID=s.id
+		    WHERE ( a.name LIKE $search
+                OR t.name LIKE $search
+                OR s.name LIKE $search
+            )
+        ";
+        $sql = Shopware()->Db()->limit($sql, $this->searchLimit);
+        return Shopware()->Db()->fetchAll($sql);
+    }
 
-		$sql = Shopware()->Db()->limit($sql, $this->searchLimit);
-		return Shopware()->Db()->fetchAll($sql);
-	}
+    /**
+     * Queries the customers from the database based on the passed search term
+     *
+     * @param $search
+     * @return array
+     */
+    public function getCustomers($search)
+    {
+        $search2 = Shopware()->Db()->quote("$search%");
+        $search = Shopware()->Db()->quote("%$search%");
 
-	/**
-	 * Queries the customers from the database based on the passed search term
-	 *
-	 * @param $search
-	 * @return array
-	 */
-	public function getCustomers($search) {
-
-		$search = Shopware()->Db()->quote("%$search%");
-
-		$sql = "
-			SELECT DISTINCT userID as id, firstname, lastname, company,
+        $sql = "
+			SELECT userID as id,
+			IF(b.company != '', b.company, CONCAT(b.firstname, ' ', b.lastname)) as name,
 			CONCAT(street, ' ', streetnumber, ' ', zipcode, ' ', city) as description
-			FROM s_user_billingaddress, s_user
+			FROM s_user_billingaddress b, s_user u
 			WHERE (
 				email LIKE $search
-				OR customernumber LIKE $search
-				OR TRIM(CONCAT(company,' ',department)) LIKE $search
+				OR customernumber LIKE $search2
+				OR TRIM(CONCAT(company,' ', department)) LIKE $search
 				OR TRIM(CONCAT(firstname,' ',lastname)) LIKE $search
 			)
-			AND s_user.id=s_user_billingaddress.userID
+			AND u.id = b.userID
+			AND u.accountmode = 0
 			GROUP BY id
-			ORDER BY lastname, company ASC
+			ORDER BY name ASC
 		";
 
-		$sql = Shopware()->Db()->limit($sql, $this->searchLimit);
-		$result = Shopware()->Db()->fetchAll($sql);
+        $sql = Shopware()->Db()->limit($sql, $this->searchLimit);
+        $result = Shopware()->Db()->fetchAll($sql);
 
-		foreach($result as &$item) {
-			if ($item["company"]){
-				$item["name"] = $item["company"];
-			} else {
-				$item["name"] = $item["firstname"] . " " . $item["lastname"];
-			}
-		}
+        return $result;
+    }
 
-		return $result;
-	}
+    /**
+     * Queries the orders from the database based on the passed search term
+     *
+     * @param $search
+     * @return array
+     */
+    public function getOrders($search)
+    {
+        $search = Shopware()->Db()->quote("$search%");
 
-	/**
-	 * Queries the orders from the database based on the passed search term
-	 *
-	 * @param $search
-	 * @return array
-	 */
-	public function getOrders($search) {
-		$search = Shopware()->Db()->quote("%$search%");
-
-		$sql = "
+        $sql = "
 			SELECT
-				s_order.id,
-				s_order.ordernumber as name,
-				s_order.userID,
-				s_order.invoice_amount as totalAmount,
-				s_order.transactionID,
-				`status`,
-				`cleared`,
-				`type`,
-				docID,
+				o.id,
+				o.ordernumber as name,
+				o.userID,
+				o.invoice_amount as totalAmount,
+				o.transactionID,
+				o.status,
+				o.cleared,
+				d.type,
+				d.docID,
 				CONCAT(
-                    (
-                    CASE s_order_billingaddress.salutation
-                       WHEN 'company'
-                       THEN s_order_billingaddress.company
-                       ELSE CONCAT(s_order_billingaddress.firstname, ' ', s_order_billingaddress.lastname)
-                    END
-                    ), ', ',
-                    s_core_paymentmeans.description
-				) AS description
-			FROM
-				s_order
-			LEFT JOIN s_order_documents
-			    ON s_order_documents.orderID=s_order.id AND docID != '0'
-			LEFT JOIN s_order_billingaddress
-			    ON s_order.id=s_order_billingaddress.orderID
-			LEFT JOIN s_core_paymentmeans
-			    ON s_order.paymentID = s_core_paymentmeans.id
-			WHERE  s_order.id != '0'
-			AND (s_order.ordernumber LIKE $search OR s_order.transactionID LIKE $search OR docID LIKE $search)
-			GROUP BY s_order.id
-			ORDER BY s_order.ordertime DESC
+                    IF(b.company != '', b.company, CONCAT(b.firstname, ' ', b.lastname)),
+                    ', ',
+                    p.description
+				) as description
+			FROM s_order o
+			LEFT JOIN s_order_documents d
+            ON d.orderID=o.id AND docID != '0'
+			LEFT JOIN s_order_billingaddress b
+            ON o.id=b.orderID
+			LEFT JOIN s_core_paymentmeans p
+            ON o.paymentID = p.id
+			WHERE o.id != '0'
+			AND (o.ordernumber LIKE $search
+			OR o.transactionID LIKE $search
+			OR docID LIKE $search)
+			GROUP BY o.id
+			ORDER BY o.ordertime DESC
 		";
-		$sql = Shopware()->Db()->limit($sql, $this->searchLimit);
-		return Shopware()->Db()->fetchAll($sql);
-	}
+        $sql = Shopware()->Db()->limit($sql, $this->searchLimit);
+        return Shopware()->Db()->fetchAll($sql);
+    }
 }
