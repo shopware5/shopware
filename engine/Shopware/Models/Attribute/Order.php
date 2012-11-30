3b35af782d118669715182c78d5fb0e491340c9b
Stephan Pohl <info@shopware.de>
2012-11-30 13:07:27 +0000

    SW-4572 - Add Icons

--- engine/Shopware/Models/Attribute/Order.php
+++ engine/Shopware/Models/Attribute/Order.php
@@ -111,6 +111,14 @@ class Order extends ModelEntity
 
 
     /**
+     * @var string $swagIpaymentDescription
+     *
+     * @ORM\Column(name="swag_ipayment_description", type="string", nullable=true)
+     */
+     protected $swagIpaymentDescription;
+
+
+    /**
      * @var \Shopware\Models\Order\Order
      *
      * @ORM\OneToOne(targetEntity="Shopware\Models\Order\Order", inversedBy="attribute")
@@ -213,6 +221,18 @@ class Order extends ModelEntity
     public function setAttribute6($attribute6)
     {
         $this->attribute6 = $attribute6;
+        return $this;
+    }
+    
+
+    public function getSwagIpaymentDescription()
+    {
+        return $this->swagIpaymentDescription;
+    }
+
+    public function setSwagIpaymentDescription($swagIpaymentDescription)
+    {
+        $this->swagIpaymentDescription = $swagIpaymentDescription;
         return $this;
     }
     
