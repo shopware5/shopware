3e042bdedcb78400485a132e5f54c9c40d7153c5
Heiner Lohaus <hl@shopware.de>
2013-01-22 18:47:32 +0100

    PLUG-783 Fix update frontend snippets

--- update/deltas/4-snippet.sql
+++ update/deltas/4-snippet.sql
@@ -1,15 +1,28 @@
-REPLACE INTO s_core_snippets
-SELECT s.*
+REPLACE INTO s_core_snippets (`namespace` , `shopID` , `localeID` , `name` , `value` , `created` , `updated`)
+SELECT s.namespace, s.shopID, s.localeID, s.name, s.value, s.created, NOW() as updated, o.value, o2.value
 FROM backup_s_core_snippets s
-LEFT JOIN backup_s_core_snippets o
+
+LEFT JOIN s_core_snippets o
 ON o.namespace = s.namespace
 AND o.name = s.name
 AND o.shopID = 1
 AND o.localeID = 1
+
+LEFT JOIN s_core_snippets o2
+ON o2.namespace = s.namespace
+AND o2.name = s.name
+AND o2.shopID = 1
+AND o2.localeID = 2
+
 WHERE (s.namespace LIKE 'frontend/%'
 OR s.namespace LIKE 'documents/%'
 OR s.namespace LIKE 'newsletter/%')
-AND s.updated > '2011-05-25 00:00:00'
 AND s.value != ''
-AND (o.value IS NULL OR s.id = o.id OR s.value != o.value)
-AND s.value NOT LIKE '%$this->%';
+AND o.id IS NOT NULL
+AND (o.value IS NULL OR s.value != o.value)
+AND (o2.value IS NULL OR s.value != o2.value)
+AND s.value NOT LIKE '%$this->%'
+AND s.value NOT LIKE '%+I120n%'
+AND (s.created != '' OR s.updated > '2010-10-18 00:00:0')
+AND s.value NOT LIKE '%201% shopware%'
+AND (o.localeID != 1 OR s.namespace NOT IN ('frontend/search/fuzzy_left', 'frontend/register/steps'));
