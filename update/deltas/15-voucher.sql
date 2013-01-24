a05fa079f24648e5e896a447b7ef0aaef0e27efb
Benjamin Cremer <bc@shopware.de>
2013-01-24 13:10:59 +0100

    SW-5066 - Separate update-script into independent repository

--- update/deltas/15-voucher.sql
+++
@@ -1,5 +0,0 @@
-UPDATE s_emarketing_vouchers SET `valid_from` = NULL WHERE valid_from = '0000-00-00';
-UPDATE s_emarketing_vouchers SET `valid_to` = NULL WHERE valid_to = '0000-00-00';
-UPDATE s_emarketing_vouchers SET `subshopID` = NULL WHERE subshopID = 0;
-UPDATE s_emarketing_vouchers SET `bindtosupplier` = NULL WHERE bindtosupplier = 0;
-UPDATE s_emarketing_vouchers SET `customergroup` = NULL WHERE customergroup = 0;
--- update/deltas/19-auth.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `s_core_auth` SET `roleID` = '1',
-`failedlogins` = '0',
-`lockeduntil` = '2000-01-01 00:00:00';
