e97a1f5341b16af7f01eaed5bd0f14febb549b1d
Benjamin Cremer <bc@shopware.de>
2013-04-04 14:21:58 +0200

    SW-5636 - Add new file and class-header

--- engine/Shopware/Plugins/Default/Core/SelfHealing/Bootstrap.php
+++ engine/Shopware/Plugins/Default/Core/SelfHealing/Bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Shopware 4.0
- * Copyright © 2012 shopware AG
+ * Copyright © 2013 shopware AG
  *
  * According to our dual licensing model, this program can be used either
  * under the terms of the GNU Affero General Public License, version 3,
@@ -20,16 +20,13 @@
  * The licensing of the program under the AGPLv3 does not imply a
  * trademark license. Therefore any rights, title and interest in
  * our trademarks remain entirely with us.
- *
- * @category   Shopware
- * @package    Shopware_Plugins
- * @subpackage Plugin
- * @copyright  Copyright (c) 2012, shopware AG (http://www.shopware.de)
- * @version    $Id$
- * @author     shopware AG
  */
 
-
+/**
+ * @category  Shopware
+ * @package   Shopware\Plugins\Core
+ * @copyright Copyright (c) 2013, shopware AG (http://www.shopware.de)
+ */
 class Shopware_Plugins_Core_SelfHealing_Bootstrap extends Shopware_Components_Plugin_Bootstrap
 {
     const redirectCookieString = 'ShopwarePluginsCoreSelfHealingRedirect';
