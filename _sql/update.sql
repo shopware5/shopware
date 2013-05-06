71f39b3bcbe298ed883b741fcbbfeba9aa7f38f1
Benjamin Cremer <bc@shopware.de>
2013-05-06 14:09:23 +0200

    Prepare 4.0.8 release

--- _sql/update.sql
+++ _sql/update.sql
@@ -3,55 +3,79 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 SET FOREIGN_KEY_CHECKS = 0;
 
--- 1-add-esd-config.sql
+-- 1-remove-broken-subscribes.sql
+-- SW-5202-remove-broken-subscribes
+
 -- //
 
-SET @parent = (SELECT `id` FROM `s_core_config_forms` WHERE `label` = 'ESD');
+DELETE FROM `s_core_subscribes` WHERE  `listener` LIKE  'Shopware_Plugins_Core_Shop_Bootstrap::%';
+DELETE FROM `s_core_subscribes` WHERE  `listener` LIKE  'Shopware_Plugins_Backend_Locale_Bootstrap::%';
 
-INSERT IGNORE INTO `s_core_config_elements` (`form_id`, `name`, `value`, `label`, `description`, `type`, `required`, `position`, `scope`, `filters`, `validators`, `options`) VALUES
-(@parent, 'redirectDownload', 'b:0;', 'Auf Download-Datei direkt verlinken', NULL, 'boolean', 0, 0, 0, NULL, NULL, NULL);
-
-
-SET @parent = (SELECT `id` FROM `s_core_config_elements` WHERE `name` = 'redirectDownload');
-
-INSERT IGNORE INTO `s_core_config_element_translations` (`id` ,`element_id` ,`locale_id` ,`label` ,`description`)
-VALUES (NULL , @parent, '2', 'Link to download file directly', NULL);
-
--- 2-add-default-email-templates.sql
+-- 2-remove-needless-note-compare-snippet.sql
 -- //
 
-UPDATE `s_cms_support` SET `email_template` = 'Return - Shopware Demoshop
+DELETE FROM `s_core_snippets` WHERE `name` = 'NoteLinkCompare' AND `namespace` = 'frontend/note/item';
 
-Customer no.: {sVars.kdnr}
-eMail: {sVars.email}
+-- 3-change-notification-snippet.sql
+--  //
 
-Invoice no.: {sVars.rechnung}
-Article no.: {sVars.artikel}
-
-Comment:
-{sVars.info}'
-WHERE `name` LIKE "Return"
-AND `email_template` LIKE "INSERT INTO s_user_service%";
+UPDATE `s_core_snippets`
+SET `value` = "Bei der Validierung Ihrer E-Mail-Benachrichtigung ist ein Fehler aufgetreten. Eventuell wurde Ihre eMail-Adresse bereits validiert."
+WHERE `name` = 'DetailNotifyInfoInvalid'
+AND `namespace` = 'frontend/plugins/notification/index'
+AND `value` = "Bei der Validierung Ihrer E-Mail-Benachrichtigung ist ein Fehler aufgetreten.";
 
 
+UPDATE `s_core_snippets`
+SET `value` = "An error has occurred while validating your e-mail address. Possibly your email address has already been validated."
+WHERE `name` = 'DetailNotifyInfoInvalid'
+AND `namespace` = 'frontend/plugins/notification/index'
+AND `value` = "An error has occured while validating your e-mail address.";
 
-UPDATE `s_cms_support` SET `email_template` = 'Defective product - Shopware Demoshop
+-- 4-replicate-the-multilanguage-parent-category-id.sql
+-- //
 
-Company: {sVars.firma}
-Customer no.: {sVars.kdnr}
-eMail: {sVars.email}
+UPDATE s_core_multilanguage as m, s_core_shops as s SET m.parentID=s.category_id WHERE m.id = s.id;
 
-Invoice no.: {sVars.rechnung}
-Article no.: {sVars.artikel}
+-- 5-change-blog-settings-description.sql
+-- //
 
-Description of failure:
---------------------------------
-{sVars.fehler}
+SET @parent = (SELECT `id` FROM `s_core_config_elements` WHERE `name` = 'blogcategory' AND `label` = 'Blog-Einträge aus Kategorie (ID) auf Startseite anzeigen');
 
-Type: {sVars.rechner}
-System {sVars.system}
-How does the problem occur:
-{sVars.wie}'
-WHERE `name` LIKE "Defective product"
-AND `email_template` LIKE "INSERT INTO s_user_service%";
+UPDATE `s_core_config_elements`
+SET `label` = 'Blog-Einträge aus Kategorie (ID) auf Startseite anzeigen (Nur alte Templatebasis)'
+WHERE `id` = @parent;
+
+UPDATE `s_core_config_element_translations`
+SET `label` = 'Show blog entries from category (ID) on starting page (Only old template base)'
+WHERE `id` = @parent;
+
+
+-- 6-remove-broken-cms-support-field.sql
+-- //
+DELETE FROM `s_cms_support_fields` WHERE `name` LIKE "sdfg" AND `label` LIKE "sdf";
+
+-- 7-fix-umlauts-in-frontend-account-snippet.sql
+-- //
+
+UPDATE `s_core_snippets` 
+SET `value` = 'Nachdem Sie die erste Bestellung durchgeführt haben, können Sie hier auf vorherige Rechnungsadressen zugreifen.'
+WHERE `name` LIKE 'SelectBillingInfoEmpty' 
+AND `value` LIKE 'Nachdem Sie die erste Bestellung durchgef?hrt haben, k?nnen Sie hier auf vorherige Rechnungsadressen zugreifen.';
+
+-- 8-add-frontend-account-snippet.sql
+-- //
+
+INSERT IGNORE INTO `s_core_snippets` (`namespace`, `shopID`, `localeID`, `name`, `value`, `created`, `updated`) VALUES
+('frontend/register/shipping_fieldset', 1, 2, 'RegisterShippingLabelMr', 'Mr', '2013-04-22 16:04:23', '2013-04-22 16:04:23'),
+('frontend/register/shipping_fieldset', 1, 1, 'RegisterShippingLabelMr', 'Herr', '2013-04-22 16:04:23', '2013-04-22 16:04:23'),
+('frontend/register/shipping_fieldset', 1, 2, 'RegisterShippingLabelMrs', 'Mrs', '2013-04-22 16:04:23', '2013-04-22 16:04:23'),
+('frontend/register/shipping_fieldset', 1, 1, 'RegisterShippingLabelMrs', 'Frau', '2013-04-22 16:04:23', '2013-04-22 16:04:23');
+
+-- 9-fix-esd-snippet.sql
+-- //
+UPDATE `s_core_snippets`
+SET `value` = 'Dieser Download steht Ihnen nicht zur Verfügung!'
+WHERE `name` LIKE 'DownloadsInfoAccessDenied'
+AND `value` LIKE 'Dieser Download stehen Ihnen nicht zur Verfügung!';
 
