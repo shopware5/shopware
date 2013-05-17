e71267198c0052901c0bb48f1a9d6cbf04d7964a
Benjamin Cremer <bc@shopware.de>
2013-05-17 13:42:29 +0200

    NTR - Fix duplicate key error in deltas

--- _sql/deltas/114-add-new-top-seller.sql
+++ _sql/deltas/114-add-new-top-seller.sql
@@ -1,11 +1,9 @@
 -- //
 
-
 INSERT INTO `s_core_plugins` (`id`, `namespace`, `name`, `label`, `source`, `description`, `description_long`, `active`, `added`, `installation_date`, `update_date`, `refresh_date`, `author`, `copyright`, `license`, `version`, `support`, `changes`, `link`, `store_version`, `store_date`, `capability_update`, `capability_install`, `capability_enable`, `update_source`, `update_version`) VALUES
 (NULL, 'Core', 'MarketingAggregate', 'Shopware Marketing Aggregat Funktionen', 'Default', NULL, NULL, 1, '2013-04-30 14:19:13', '2013-04-30 14:26:48', '2013-04-30 14:26:48', '2013-04-30 14:26:51', 'shopware AG', 'Copyright © 2012, shopware AG', NULL, '1.0.0', NULL, NULL, 'http://www.shopware.de/', NULL, NULL, 1, 1, 1, NULL, NULL);
 
 SET @pluginId = (SELECT id FROM s_core_plugins WHERE name = 'MarketingAggregate');
-
 
 INSERT INTO `s_core_subscribes` (`id`, `subscribe`, `type`, `listener`, `pluginID`, `position`) VALUES
 (NULL, 'Shopware_Modules_Order_SaveOrder_ProcessDetails', 0, 'Shopware_Plugins_Core_MarketingAggregate_Bootstrap::incrementTopSeller', @pluginId, 0),
@@ -25,7 +23,6 @@ INSERT INTO `s_core_subscribes` (`id`, `subscribe`, `type`, `listener`, `pluginI
 (NULL, 'Shopware_Plugins_LastArticles_ResetLastArticles', 0, 'Shopware_Plugins_Core_MarketingAggregate_Bootstrap::afterSimilarShownArticlesReset', @pluginId, 0),
 (NULL, 'Shopware_Modules_Articles_Before_SetLastArticle', 0, 'Shopware_Plugins_Core_MarketingAggregate_Bootstrap::beforeSetLastArticle', @pluginId, 0),
 (NULL, 'Shopware_CronJob_RefreshSimilarShown', 0, 'Shopware_Plugins_Core_MarketingAggregate_Bootstrap::refreshSimilarShown', @pluginId, 0);
-
 
 CREATE TABLE IF NOT EXISTS `s_articles_also_bought_ro` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -63,11 +60,7 @@ CREATE TABLE IF NOT EXISTS `s_articles_top_seller_ro` (
 
 ALTER TABLE  `s_emarketing_lastarticles` ADD INDEX  `get_last_articles` (  `sessionID` ,  `time` );
 ALTER TABLE  `s_articles` ADD INDEX  `product_newcomer` (  `active` ,  `datum` );
-ALTER TABLE  `s_articles_categories` ADD INDEX  `category_id_by_article_id` (  `articleID` ,  `id` );
 ALTER TABLE  `s_articles_details` ADD INDEX  `get_similar_articles` (  `kind` ,  `sales` );
-
-
-
 
 -- //@UNDO
 
