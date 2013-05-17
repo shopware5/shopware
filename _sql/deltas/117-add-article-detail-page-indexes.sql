9b6aeabc5c1bf2a4910f08ea81cf3f330327e1a8
Benjamin Cremer <bc@shopware.de>
2013-05-17 16:09:21 +0200

    NTR - Fix merge regression

--- _sql/deltas/117-add-article-detail-page-indexes.sql
+++ _sql/deltas/117-add-article-detail-page-indexes.sql
@@ -2,11 +2,11 @@
 
 -- //
 
-ALTER TABLE `s_articles_categories` ADD INDEX  `category_id_by_article_id` (  `articleID` ,  `id` );
+ALTER TABLE `s_articles_categories_ro` ADD INDEX  `category_id_by_article_id` (  `articleID` ,  `id` );
 
 -- //@UNDO
 
-ALTER TABLE `s_articles_categories` DROP INDEX  `category_id_by_article_id`;
+ALTER TABLE `s_articles_categories_ro` DROP INDEX  `category_id_by_article_id`;
 
 -- //
 
