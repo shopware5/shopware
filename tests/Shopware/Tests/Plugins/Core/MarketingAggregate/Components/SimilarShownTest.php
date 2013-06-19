1cf2aab7c729b13c70133a508a2648ffa34ab99c
Oliver Denter <dr@shopware.de>
2013-06-19 13:27:34 +0200

    SW-6383 - Add unit test case class for the similar shown articles. Add fixture for some similar shown demo data. Implement test case for the initSimilarShown function.

--- tests/Shopware/Tests/Plugins/Core/MarketingAggregate/Components/SimilarShownTest.php
+++ tests/Shopware/Tests/Plugins/Core/MarketingAggregate/Components/SimilarShownTest.php
@@ -3,7 +3,52 @@
 class Shopware_Tests_Plugins_Core_MarketingAggregate_Components_SimilarShownTest extends Shopware_Tests_Plugins_Core_MarketingAggregate_AbstractMarketing
 {
 
+    protected function getDemoData()
+    {
+        return require __DIR__ . '/fixtures/similarShown.php';
+    }
 
+    /**
+     * The demo data contains 144 combinations of the similar shown articles for three users.
+     */
+    protected function insertDemoData()
+    {
+        $this->Db()->query("DELETE FROM s_emarketing_lastarticles");
+        $statement = $this->Db()->prepare("
+            INSERT INTO s_emarketing_lastarticles (img, name, articleID, sessionID, time, userID, shopID)
+            VALUES(:img, :name, :articleID, :sessionID, :time, :userID, :shopID)"
+        );
+        foreach($this->getDemoData() as $data) {
+            $statement->execute($data);
+        }
+    }
+
+    protected function getAllSimilarShown($condition = '')
+    {
+        return $this->Db()->fetchAll('SELECT * FROM s_articles_similar_shown_ro ' . $condition);
+    }
+
+    protected function resetSimilarShown($condition = '')
+    {
+        $this->Db()->query("DELETE FROM s_articles_similar_shown_ro " . $condition);
+    }
+
+    public function testResetSimilarShown()
+    {
+        $this->SimilarShown()->resetSimilarShown();
+        $this->assertCount(0, $this->getAllSimilarShown());
+    }
+
+    public function testInitSimilarShown()
+    {
+        $this->insertDemoData();
+
+        $this->SimilarShown()->initSimilarShown();
+
+        $data = $this->getAllSimilarShown();
+
+        $this->assertCount(144, $data);
+    }
 
 
 
