735c0df9d61f103b630a4b556e8cd073946b645b
Marcel Schmaeing <ms@shopware.de>
2013-06-24 13:51:20 +0200

    SW-6390 change sw version for final release

--- install/templates/header.php
+++ install/templates/header.php
@@ -27,7 +27,7 @@ if (!defined("installer")){
 
     <div class="meta">
         <p>
-            <strong>Shopware-Version:</strong> 4.1.0 RC2
+            <strong>Shopware-Version:</strong> 4.1.0
         </p>
 
         <p>
