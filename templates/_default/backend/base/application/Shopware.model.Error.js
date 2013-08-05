d7afa87cc82b158350ed2dcb8a0126c8c49657fc
Oliver Denter <dr@shopware.de>
2013-08-05 09:01:49 +0200

    SW-5370 - Update grid panel and controller documentation. Remove setOperation function in the error model. Add internal property definition.

--- templates/_default/backend/base/application/Shopware.model.Error.js
+++ templates/_default/backend/base/application/Shopware.model.Error.js
@@ -10,20 +10,7 @@ Ext.define('Shopware.model.Error', {
         { name: 'request' },
         { name: 'error', type: 'string' },
         { name: 'operation' },
-    ],
-
-
-    setOperation: function(operation) {
-        var me = this;
-
-        me.set('success', operation.wasSuccessful());
-        me.set('error', operation.getError());
-        me.set('request', operation.request);
-        me.set('operation', operation);
-
-        console.log(me);
-    }
-
+    ]
 });
 
 
