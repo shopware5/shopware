5da983c7e4d8edc95837748c5372d8195d87b05e
Oliver Denter <dr@shopware.de>
2013-08-07 12:12:41 +0200

    SW-6621 - Fix migration number.

--- _sql/migrations/131-add-top-seller-index.php
+++ _sql/migrations/131-add-top-seller-index.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration130 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration131 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
