4d667ce5dc8239fc13a113d997f6c5d5553b261b
Linus Holtstiege <lh@shopware.de>
2013-09-11 15:21:28 +0200

    SW-6759 - code styling & snippet fix for default template

---
+++ _sql/migrations/137-fix-typo-last-articles-headline.php
@@ -0,0 +1,11 @@
+<?php
+class Migrations_Migration137 Extends Shopware\Components\Migrations\AbstractMigration
+{
+    public function up()
+    {
+        $sql = <<<'EOD'
+UPDATE `s_core_snippets` SET `value` = 'Zuletzt angesehen' WHERE `name` = 'WidgetsRecentlyViewedHeadline' AND `value` = 'Zuletzt angeschaute Artikel';
+EOD;
+        $this->addSql($sql);
+    }
+}
