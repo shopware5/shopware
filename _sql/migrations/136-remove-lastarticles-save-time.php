2950d3285ff84638f38b80b8f8abd380717dff37
Linus Holtstiege <lh@shopware.de>
2013-09-11 16:05:03 +0200

    SW-6759 - snippet fix

--- _sql/migrations/136-remove-lastarticles-save-time.php
+++ _sql/migrations/136-remove-lastarticles-save-time.php
@@ -4,7 +4,12 @@ class Migrations_Migration136 Extends Shopware\Components\Migrations\AbstractMig
     public function up()
     {
         $sql = <<<'EOD'
-UPDATE `s_core_snippets` SET `value` = 'Zuletzt angesehen' WHERE `name` = 'WidgetsRecentlyViewedHeadline' AND `value` = 'Zuletzt angeschaute Artikel';
+SET @formId = (SELECT id FROM s_core_config_forms WHERE name = 'LastArticles' LIMIT 1);
+SET @elementId = (SELECT id FROM s_core_config_elements WHERE name = 'time' AND form_id = @formId LIMIT 1);
+
+DELETE FROM s_core_config_element_translations WHERE element_id = @elementId;
+DELETE FROM s_core_config_values WHERE element_id = @elementId;
+DELETE FROM s_core_config_elements WHERE id = @elementId;
 EOD;
         $this->addSql($sql);
     }
