8a4987b97c03172fc8e6fd276f60c471ad55c60b
Benjamin Cremer <bc@shopware.de>
2013-09-17 15:30:12 +0200

    SW-6759 - Adjust migration-numbers to current master

--- _sql/migrations/137-remove-lastarticles-save-time.php
+++ _sql/migrations/137-remove-lastarticles-save-time.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration136 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration137 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
