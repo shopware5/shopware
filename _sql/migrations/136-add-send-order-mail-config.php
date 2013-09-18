320340e14485e6bd1335b262947c204d4f27b02e
Marcel Schmaeing <ms@shopware.de>
2013-09-18 10:54:29 +0200

    SW-6908 remove snippet from base component and add scope to send order mail config

--- _sql/migrations/136-add-send-order-mail-config.php
+++ _sql/migrations/136-add-send-order-mail-config.php
@@ -7,7 +7,7 @@ class Migrations_Migration136 Extends Shopware\Components\Migrations\AbstractMig
         SET @parent = (SELECT id FROM s_core_config_forms WHERE name = 'Checkout' LIMIT 1);
 
         INSERT IGNORE INTO `s_core_config_elements` (`id`, `form_id`, `name`, `value`, `label`, `description`, `type`, `required`, `position`, `scope`, `filters`, `validators`, `options`) VALUES
-        (NULL, @parent, 'sendOrderMail', 'b:1;', 'Bestell-Abschluss-eMail versenden', NULL, 'checkbox', 0, 0, 0, NULL, NULL, 'a:0:{}');
+        (NULL, @parent, 'sendOrderMail', 'b:1;', 'Bestell-Abschluss-eMail versenden', NULL, 'checkbox', 0, 0, 1, NULL, NULL, 'a:0:{}');
 
         SET @elementId = (SELECT id FROM `s_core_config_elements` WHERE `name` = 'sendOrderMail' LIMIT 1);
         INSERT IGNORE INTO `s_core_config_element_translations` (`element_id`, `locale_id`, `label`)
