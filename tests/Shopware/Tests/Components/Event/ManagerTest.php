2eafbf6670d4b743a552caf8dba009fea7bb7287
Oliver Denter <dr@shopware.de>
2013-10-16 11:38:33 +0200

    Implement new collect event which can be used to add additionally entries in an collection.

---
+++ tests/Shopware/Tests/Components/Event/ManagerTest.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Shopware 4.0
+ * Copyright © 2012 shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+/**
+ * @category  Shopware
+ * @package   Shopware\Tests
+ * @copyright Copyright (c) 2012, shopware AG (http://www.shopware.de)
+ */
+class Shopware_Tests_Components_Event_ManagerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testAppendEventWithArray()
+    {
+        $event = new Enlight_Event_EventHandler(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithArray',
+            array($this, 'appendEventWithArrayListener')
+        );
+        Shopware()->Events()->registerListener($event);
+
+        /**@var $values \Doctrine\Common\Collections\ArrayCollection */
+        $values = new \Doctrine\Common\Collections\ArrayCollection(array('foo', 'bar'));
+        $values = Shopware()->Events()->collect(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithArray',
+            $values
+        );
+        $this->assertCount(4, $values->getValues());
+        $this->assertEquals('foo', $values->get(0));
+        $this->assertEquals('bar', $values->get(1));
+        $this->assertEquals(array('foo2'), $values->get(2));
+        $this->assertEquals('bar2', $values->get(3));
+    }
+
+    public function appendEventWithArrayListener(Enlight_Event_EventArgs $args)
+    {
+        return new \Doctrine\Common\Collections\ArrayCollection(array(
+            array('foo2'),
+            'bar2'
+        ));
+    }
+
+    public function testAppendEventWithSingleValue()
+    {
+        $values = new \Doctrine\Common\Collections\ArrayCollection(array('foo', 'bar'));
+
+        $event = new Enlight_Event_EventHandler(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithSingleValue',
+            array($this, 'appendEventWithSingleValueListener')
+        );
+        Shopware()->Events()->registerListener($event);
+
+        $values = Shopware()->Events()->collect(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithSingleValue',
+            $values,
+            array()
+        );
+        $this->assertCount(3, $values);
+    }
+
+    public function appendEventWithSingleValueListener(Enlight_Event_EventArgs $args)
+    {
+        return 'foo2';
+    }
+
+
+    public function testAppendEventWithNullValue()
+    {
+        $values = new \Doctrine\Common\Collections\ArrayCollection(array('foo', 'bar'));
+
+        $event = new Enlight_Event_EventHandler(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithNullValue',
+            array($this, 'appendEventWithNullValueListener')
+        );
+        Shopware()->Events()->registerListener($event);
+
+        $values = Shopware()->Events()->collect(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithNullValue',
+            $values,
+            array()
+        );
+        $this->assertCount(2, $values->getValues());
+    }
+
+    public function appendEventWithNullValueListener(Enlight_Event_EventArgs $args)
+    {
+        return null;
+    }
+
+
+    public function testAppendEventWithBooleanValue()
+    {
+        $values = new \Doctrine\Common\Collections\ArrayCollection(array('foo', 'bar'));
+
+        $event = new Enlight_Event_EventHandler(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithBooleanValue',
+            array($this, 'appendEventWithBooleanValueListener')
+        );
+        Shopware()->Events()->registerListener($event);
+
+        $values = Shopware()->Events()->collect(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithBooleanValue',
+            $values
+        );
+        $this->assertCount(3, $values->getValues());
+        $this->assertEquals(true, $values->get(2));
+    }
+
+    public function appendEventWithBooleanValueListener(Enlight_Event_EventArgs $args)
+    {
+        return new \Doctrine\Common\Collections\ArrayCollection(array(
+            true
+        ));
+    }
+
+
+    public function testAppendEventWithNoListener()
+    {
+        $values = new \Doctrine\Common\Collections\ArrayCollection(array('foo', 'bar'));
+        $values = Shopware()->Events()->collect(
+            'Shopware_Tests_Components_Event_ManagerTest_Append_testAppendEventWithNoListener',
+            $values
+        );
+        $this->assertCount(2, $values->getValues());
+    }
+}
