fd27474d18f3f21976a2a6be5502df086419f186
Benjamin Cremer <bc@shopware.de>
2013-10-21 09:49:56 +0200

    Tidy up DependencyInjection namespace, remove unused files, add docblocks etc.

--- engine/Shopware/Components/DependencyInjection/Bridge/Cache.php
+++ engine/Shopware/Components/DependencyInjection/Bridge/Cache.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Shopware 4.0
+ * Copyright © 2013 shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 namespace Shopware\Components\DependencyInjection\Bridge;
 
@@ -10,6 +32,10 @@ use Zend_Locale_Data;
  *
  * Wrapper for accessing the used zend cache instance
  * + call of Zend_Locale_Data::setCache.
+ *
+ * @category  Shopware
+ * @package   Shopware\Components\DependencyInjection\Bridge
+ * @copyright Copyright (c) 2013, shopware AG (http://www.shopware.de)
  */
 class Cache
 {
--- engine/Shopware/Components/DependencyInjection/Bridge/Config.php
+++ engine/Shopware/Components/DependencyInjection/Bridge/Config.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Shopware 4.0
+ * Copyright © 2013 shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 namespace Shopware\Components\DependencyInjection\Bridge;
 
@@ -6,6 +28,11 @@ use Enlight_Components_Db_Adapter_Pdo_Mysql;
 use Shopware_Components_Config;
 use Zend_Cache_Core;
 
+/**
+* @category  Shopware
+* @package   Shopware\Components\DependencyInjection\Bridge
+* @copyright Copyright (c) 2013, shopware AG (http://www.shopware.de)
+ */
 class Config
 {
     private $cache;
