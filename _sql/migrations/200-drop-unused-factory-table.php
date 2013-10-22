47c8c8ab7557963fab746c87ca2492452a6b88e7
Benjamin Cremer <bc@shopware.de>
2013-10-22 10:11:09 +0200

    Refactor loading of core classes.
    - Remove unused table lookup and delete table
    - Load files via composer

---
+++ _sql/migrations/200-drop-unused-factory-table.php
@@ -0,0 +1,12 @@
+<?php
+class Migrations_Migration200 Extends Shopware\Components\Migrations\AbstractMigration
+{
+    public function up()
+    {
+        $sql = <<<'EOD'
+DROP TABLE s_core_factory;
+EOD;
+
+        $this->addSql($sql);
+    }
+}
