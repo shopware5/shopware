66a6146a741ced155f2ccadd734a68e7f56412e7
Tiago Garcia <tg@shopware.de>
2013-11-18 15:27:55 +0100

    SW-7398 Added config option to choose between new and old behavior

---
+++ _sql/migrations/150-add-checkout-payment-info-change-config.php
@@ -0,0 +1,22 @@
+<?php
+class Migrations_Migration150 Extends Shopware\Components\Migrations\AbstractMigration
+{
+    public function up()
+    {
+        $sql = <<<'EOD'
+        SET @parent = (SELECT id FROM s_core_config_forms WHERE name = 'Checkout' LIMIT 1);
+
+        INSERT IGNORE INTO `s_core_config_elements`
+        (`form_id`, `name`, `value`, `label`, `description`, `type`, `required`, `position`, `scope`, `filters`, `validators`, `options`)
+        VALUES
+        (@parent, 'paymentEditingInCheckoutPage', 'b:0;', 'Lastschriftdaten im Checkout editierbar', NULL, 'boolean', 0, 1, 1, NULL, NULL, NULL);
+
+        SET @elementOne = (SELECT id FROM s_core_config_elements WHERE name = 'paymentEditingInCheckoutPage' LIMIT 1);
+
+        INSERT IGNORE INTO `s_core_config_element_translations` (`id`, `element_id`, `locale_id`, `label`, `description`)
+        VALUES
+        (NULL, @elementOne, '2', 'Allow payment details editing on checkout page', 'If set to "No", the customer will not be able to edit the debit payment details directly on the checkout page, and will need to use a dedicated page for that purpose.');
+EOD;
+        $this->addSql($sql);
+    }
+}
