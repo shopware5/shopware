487dff48d922e0aca55c6bdabd7b87f770008392
Benjamin Cremer <bc@shopware.de>
2013-11-21 16:02:06 +0100

    SW-6856 - Add Plugin testcase to setup plugin before test runs.

---
+++ engine/Shopware/Components/Test/Plugin/TestCase.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Shopware 4.0
+ * Copyright © 2013 shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+namespace Shopware\Components\Test\Plugin;
+
+use Shopware\Components\Plugin\Installer;
+
+/**
+ * @runInSeparateProcess
+ * @category  Shopware
+ * @package   Shopware\Components\Test\Plugin
+ * @copyright Copyright (c) 2013, shopware AG (http://www.shopware.de)
+ */
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Installer
+     */
+    private static $pluginInstaller;
+
+    /**
+     * @var array
+     */
+    protected static $ensureLoadedPlugins = array();
+
+    /**
+     * @var array
+     */
+    private static $pluginStates = array();
+
+    public static function setUpBeforeClass()
+    {
+        self::$pluginInstaller = Shopware()->ResourceLoader()->get('shopware.plugin_installer');
+        $loadedPlugins = static::$ensureLoadedPlugins;
+
+        foreach ($loadedPlugins as $key => $value) {
+
+            if (is_array($value)) {
+                $pluginName = $key;
+                $config = $value;
+            } else {
+                $pluginName = $value;
+                $config = array();
+            }
+
+            self::ensurePluginAvailable($pluginName, $config);
+        }
+    }
+
+    public static function tearDownAfterClass()
+    {
+        self::restorePluginStates();
+        self::$pluginInstaller = null;
+        Shopware()->Models()->clear();
+    }
+
+    /**
+     * @param $pluginName
+     * @param array $config
+     */
+    private static function ensurePluginAvailable($pluginName, $config = array())
+    {
+        $plugin = self::$pluginInstaller->getPluginByName($pluginName);
+
+        self::$pluginStates[$pluginName] = array(
+            'isInstalled' => (bool) $plugin->getInstalled(),
+            'isActive'    => (bool) $plugin->getActive(),
+        );
+
+        self::$pluginInstaller->installPlugin($plugin);
+        self::$pluginInstaller->activatePlugin($plugin);
+        self::$pluginInstaller->activatePlugin($plugin);
+
+        foreach ($config as $element => $value) {
+            self::$pluginInstaller->saveConfigElement($plugin, $element, $value);
+        }
+
+    }
+
+    private static function restorePluginStates()
+    {
+        Shopware()->Models()->clear();
+        foreach (self::$pluginStates as $pluginName => $status) {
+            self::restorePluginState($pluginName, $status);
+        }
+    }
+
+    /**
+     * @param $pluginName
+     * @param $status
+     * @return int
+     */
+    private static function restorePluginState($pluginName, $status)
+    {
+        $plugin = self::$pluginInstaller->getPluginByName($pluginName);
+
+        if (!$status['isInstalled']) {
+            self::$pluginInstaller->uninstallPlugin($plugin);
+            return;
+        }
+
+        if (!$status['isActive']) {
+            self::$pluginInstaller->deactivatePlugin($plugin);
+        }
+    }
+}
