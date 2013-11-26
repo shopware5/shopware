c7e4e74e9993e2c1836c932cb3edd9b0124cde15
Benjamin Cremer <bc@shopware.de>
2013-11-26 13:26:13 +0100

    SW-6856 - Remove dependecy on backend session form client.

--- engine/Shopware/Plugins/Default/Backend/StoreApi/Components/StoreConfig.php
+++ engine/Shopware/Plugins/Default/Backend/StoreApi/Components/StoreConfig.php
@@ -1,15 +1,36 @@
 <?php
 class Shopware_Components_StoreConfig extends Enlight_Class
 {
+    protected $version;
+    protected $language;
+
     public function setVersion($version)
     {
-        Shopware()->BackendSession()->storeApiConfigVersion = $version;
+        $this->version = $version;
+
         return $this;
     }
 
     public function setLanguage($language)
     {
-        Shopware()->BackendSession()->storeApiConfigLanguage = $language;
+        $this->language = $language;
+
         return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVersion()
+    {
+        return $this->version;
     }
 }
