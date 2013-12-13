66fcd4bfe9ea55c163ef4f68de2917cebd676554
Benjamin Cremer <bc@shopware.de>
2013-12-13 09:25:52 +0100

    SW-7461 - Reformat legacy class according to our coding styles.

--- engine/Shopware/Plugins/Default/Core/PaymentMethods/Components/Legacy/debit.php
+++ engine/Shopware/Plugins/Default/Core/PaymentMethods/Components/Legacy/debit.php
@@ -27,123 +27,127 @@
  *
  * Deprecated payment class for debit procedures
  */
-class sPaymentMean{
-	var $sSYSTEM;
+class sPaymentMean
+{
+    public $sSYSTEM;
 
     /**
      * DEPRECATED
      * @return array|bool
      */
-	function sInit(){
-
-		if (!$this->sSYSTEM->_POST["sDebitAccount"]){
-			$sErrorFlag["sDebitAccount"] = true;
-		}
-		if (!$this->sSYSTEM->_POST["sDebitBankcode"]){
-			$sErrorFlag["sDebitBankcode"] = true;
-		}
-		if (!$this->sSYSTEM->_POST["sDebitBankName"]){
-			$sErrorFlag["sDebitBankName"] = true;
-		}
-		if (empty($this->sSYSTEM->_POST["sDebitBankHolder"])&&isset($this->sSYSTEM->_POST["sDebitBankHolder"])){
-			$sErrorFlag["sDebitBankHolder"] = true;
-		}
-
-
-		$checkColumns = $this->sSYSTEM->sDB_CONNECTION->GetAll("SHOW COLUMNS FROM s_user_debit");
-		$foundColumn = false;
-		foreach ($checkColumns as $column){
-			if ($column["Field"]=="bankholder"){
-				$foundColumn = true;
-			}
-		}
-		if (empty($foundColumn)){
-
-			$this->sSYSTEM->sDB_CONNECTION->Execute("ALTER TABLE `s_user_debit` ADD `bankholder` VARCHAR( 255 ) NOT NULL ;");
-		}
+    public function sInit()
+    {
+        if (!$this->sSYSTEM->_POST["sDebitAccount"]) {
+            $sErrorFlag["sDebitAccount"] = true;
+        }
+        if (!$this->sSYSTEM->_POST["sDebitBankcode"]) {
+            $sErrorFlag["sDebitBankcode"] = true;
+        }
+        if (!$this->sSYSTEM->_POST["sDebitBankName"]) {
+            $sErrorFlag["sDebitBankName"] = true;
+        }
+        if (empty($this->sSYSTEM->_POST["sDebitBankHolder"])&&isset($this->sSYSTEM->_POST["sDebitBankHolder"])) {
+            $sErrorFlag["sDebitBankHolder"] = true;
+        }
 
 
-		if (count($sErrorFlag)) $error = true;
+        $checkColumns = $this->sSYSTEM->sDB_CONNECTION->GetAll("SHOW COLUMNS FROM s_user_debit");
+        $foundColumn = false;
+        foreach ($checkColumns as $column) {
+            if ($column["Field"]=="bankholder") {
+                $foundColumn = true;
+            }
+        }
+        if (empty($foundColumn)) {
 
-		if ($error){
-			$sErrorMessages[] = Shopware()->Snippets()->getNamespace('frontend/account/internalMessages')->get('ErrorFillIn','Please fill in all red fields');
+            $this->sSYSTEM->sDB_CONNECTION->Execute("ALTER TABLE `s_user_debit` ADD `bankholder` VARCHAR( 255 ) NOT NULL ;");
+        }
 
-			return array("sErrorFlag"=>$sErrorFlag,"sErrorMessages"=>$sErrorMessages);
-		}else {
-			return true;
-		}
 
-		return array();
-	}
+        if (count($sErrorFlag)) $error = true;
+
+        if ($error) {
+            $sErrorMessages[] = Shopware()->Snippets()->getNamespace('frontend/account/internalMessages')->get('ErrorFillIn', 'Please fill in all red fields');
+
+            return array("sErrorFlag"=>$sErrorFlag,"sErrorMessages"=>$sErrorMessages);
+        } else {
+            return true;
+        }
+
+        return array();
+    }
 
     /**
      * DEPRECATED
      * @return mixed
      */
-	function sUpdate(){
-		if (empty($this->sSYSTEM->_SESSION["sUserId"])) return;
+    public function sUpdate()
+    {
+        if (empty($this->sSYSTEM->_SESSION["sUserId"])) return;
 
-		if (count($this->getData())){
-			$data = array(
-				$this->sSYSTEM->_POST["sDebitAccount"],
-				$this->sSYSTEM->_POST["sDebitBankcode"],
-				$this->sSYSTEM->_POST["sDebitBankName"],
-				$this->sSYSTEM->_POST["sDebitBankHolder"],
-				$this->sSYSTEM->_SESSION["sUserId"]
-			);
+        if (count($this->getData())) {
+            $data = array(
+                $this->sSYSTEM->_POST["sDebitAccount"],
+                $this->sSYSTEM->_POST["sDebitBankcode"],
+                $this->sSYSTEM->_POST["sDebitBankName"],
+                $this->sSYSTEM->_POST["sDebitBankHolder"],
+                $this->sSYSTEM->_SESSION["sUserId"]
+            );
 
-			$update = $this->sSYSTEM->sDB_CONNECTION->Execute("
-			UPDATE s_user_debit SET account=?, bankcode=?, bankname=?,bankholder=?
-			WHERE userID=?",$data);
-		}else {
-			$data = array(
-				$this->sSYSTEM->_SESSION["sUserId"],
-				$this->sSYSTEM->_POST["sDebitAccount"],
-				$this->sSYSTEM->_POST["sDebitBankcode"],
-				$this->sSYSTEM->_POST["sDebitBankName"],
-				$this->sSYSTEM->_POST["sDebitBankHolder"]
-			);
-			$update = $this->sSYSTEM->sDB_CONNECTION->Execute("
-			INSERT INTO s_user_debit (userID, account, bankcode, bankname, bankholder)
-			VALUES (?,?,?,?,?)
-			",$data);
-		}
-	}
+            $update = $this->sSYSTEM->sDB_CONNECTION->Execute("
+            UPDATE s_user_debit SET account=?, bankcode=?, bankname=?,bankholder=?
+            WHERE userID=?",$data);
+        } else {
+            $data = array(
+                $this->sSYSTEM->_SESSION["sUserId"],
+                $this->sSYSTEM->_POST["sDebitAccount"],
+                $this->sSYSTEM->_POST["sDebitBankcode"],
+                $this->sSYSTEM->_POST["sDebitBankName"],
+                $this->sSYSTEM->_POST["sDebitBankHolder"]
+            );
+            $update = $this->sSYSTEM->sDB_CONNECTION->Execute("
+            INSERT INTO s_user_debit (userID, account, bankcode, bankname, bankholder)
+            VALUES (?,?,?,?,?)
+            ",$data);
+        }
+    }
 
     /**
      * DEPRECATED
      * @param $userId
      * @return bool
      */
-	function sInsert($userId){
-		if (!$userId) return false;
-		// Insert data
-		$data = array(
-				$userId,
-				$this->sSYSTEM->_POST["sDebitAccount"],
-				$this->sSYSTEM->_POST["sDebitBankcode"],
-				$this->sSYSTEM->_POST["sDebitBankName"],
-				$this->sSYSTEM->_POST["sDebitBankHolder"]
-			);
-		$update = $this->sSYSTEM->sDB_CONNECTION->Execute("
-		INSERT INTO s_user_debit (userID, account, bankcode, bankname,bankholder)
-		VALUES (?,?,?,?,?)
-		",$data);
-		return true;
-	}
+    public function sInsert($userId)
+    {
+        if (!$userId) return false;
+        
+        // Insert data
+        $data = array(
+                $userId,
+                $this->sSYSTEM->_POST["sDebitAccount"],
+                $this->sSYSTEM->_POST["sDebitBankcode"],
+                $this->sSYSTEM->_POST["sDebitBankName"],
+                $this->sSYSTEM->_POST["sDebitBankHolder"]
+            );
+        $update = $this->sSYSTEM->sDB_CONNECTION->Execute("
+        INSERT INTO s_user_debit (userID, account, bankcode, bankname,bankholder)
+        VALUES (?,?,?,?,?)
+        ",$data);
+        return true;
+    }
 
     /**
      * DEPRECATED
      * @return array
      */
-	function getData(){
-		if (empty($this->sSYSTEM->_SESSION["sUserId"])) return array();
+    public function getData()
+    {
+        if (empty($this->sSYSTEM->_SESSION["sUserId"])) return array();
 
-		$getData = $this->sSYSTEM->sDB_CONNECTION->GetRow("
-		SELECT account AS sDebitAccount, bankcode AS sDebitBankcode, bankname AS sDebitBankName, bankholder AS sDebitBankHolder FROM s_user_debit WHERE
-		userID=?",array($this->sSYSTEM->_SESSION["sUserId"]));
+        $getData = $this->sSYSTEM->sDB_CONNECTION->GetRow("
+        SELECT account AS sDebitAccount, bankcode AS sDebitBankcode, bankname AS sDebitBankName, bankholder AS sDebitBankHolder FROM s_user_debit WHERE
+        userID=?",array($this->sSYSTEM->_SESSION["sUserId"]));
 
-		return $getData;
-	}
+        return $getData;
+    }
 }
-?>
