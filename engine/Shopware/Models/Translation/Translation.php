25bf1bbee06693083f73f9e15084e2b8079a3ffb
Oliver Denter <dr@shopware.de>
2013-12-17 10:59:38 +0100

    SW-7011 - Add licence header

--- engine/Shopware/Models/Translation/Translation.php
+++ engine/Shopware/Models/Translation/Translation.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * Shopware 4
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
 
 namespace Shopware\Models\Translation;
 
@@ -11,6 +34,11 @@ use Doctrine\ORM\Mapping AS ORM;
 
 /**
  * Class Translation
+ *
+ * @category  Shopware
+ * @package   Shopware\Models
+ * @copyright Copyright (c) shopware AG (http://www.shopware.de)
+ *
  * @ORM\Entity
  * @ORM\Table(name="s_core_translations")
  */
