fcc2c9bbc4191c7b50a7fa17725e038609348afa
adminmarci <marcel.mallek@gmx.de>
2013-12-30 12:19:35 +0100

    Update DateObject.php

--- engine/Library/Zend/Date/DateObject.php
+++ engine/Library/Zend/Date/DateObject.php
@@ -312,10 +312,17 @@ abstract class Zend_Date_DateObject {
         }
 
         if (abs($timestamp) <= 0x7FFFFFFF) {
-            $result = ($gmt) ? @gmdate($format, $timestamp) : @date($format, $timestamp);
-            date_default_timezone_set($oldzone);
-            return $result;
-        }
+    // See ZF-11992
+    // "o" will sometimes resolve to the previous year (see 
+    // http://php.net/date ; it's part of the ISO 8601 
+    // standard). However, this is not desired, so replacing 
+    // all occurrences of "o" not preceded by a backslash 
+    // with "Y"
+    $format = preg_replace('/(?<!\\\\)o\b/', 'Y', $format);
+    $result = ($gmt) ? @gmdate($format, $timestamp) : @date($format, $timestamp);
+    date_default_timezone_set($oldzone);
+    return $result;
+}
 
         $jump      = false;
         $origstamp = $timestamp;
