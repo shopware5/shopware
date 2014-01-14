6e44a0a355cebff1ea37c30da41bbef63a917ca1
t.garcia <tg@shopware.de>
2014-01-14 12:09:35 +0100

    SW-7561 - Fix migration name conflict

--- _sql/migrations/214-add-debit-fields-to-payment-data.php
+++ _sql/migrations/214-add-debit-fields-to-payment-data.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration213 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration214 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
