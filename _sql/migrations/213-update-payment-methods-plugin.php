d3553f20c8d56246b74dac06bca084069c2fd3c5
t.garcia <tg@shopware.de>
2014-01-14 07:44:19 +0100

    SW-7681 - Add additional restrictions to migration

--- _sql/migrations/213-update-payment-methods-plugin.php
+++ _sql/migrations/213-update-payment-methods-plugin.php
@@ -9,7 +9,7 @@ class Migrations_Migration213 Extends Shopware\Components\Migrations\AbstractMig
         description = 'Shopware Payment Methods handling. This plugin is required to handle payment methods, and should not be deactivated',
         capability_enable = 1,
         capability_update = 1
-        WHERE name = 'PaymentMethods' and version = '1.0.0'
+        WHERE name = 'PaymentMethods' and version = '1.0.0' and author = 'shopware AG'
 EOD;
 
         $this->addSql($sql);
