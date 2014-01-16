2372ca813219d77244b34964c5024a04f4141625
Oliver Denter <dr@shopware.de>
2014-01-16 17:41:26 +0100

    SW-6606 - Add statistic for sales per customer group.

---
+++ templates/_default/backend/analytics/view/table/customer_group.js
@@ -0,0 +1,68 @@
+/**
+ * Shopware 4.0
+ * Copyright © 2012 shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ *
+ * @category   Shopware
+ * @package    Analytics
+ * @subpackage Country
+ * @copyright  Copyright (c) 2012, shopware AG (http://www.shopware.de)
+ * @version    $Id$
+ * @author shopware AG
+ */
+
+/**
+ * todo@all: Documentation
+ */
+//{namespace name=backend/analytics/view/main}
+//{block name="backend/analytics/view/table/customer_group"}
+Ext.define('Shopware.apps.Analytics.view.table.CustomerGroup', {
+    extend: 'Shopware.apps.Analytics.view.main.Table',
+    alias: 'widget.analytics-table-customer-group',
+
+    initComponent: function() {
+        var me = this;
+
+        me.columns = {
+            items: me.getColumns(),
+            defaults: {
+                flex: 1
+            }
+        };
+
+        me.callParent(arguments);
+    },
+
+    getColumns: function(){
+        return [{
+            dataIndex: 'customerGroup',
+            text: '{s name=table/country/country}Customer group{/s}'
+        }, {
+            xtype: 'numbercolumn',
+            dataIndex: 'amount',
+            text: '{s name=table/country/sales}Sales{/s}'
+        }, {
+            xtype: 'numbercolumn',
+            dataIndex: 'count',
+            text: '{s name=table/country/count}Count{/s}'
+        }];
+    }
+});
+//{/block}
