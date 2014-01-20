182f0da7eee6e60284508997ac6678dd2f9af7a2
Benjamin Cremer <bc@shopware.de>
2014-01-20 08:08:51 +0100

    SW-7191 - Add Request/Response aware FirePHP monolog handler.

---
+++ engine/Shopware/Components/Log/Handler/FirePHPHandler.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Shopware 4
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+namespace Shopware\Components\Log\Handler;
+
+use Enlight_Controller_Request_RequestHttp as Request;
+use Enlight_Controller_Response_ResponseHttp as Response;
+use Monolog\Handler\FirePHPHandler as BaseFirePHPHandler;
+
+/**
+ * FirePHPHandler.
+ *
+ * @category  Shopware
+ * @package   Shopware\Components\Log\Handler
+ * @copyright Copyright (c) shopware AG (http://www.shopware.de)
+ */
+class FirePHPHandler extends BaseFirePHPHandler
+{
+    /**
+     * @var array
+     */
+    private $headers = array();
+
+    /**
+     * @var \Enlight_Controller_Response_ResponseHttp
+     */
+    private $response;
+
+    /**
+     * Adds the headers to the response once it's created
+     */
+    public function onRouteStartUp(\Enlight_Controller_EventArgs $args)
+    {
+        $response = $args->getResponse();
+        $request = $args->getRequest();
+
+        $this->setUp($request, $response);
+    }
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    public function acceptsRequest(Request $request)
+    {
+        $firePhpVersion = $request->getHeader('X-FirePHP-Version');
+        $userAgent      = preg_match('{\bFirePHP/\d+\.\d+\b}', $request->getHeader('User-Agent'));
+
+        return $firePhpVersion || $userAgent;
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     */
+    public function setUp(Request $request, Response $response)
+    {
+        if (!$this->acceptsRequest($request)) {
+            $this->sendHeaders = false;
+            $this->headers = array();
+
+            return;
+        }
+
+        $this->response = $response;
+        foreach ($this->headers as $header => $content) {
+            $this->response->setHeader($header, $content, true);
+        }
+
+        $this->headers = array();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function sendHeader($header, $content)
+    {
+        if (!$this->sendHeaders) {
+            return;
+        }
+
+        if ($this->response) {
+            $this->response->setHeader($header, $content, true);
+        } else {
+            $this->headers[$header] = $content;
+        }
+    }
+
+    /**
+     * Override default behavior since we check the user agent in onKernelResponse
+     */
+    protected function headersAccepted()
+    {
+        return true;
+    }
+}
