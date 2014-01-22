1e45d13c0b3d932d7ac1b8b0b3654ee04533b893
Oliver Denter <dr@shopware.de>
2014-01-22 09:14:33 +0100

    NTR - Fix migration file names.

--- _sql/migrations/222-add-logmail-config.php
+++ _sql/migrations/222-add-logmail-config.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration220 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration222 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
