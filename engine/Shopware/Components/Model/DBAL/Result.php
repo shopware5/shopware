acd50340f7c8db2fe3e21804b455e9970a80199b
Oliver Denter <dr@shopware.de>
2014-01-22 09:35:35 +0100

    SW-6606 - Fix coding styles. Fix migration numbers.

--- engine/Shopware/Components/Model/DBAL/Result.php
+++ engine/Shopware/Components/Model/DBAL/Result.php
@@ -1,9 +1,35 @@
 <?php
+/**
+ * Shopware 4
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 namespace Shopware\Components\Model\DBAL;
 
 use Doctrine\DBAL\Query\QueryBuilder;
 
+/**
+ * Class Result which allows to paginate dbal queries.
+ * @package Shopware\Components\Model\DBAL
+ */
 class Result
 {
     /**
@@ -29,7 +55,7 @@ class Result
     protected $totalCount;
 
     /**
-     * Class constructor which expects the executed pdo statement.
+     * Class constructor which expects the DBAL query builder object
      *
      * @param QueryBuilder $builder
      * @internal param array $data
