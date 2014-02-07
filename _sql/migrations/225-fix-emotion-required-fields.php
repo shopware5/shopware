e69cad23c9277894c4a78d4ccee00d9a4c1a8045
Marcel Schmaeing <ms@shopware.de>
2014-02-07 16:32:35 +0100

    SW-7914 fix mandatory emotion fields

--- _sql/migrations/225-fix-emotion-required-fields.php
+++ _sql/migrations/225-fix-emotion-required-fields.php
@@ -4,13 +4,12 @@ class Migrations_Migration225 Extends Shopware\Components\Migrations\AbstractMig
     public function up()
     {
         $sql = <<<'EOD'
-UPDATE s_library_component_field
-SET allow_blank = '1'
-WHERE name IN (
-    'banner_slider_title',
-    'manufacturer_slider_title',
-    'article_slider_title'
-);
+UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'banner_slider_title' AND componentID = 7;
+UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'manufacturer_slider_title' AND componentID = 10;
+UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'cms_title' AND componentID = 2;
+UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'link' AND componentID = 3;
+UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'article_slider_title' AND componentID = 11;
+
 EOD;
         $this->addSql($sql);
     }
