9062ac5696b4caf020db549342026854e324bdab
Marcel Schmaeing <ms@shopware.de>
2014-02-12 08:43:40 +0100

    SW-7914 fix delta

--- _sql/migrations/225-fix-emotion-required-fields.php
+++ _sql/migrations/225-fix-emotion-required-fields.php
@@ -4,19 +4,19 @@ class Migrations_Migration225 Extends Shopware\Components\Migrations\AbstractMig
     public function up()
     {
         $sql = <<<'EOD'
-SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-banner-slider');
+SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-banner-slider' AND template = 'component_banner_slider' AND pluginID IS NULL LIMIT 1);
 UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'banner_slider_title' AND componentID = @parent;
 
-SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-manufacturer-slider');
+SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-manufacturer-slider' AND template = 'component_manufacturer_slider' AND pluginID IS NULL LIMIT 1);
 UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'manufacturer_slider_title' AND componentID = @parent;
 
-SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-html-element');
+SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-html-element' AND template = 'component_html' AND pluginID IS NULL LIMIT 1);
 UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'cms_title' AND componentID = @parent;
 
-SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-banner');
+SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-banner' AND template = 'component_banner' AND pluginID IS NULL LIMIT 1);
 UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'link' AND componentID = @parent;
 
-SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-article-slider');
+SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-article-slider' AND template = 'component_article_slider' AND pluginID IS NULL LIMIT 1);
 UPDATE s_library_component_field SET allow_blank = '1' WHERE name = 'article_slider_title' AND componentID = @parent;
 
 EOD;
