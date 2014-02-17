e75702c612c95832a4e067c6432e6395368fc259
Oliver Denter <dr@shopware.de>
2014-02-17 11:28:33 +0100

    SW-7836 - Add php documentation.

--- engine/Shopware/Components/File/Manager.php
+++ engine/Shopware/Components/File/Manager.php
@@ -1,18 +1,46 @@
 <?php
+/**
+ * Shopware 4
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 namespace Shopware\Components\File;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\FileBag;
 
+/**
+ * Class to handle file uploads.
+ *
+ * @category  Shopware
+ * @package   Shopware\Components\HttpCache
+ * @copyright Copyright (c) shopware AG (http://www.shopware.de)
+ */
 class Manager
 {
-
     /**
      * Helper function which creates a UploadedFile object
      * for the fileId element in the $_FILES object.
      *
-     * @param array $files Global $_FILES object
+     * @param array $files Global $_FILES array
      * @param $id
      * @throws \Exception
      * @return UploadedFile
