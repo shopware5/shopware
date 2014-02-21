1bfeddac21efabf76e41d5b0bba27ca47d847f4e
Oliver Denter <dr@shopware.de>
2014-02-21 16:11:40 +0100

    SW-8073 - Implement toArray function for the base class.

--- engine/Shopware/Components/Form/Base.php
+++ engine/Shopware/Components/Form/Base.php
@@ -2,8 +2,62 @@
 
 namespace Shopware\Components\Form;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 class Base
 {
+    /**
+     * Outputs the object data as array.
+     * Used to convert a form structure to
+     * a uniform array structure which can be used
+     * from the Shopware\Components\Form\Persister and
+     * Shopware\Components\Form\Hydrator
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $properties = get_class_vars(get_class($this));
+
+        $data = array(
+            'type' => get_class($this)
+        );
+
+        foreach($properties as $property => $value) {
+            $method = 'get' . ucfirst($property);
+
+            if (!method_exists($this, $method)) {
+                continue;
+            }
+
+            $value = $this->$method();
+
+            if (is_array($value) || $value instanceof ArrayCollection) {
+                $converted = array();
+                foreach($value as $item) {
+                    if ($item instanceof Base) {
+                        $converted[] = $item->toArray();
+                    } else {
+                        $converted[] = $item;
+                    }
+                }
+                $value = $converted;
+            }
+
+            $data[$property] = $value;
+        }
+
+
+        return $data;
+    }
+
+    /**
+     * Helper function to set the object
+     * data by array data.
+     *
+     * @param array $array
+     * @return $this
+     */
     public function fromArray(array $array)
     {
         foreach ($array as $key => $value) {
