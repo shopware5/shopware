36dacdb4b2615bcbdf44a5bcc0541cf3276e8d66
Oliver Denter <dr@shopware.de>
2014-02-21 17:34:12 +0100

    SW-8073 - Extends selection field validation. Add licence and class header.

--- engine/Shopware/Components/Form/Base.php
+++ engine/Shopware/Components/Form/Base.php
@@ -1,9 +1,32 @@
 <?php
-
+/**
+ * Shopware 4
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 namespace Shopware\Components\Form;
 
-use Doctrine\Common\Collections\ArrayCollection;
-
+/**
+ * Class Base
+ * @package Shopware\Components\Form
+ */
 class Base
 {
     /**
@@ -31,7 +54,7 @@ class Base
             }
 
             $value = $this->$method();
-            
+
             if ($value instanceof \Traversable) {
                 $converted = array();
                 foreach ($value as $item) {
@@ -66,4 +89,5 @@ class Base
         }
         return $this;
     }
+
 }
--- engine/Shopware/Components/Form/Container.php
+++ engine/Shopware/Components/Form/Container.php
@@ -1,10 +1,36 @@
 <?php
+/**
+ * Shopware 4
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 namespace Shopware\Components\Form;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Shopware\Components\Form\Interfaces\Container as ContainerInterface;
 
+/**
+ * Class Container
+ * @package Shopware\Components\Form
+ */
 class Container extends Base implements ContainerInterface
 {
     /**
