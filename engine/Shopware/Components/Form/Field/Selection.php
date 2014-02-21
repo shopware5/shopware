e66201f173a2d50aa28330481fb58f1356d271d9
Oliver Denter <dr@shopware.de>
2014-02-21 17:28:22 +0100

    SW-8073 - Implement store property for the selection field. Add optional attributes for form fields.§

--- engine/Shopware/Components/Form/Field/Selection.php
+++ engine/Shopware/Components/Form/Field/Selection.php
@@ -7,11 +7,37 @@ use Shopware\Components\Form\Field;
 class Selection extends Field
 {
     /**
+     * Contains the store data for the selection field.
+     *
+     * @var mixed
+     */
+    protected $store;
+
+    /**
      * Requires to set a name for the field
      * @param $name
+     * @param $store
      */
-    function __construct($name)
+    function __construct($name, $store)
     {
         $this->name = $name;
+        $this->store = $store;
     }
+
+    /**
+     * @param mixed $store
+     */
+    public function setStore($store)
+    {
+        $this->store = $store;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStore()
+    {
+        return $this->store;
+    }
+
 }
