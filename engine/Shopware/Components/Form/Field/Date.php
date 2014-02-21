befb1bc2c81d25c50c52769f2d8aecf2d5b68d0b
Oliver Denter <dr@shopware.de>
2014-02-21 15:53:34 +0100

    SW-8073 - Implement field constructor. Add date format property.

--- engine/Shopware/Components/Form/Field/Date.php
+++ engine/Shopware/Components/Form/Field/Date.php
@@ -6,4 +6,35 @@ use Shopware\Components\Form\Field;
 
 class Date extends Field
 {
+
+    /**
+     * Format of the date value.
+     * @var string
+     */
+    private $format;
+
+    /**
+     * Requires to set a name for the field
+     * @param $name
+     */
+    function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $format
+     */
+    public function setFormat($format)
+    {
+        $this->format = $format;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
 }
