8aa7a573a3bf87cda0b489b96256d463a2495c2b
Marcel Schmaeing <ms@shopware.de>
2014-02-24 16:26:58 +0100

    SW-7800 fix licence header

--- templates/_default/backend/canceled_order/view/tabs/order/position.js
+++ templates/_default/backend/canceled_order/view/tabs/order/position.js
@@ -1,6 +1,6 @@
 /**
- * Shopware 4.0
- * Copyright © 2012 shopware AG
+ * Shopware 4
+ * Copyright © shopware AG
  *
  * According to our dual licensing model, this program can be used either
  * under the terms of the GNU Affero General Public License, version 3,
@@ -19,13 +19,6 @@
  * The licensing of the program under the AGPLv3 does not imply a
  * trademark license. Therefore any rights, title and interest in
  * our trademarks remain entirely with us.
- *
- * @category   Shopware
- * @package    CanceledOrder
- * @subpackage View
- * @copyright  Copyright (c) 2012, shopware AG (http://www.shopware.de)
- * @version    $Id$
- * @author shopware AG
  */
 
 //{namespace name=backend/canceled_order/view/main}
