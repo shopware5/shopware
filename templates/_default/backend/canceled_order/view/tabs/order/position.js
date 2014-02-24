41ce688be52809ef64789a9faf8515bd673b12ca
Marcel Schmaeing <ms@shopware.de>
2014-02-24 16:22:47 +0100

    SW-7800 adding order position grid

--- templates/_default/backend/canceled_order/view/tabs/order/position.js
+++ templates/_default/backend/canceled_order/view/tabs/order/position.js
@@ -35,11 +35,25 @@
  */
 //{block name="backend/canceled_order/view/tabs/order/position"}
 Ext.define('Shopware.apps.CanceledOrder.view.tabs.order.Position', {
-    extend: 'Ext.panel.Panel',
-    title: 'Order positions',
+    extend: 'Ext.grid.Panel',
+    title: '{s name=position/title}Order positions{/s}',
     region: 'south',
     height: 300,
+    collapsed: false,
+    collapsible: true,
     alias: 'widget.canceled-order-view-order-position',
+
+    snippets : {
+        columns : {
+            articleNumber: '{s name=columns/position/number}Articlenumber{/s}',
+            articleName: '{s name=columns/position/name}Articlename{/s}',
+            articleQuantity: '{s name=columns/position/quantity}Quantity{/s}',
+            articlePrice: '{s name=columns/position/price}Price{/s}',
+            totalAmount: '{s name=columns/position/total_amount}Total Amount{/s}',
+            openArticle: '{s name=column/position/open_article}Show article{/s}'
+
+        }
+    },
 
     /**
      * Init the main detail component, add components
@@ -47,90 +61,87 @@ Ext.define('Shopware.apps.CanceledOrder.view.tabs.order.Position', {
      */
     initComponent: function() {
         var me = this;
-
-        me.items = [ me.createContainer() ];
-        me.addEvents( 'askForReason' );
+        me.columns = me.getColumns();
+        // register events
+        me.addEvents( 'openArticle' );
         me.callParent(arguments);
     },
 
     /**
-     * Creates the main container, sets layout and adds the components needed
-     * @return Ext.container.Container
+     /**
+     * Creates the grid columns
+     * Data indices where chosen in order to match the database scheme for sorting in the PHP backend.
+     * Therefore each Column requieres its own renderer in order to display the correct value.
+     *
+     * @return [array] grid columns
      */
-    createContainer: function() {
+    getColumns: function() {
         var me = this;
 
-        return Ext.create('Ext.container.Container', {
-            border: false,
-            padding: 10,
-            layout: {
-                type: 'vbox',
-                align : 'stretch',
-                pack  : 'start'
+        return [
+            {
+                header: me.snippets.columns.articleNumber,
+                dataIndex: 'articleNumber',
+                flex: 1
             },
-            items: [
-                me.createInfoText(),
-                me.createVoucherCombo(),
-                me.createAskReasonButton()
-            ]
-        });
-    },
+            {
+                header: me.snippets.columns.articleName,
+                dataIndex: 'articleName',
+                flex: 1
+            },
+            {
+                header: me.snippets.columns.articleQuantity,
+                dataIndex: 'quantity',
+                flex: 1
+            },
+            {
+                header: me.snippets.columns.articlePrice,
+                dataIndex: 'price',
+                flex: 2,
+                renderer: me.priceColumn
 
-    /**
-     * Creates and returns a simple label which will later inform the user about the possible options (voucher / mail)
-     * @return Ext.form.Label
-     */
-    createInfoText: function()  {
-        var me = this;
+            },
+            {
+                header: me.snippets.columns.totalAmount,
+                dataIndex: 'total',
+                flex: 1,
+                renderer: me.priceColumn
+            },
+            {
+                xtype:'actioncolumn',
+                width:90,
+                items:[
+                    {
+                        iconCls:'sprite-inbox',
+                        action:'openArticle',
+                        tooltip: me.snippets.openArticle,
+                        handler:function (view, rowIndex) {
+                            var store = view.getStore(),
+                                record = store.getAt(rowIndex);
 
-        me.infoLabel = Ext.create('Ext.form.Label', {
-            text: '{s name=detailsNoOrderSelected}No order selected{/s}',
-            padding: '0 0 20 0'
-        });
-        return me.infoLabel;
-    },
-
-    /**
-     * Creates and returns a button used to send 'ask for reason' mails
-     * @return Ext.button.Button
-     */
-    createAskReasonButton: function() {
-        var me = this;
-
-        me.askReasonButton = Ext.create('Ext.button.Button', {
-            text: '{s name=askForReason}Ask for reason{/s}',
-            hidden: true,
-            handler: function () {
-                me.fireEvent('askForReason');
+                            me.fireEvent('openArticle', record);
+                        },
+                        getClass: function(value, metadata, record) {
+                            if (!record.get('articleId') || record.get('mode') != '0') {
+                                return 'x-hidden';
+                            }
+                        }
+                    }
+                ]
             }
-                });
-        return me.askReasonButton;
+        ];
     },
 
     /**
-     * Creates and returns the voucher combo. It will contain the available vouchers
-     * @return Ext.form.ComboBox
+     * Formats the price column
+     * @param [string] - The price value
+     * @return [string] - The passed value, formatted with Ext.util.Format.currency()
      */
-    createVoucherCombo: function() {
-        var me = this;
-
-        me.voucherCombo = Ext.create('Ext.form.ComboBox', {
-            store: me.canceledOrderVoucher,
-            fieldLabel: '{s name=sendVoucher}Send Voucher{/s}',
-            displayField: 'value',
-            valueField: 'id',
-            emptyText: '{s name=selectVoucher}Select Voucher{/s}',
-            editable: false,
-            hidden: true,
-            listeners: {
-                select: function(combo, record) {
-                    me.fireEvent('sendVoucher', combo, record);
-                }
-            }
-
-        });
-
-        return me.voucherCombo;
+    priceColumn:function (value) {
+        if ( value === Ext.undefined ) {
+            return value;
+        }
+        return Ext.util.Format.currency(value);
     }
 });
 //{/block}
