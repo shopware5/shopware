8bd4d42793dc8c6b0def5e952fac2ba17ed1c7c1
t.garcia <tg@shopware.de>
2014-03-03 10:17:17 +0100

    SW-8129 - sNewsletter refactoring

--- engine/core/class/sNewsletter.php
+++ engine/core/class/sNewsletter.php
@@ -27,99 +27,164 @@
  */
 class sNewsletter
 {
+    /**
+     * @var sSystem
+     */
     public $sSYSTEM;
 
-        public function sCampaignsGetSuggestions($id,$userid=0)
-        {
-            $id = intval($id);
+    /**
+     * Database connection which used for each database operation in this class.
+     * Injected over the class constructor
+     *
+     * @var Enlight_Components_Db_Adapter_Pdo_Mysql
+     */
+    private $db;
 
-            $sql = "
+    /**
+     * Shopware configuration object which used for
+     * each config access in this class.
+     * Injected over the class constructor
+     *
+     * @var Shopware_Components_Config
+     */
+    private $config;
+
+    /**
+     * Class constructor.
+     * Injects all dependencies which are required for this class.
+     */
+    public function __construct()
+    {
+        $this->db = Shopware()->Db();
+        $this->config = Shopware()->Config();
+    }
+
+    /**
+     * Gets article suggestions
+     *
+     * @param $id
+     * @param int $userId
+     * @return null|array
+     */
+    public function sCampaignsGetSuggestions($id, $userId = 0)
+    {
+        $sql = "
             SELECT value, description FROM s_campaigns_containers WHERE type='ctSuggest'
-            AND promotionID=$id
-            ";
-            unset($this->sSYSTEM->sMODULES['sArticles']->sCachePromotions);
-            unset($this->sSYSTEM->sMODULES['sMarketing']->sBlacklist);
+            AND promotionID = ?
+        ";
+        unset($this->sSYSTEM->sMODULES['sArticles']->sCachePromotions);
+        unset($this->sSYSTEM->sMODULES['sMarketing']->sBlacklist);
 
-            $getSuggestInfo = $this->sSYSTEM->sDB_CONNECTION->CacheGetRow($sql);
-            if ($getSuggestInfo["value"] && $getSuggestInfo["description"]) {
-                // Main-Information
-                $sSuggestion["description"] = $getSuggestInfo["description"];
-                $sSuggestion["value"] = $getSuggestInfo["value"];
-                // Get personalized articles
+        $getSuggestInfo = $this->sSYSTEM->sDB_CONNECTION->CacheGetRow($sql, null, array(intval($id)));
+        if ($getSuggestInfo["value"] && $getSuggestInfo["description"]) {
+            // Main information
+            $sSuggestion["description"] = $getSuggestInfo["description"];
+            $sSuggestion["value"] = $getSuggestInfo["value"];
 
-                $limit = intval($sSuggestion["value"] / 2);
-                if ($userid) {
+            // Get personalized articles
+            $limit = intval($sSuggestion["value"] / 2);
+            if ($userId) {
 
-                    // 1.) Get last viewed articles
-                    $sql = "
-                    SELECT DISTINCT articleID FROM s_emarketing_lastarticles WHERE userID=$userid
-                    ORDER BY time DESC LIMIT $limit
-                    ";
+                $selectLast = array_merge(
+                    $this->getLastViewedArticles($userId, $limit),
+                    $this->getLastBoughtArticles($userId, $limit)
+                );
 
-                    $selectLast = $this->sSYSTEM->sDB_CONNECTION->GetAll($sql);
-                    $countLimit = $limit - count($selectLast);
-                    $this->sSYSTEM->sCONFIG['sMAXCROSSSIMILAR'] = 1;
-                    foreach ($selectLast as $lastArticle) $this->sSYSTEM->sMODULES['sMarketing']->sBlacklist[] = $lastArticle["articleID"];
-                    foreach ($selectLast as $lastArticle) {
-                        $temp = $this->sSYSTEM->sMODULES['sMarketing']->sGetSimilaryShownArticles($lastArticle["articleID"]);
-                        if ($temp[0]["id"]) {
-                            $selectLastAlsoView[]["articleID"] = $temp[0]["id"];
-                        }
-                    }
-                    if (count($selectLastAlsoView)) $selectLast = array_merge($selectLast,$selectLastAlsoView);
-                    // 2.) Get last bought articles
-                    $sql = "
-                    SELECT DISTINCT articleID FROM s_order_details, s_order WHERE
-                    s_order.userID=$userid
-                    AND s_order_details.orderID = s_order.id
-                    ORDER BY ordertime DESC LIMIT $limit
-                    ";
+                $blacklist = array();
 
-                    $selectLastOrders = $this->sSYSTEM->sDB_CONNECTION->GetAll($sql);
-                    foreach ($selectLastOrders as $lastArticle) $this->sSYSTEM->sMODULES['sMarketing']->sBlacklist[] = $lastArticle["articleID"];
-                    foreach ($selectLastOrders as $lastArticle) {
-                        $temp = $this->sSYSTEM->sMODULES['sMarketing']->sGetAlsoBoughtArticles($lastArticle["articleID"]);
-                        if ($temp[0]["id"]) {
-                            $selectLastAlsoBought[]["articleID"] = $temp[0]["id"];
-                        }
-                    }
-                    if (count($selectLastAlsoBought)) $selectLast = array_merge($selectLast,$selectLastAlsoBought);
-
-                    $blacklist = array();
-
-                    $countRecommendations = count($selectLast);
-                    if ($countRecommendations) {
-                        foreach ($selectLast as $lastArticle) {
-                            $category = $this->sSYSTEM->_GET["sCategory"] ? $this->sSYSTEM->_GET["sCategory"] : 0;
-                            $temp = $this->sSYSTEM->sMODULES['sArticles']->sGetPromotionById("fix",$category,$lastArticle["articleID"]);
-                            if ($temp["articleID"] && empty($blacklist[$temp["articleID"]])) {
-                                //$this->sSYSTEM->sMODULES['sArticles']->sCachePromotions[] = $temp["articleID"];	// Refresh Blacklist
-                                $finalRecommendations[] = $temp;
-                                $blacklist[$temp["articleID"]] = $temp["articleID"];
-                            }
-                        }
+                foreach ($selectLast as $lastArticle) {
+                    $category = $this->sSYSTEM->_GET["sCategory"] ? : 0;
+                    $temp = $this->sSYSTEM->sMODULES['sArticles']->sGetPromotionById("fix", $category, $lastArticle["articleID"]);
+                    if ($temp["articleID"] && empty($blacklist[$temp["articleID"]])) {
+                        $finalRecommendations[] = $temp;
+                        $blacklist[$temp["articleID"]] = $temp["articleID"];
                     }
                 }
+            }
 
-                $leftRecommendations = $sSuggestion["value"] - count($finalRecommendations);
+            $leftRecommendations = $sSuggestion["value"] - count($finalRecommendations);
 
-                $randomize = array('new', 'top');
-                $category = $this->sSYSTEM->_GET['sCategory'] ? $this->sSYSTEM->_GET['sCategory'] : 0;
+            $randomize = array('new', 'top');
+            $category = $this->sSYSTEM->_GET['sCategory'] ? : 0;
 
-                while ($leftRecommendations>0) {
-                    $article = $this->sSYSTEM->sMODULES['sArticles']->sGetPromotionById($randomize[array_rand($randomize)], $category, '');
-                    if (!empty($article)) {
-                        $leftRecommendations--;
-                        $this->sSYSTEM->sMODULES['sArticles']->sCachePromotions[] = $article['articleID'];
-                        $finalRecommendations[] = $article;
-                    }
+            while ($leftRecommendations > 0) {
+                $article = $this->sSYSTEM->sMODULES['sArticles']->sGetPromotionById($randomize[array_rand($randomize)], $category, '');
+                if (!empty($article)) {
+                    $leftRecommendations--;
+                    $this->sSYSTEM->sMODULES['sArticles']->sCachePromotions[] = $article['articleID'];
+                    $finalRecommendations[] = $article;
                 }
+            }
 
-                $sSuggestion["data"] = $finalRecommendations;
+            $sSuggestion["data"] = $finalRecommendations;
 
-                return $sSuggestion;
-
+            return $sSuggestion;
         }
+        return null;
+    }
+
+    /**
+     * Get last viewed articles
+     *
+     * @param $userId
+     * @param $limit
+     *
+     * @return array
+     */
+    private function getLastViewedArticles($userId, $limit)
+    {
+        $sql = "
+            SELECT DISTINCT articleID FROM s_emarketing_lastarticles WHERE userID = ?
+            ORDER BY time DESC LIMIT $limit
+        ";
+
+        $lastViewedArticles = $this->db->fetchAll($sql, array($userId));
+
+        $this->config->offsetSet('sMAXCROSSSIMILAR', 1);
+
+        foreach ($lastViewedArticles as $lastArticle) {
+            $this->sSYSTEM->sMODULES['sMarketing']->sBlacklist[] = $lastArticle["articleID"];
+        }
+
+        $selectLastAlsoView = array();
+        foreach ($lastViewedArticles as $lastArticle) {
+            $temp = $this->sSYSTEM->sMODULES['sMarketing']->sGetSimilaryShownArticles($lastArticle["articleID"]);
+            if ($temp[0]["id"]) {
+                $selectLastAlsoView[]["articleID"] = $temp[0]["id"];
+            }
+        }
+
+        return array_merge($lastViewedArticles, $selectLastAlsoView);
+    }
+
+    /**
+     * Get last bought articles
+     *
+     * @param $userId
+     * @param $limit
+     *
+     * @return array
+     */
+    private function getLastBoughtArticles($userId, $limit)
+    {
+        $sql = "
+            SELECT DISTINCT articleID FROM s_order_details, s_order WHERE
+            s_order.userID = ?
+            AND s_order_details.orderID = s_order.id
+            ORDER BY ordertime DESC LIMIT $limit
+        ";
+
+        $selectLastOrders = $this->db->fetchAll($sql, array($userId));
+        foreach ($selectLastOrders as $lastArticle) {
+            $this->sSYSTEM->sMODULES['sMarketing']->sBlacklist[] = $lastArticle["articleID"];
+        }
+        foreach ($selectLastOrders as $lastArticle) {
+            $temp = $this->sSYSTEM->sMODULES['sMarketing']->sGetAlsoBoughtArticles($lastArticle["articleID"]);
+            if ($temp[0]["id"]) {
+                $selectLastAlsoBought[]["articleID"] = $temp[0]["id"];
+            }
+        }
+        return $selectLastAlsoBought;
     }
 
 }
