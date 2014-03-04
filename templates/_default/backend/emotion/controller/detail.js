8c69a5a2d8877ebeb03363b2cf38ae71b1bca113
Dieter Giesler <dg@shopware.com>
2014-03-04 12:57:02 +0100

    SW-7386 - Fixed the saving process of emotion banner mapping so they don't get lost when the mapping edit was cancelled

--- templates/_default/backend/emotion/controller/detail.js
+++ templates/_default/backend/emotion/controller/detail.js
@@ -205,17 +205,30 @@ Ext.define('Shopware.apps.Emotion.controller.Detail', {
                 data.push(me.getFieldData(field, record));
             }
         });
+        console.log('data', data);
         record.set('data', data);
         win.destroy();
     },
 
     getFieldData: function(field, record) {
         if (field.getName() === 'bannerMapping') {
+            var recordData = record.get('data'),
+                mapping = record.get('mapping');
+
+            if(!mapping) {
+                Ext.each(recordData, function(el) {
+                    if(el.key === 'bannerMapping') {
+                        mapping = el.value;
+                        return false;
+                    }
+                });
+            }
+
             return {
                 id: field.fieldId,
                 type: field.valueType,
                 key: field.getName(),
-                value: record.get('mapping')
+                value: mapping
             };
         } else if(field.getName() === 'banner_slider') {
             return {
@@ -366,7 +379,6 @@ Ext.define('Shopware.apps.Emotion.controller.Detail', {
 
         element.set('mapping', mapping);
         view.destroy();
-
     },
 
     onDuplicateEmotion: function(scope, record) {
