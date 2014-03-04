a49f11590c1fab7849807cb1448e54837c2d6cb4
Oliver Denter <dr@shopware.de>
2014-03-04 16:35:10 +0100

    SW-8057 - Implement minify/CSSmin and minify/JSmin classes for css and js minify. Refactor template inheritance. Optimize less compiling, each theme has now his own compiling process. Implement smarty functions to include compiled less, css and javascript files. Add PluginLess class which allows plugin to compile their own plugin less directories.  Remove debug sources from the Theme.php files.

---
+++ engine/Library/Enlight/Template/Plugins/function.less.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Enlight
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://enlight.de/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@shopware.de so we can send you a copy immediately.
+ *
+ * @category   Enlight
+ * @package    Enlight_Template_Plugins
+ * @copyright  Copyright (c) 2011, shopware AG (http://www.shopware.de)
+ * @license    http://enlight.de/license     New BSD License
+ * @version    $Id$
+ * @author     Heiner Lohaus
+ * @author     $Author$
+ */
+
+/**
+ * @param $params
+ * @param $template
+ * @return string
+ * @throws Exception
+ *
+ */
+function smarty_function_less($params, $template)
+{
+    $file = $params['file'];
+    $time = $params['timestamp'];
+    
+    /**@var $pathResolver \Shopware\Components\Theme\PathResolver*/
+    $pathResolver = Shopware()->Container()->get('theme_path_resolver');
+
+    /**@var $shop \Shopware\Models\Shop\Shop*/
+    $shop = Shopware()->Container()->get('shop');
+
+    $cssFile = $pathResolver->buildCssPath($shop, $file, $time);
+
+    $fileName = $pathResolver->buildCssName($shop, $file, $time);
+
+    $url = $shop->getBasePath() .
+        DIRECTORY_SEPARATOR .
+        $pathResolver->getCacheDirectoryUrl() .
+        DIRECTORY_SEPARATOR .
+        $fileName;
+    
+    if (file_exists($cssFile)) {
+        return $url;
+    }
+
+    /**@var $compiler \Shopware\Components\Theme\Compiler*/
+    $compiler = Shopware()->Container()->get('theme_compiler');
+
+    $compiler->compile($time, $shop->getTemplate(), $shop);
+
+    return $url;
+}
