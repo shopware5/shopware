c8934817d2c6762fae7d1874db9c1e8e97c4cb64
Oliver Denter <dr@shopware.de>
2014-03-05 15:02:30 +0100

    SW-8057 - Add license header.

--- engine/Shopware/Components/Theme/Configurator.php
+++ engine/Shopware/Components/Theme/Configurator.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Shopware 4
- * Copyright Â© shopware AG
+ * Copyright © shopware AG
  *
  * According to our dual licensing model, this program can be used either
  * under the terms of the GNU Affero General Public License, version 3,
@@ -21,7 +21,6 @@
  * trademark license. Therefore any rights, title and interest in
  * our trademarks remain entirely with us.
  */
-
 namespace Shopware\Components\Theme;
 
 use Doctrine\Common\Collections\ArrayCollection;
--- engine/Shopware/Models/Shop/TemplateConfig/Value.php
+++ engine/Shopware/Models/Shop/TemplateConfig/Value.php
@@ -21,7 +21,6 @@
  * trademark license. Therefore any rights, title and interest in
  * our trademarks remain entirely with us.
  */
-
 namespace Shopware\Models\Shop\TemplateConfig;
 
 use Doctrine\Common\Collections\ArrayCollection;
