265033ed6d2d831d4d7ebbacae24c5710f99b4de
Tino Vogel <tv@shopware.com>
2014-04-03 08:24:19 +0200

    SW-7764 - Initial commit

--- templates/_default/backend/base/component/element/select.js
+++ templates/_default/backend/base/component/element/select.js
@@ -45,6 +45,9 @@ Ext.define('Shopware.apps.Base.view.element.Select', {
     initComponent:function () {
         var me = this;
 
+        alert(me.store);
+
+
         if (me.controller && me.action) {
             //me.value = parseInt(me.value);
             me.store = new Ext.data.Store({
