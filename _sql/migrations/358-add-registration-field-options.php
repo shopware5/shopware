4842e6fe427de09c460a3702bb9b8fee6dcef4af
Tino Vogel <tv@shopware.com>
2014-04-07 11:36:35 +0200

    SW-7845 - Applied template changes to bare template

--- _sql/migrations/358-add-registration-field-options.php
+++ _sql/migrations/358-add-registration-field-options.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration232 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration358 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
