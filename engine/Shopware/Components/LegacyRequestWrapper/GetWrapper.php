bb6bc174fa03f6bb391413d75a57cdf289e89a8a
t.garcia <tg@shopware.de>
2014-04-10 09:50:40 +0200

    SW-8326 - Docs

--- engine/Shopware/Components/LegacyRequestWrapper/GetWrapper.php
+++ engine/Shopware/Components/LegacyRequestWrapper/GetWrapper.php
@@ -1,11 +1,39 @@
 <?php
+/**
+ * Shopware 4
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 namespace Shopware\Components\LegacyRequestWrapper;
 
 class GetWrapper implements \ArrayAccess
 {
+    /**
+     * @var \Enlight_Controller_Request_RequestHttp
+     */
     private $request;
 
+    /**
+     * @param \Enlight_Controller_Request_RequestHttp $request The request object
+     */
     public function __construct(\Enlight_Controller_Request_RequestHttp $request)
     {
         $this->request = $request;
@@ -74,11 +102,21 @@ class GetWrapper implements \ArrayAccess
         $this->request->setQuery($offset, null);
     }
 
+    /**
+     * Replace all _GET values with provided values
+     *
+     * @param array $values
+     */
     public function setAll($values)
     {
         $this->request->setQuery($values);
     }
 
+    /**
+     * Returns an array with all current values in _GET
+     *
+     * @return array
+     */
     public function toArray()
     {
         return $this->request->getQuery();
