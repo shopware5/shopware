646748e963ecd9be3c2375a7ccd732caa925d578
Tino Vogel <tv@shopware.com>
2014-04-10 11:56:16 +0200

    SW-8182 - Sitemap Controller Refactoring, SQL-Migration: columns have now default value

--- _sql/migrations/357-add-supplier-and-site-changed-column.php
+++ _sql/migrations/357-add-supplier-and-site-changed-column.php
@@ -3,10 +3,14 @@ class Migrations_Migration357 Extends Shopware\Components\Migrations\AbstractMig
 {
     public function up()
     {
-        $sql = <<<'EOD'
-            ALTER TABLE  `s_articles_supplier` ADD  `changed` DATETIME NOT NULL ;
-            ALTER TABLE  `s_cms_static` ADD  `changed` DATETIME NOT NULL ;
+        $dateTime = new DateTime();
+        $date = $dateTime->format('Y-m-d H:i:s');
+
+        $sql = <<<EOD
+            ALTER TABLE  `s_articles_supplier` ADD  `changed` DATETIME NOT NULL DEFAULT "$date";
+            ALTER TABLE  `s_cms_static` ADD  `changed` DATETIME NOT NULL DEFAULT "$date";
 
 EOD;
-        $this->addSql($sql);    }
+        $this->addSql($sql);
+    }
 }
