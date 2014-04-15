fc2ac088afd005e35a7a3370164455261dc6a76f
Tino Vogel <tv@shopware.com>
2014-04-15 16:01:05 +0200

    SW-8182 - Removed SQL AFTER from migration file

--- _sql/migrations/357-add-changed-and-mobileactive-columns.php
+++ _sql/migrations/357-add-changed-and-mobileactive-columns.php
@@ -9,7 +9,7 @@ class Migrations_Migration357 Extends Shopware\Components\Migrations\AbstractMig
         $sql = <<<EOD
             ALTER TABLE  `s_articles_supplier` ADD  `changed` DATETIME NOT NULL DEFAULT "$date";
             ALTER TABLE  `s_cms_static` ADD  `changed` DATETIME NOT NULL DEFAULT "$date";
-            ALTER TABLE  `s_core_paymentmeans` ADD  `mobileactive` INT( 1 ) NOT NULL DEFAULT 0 AFTER  `esdactive`;
+            ALTER TABLE  `s_core_paymentmeans` ADD  `mobileactive` INT( 1 ) NOT NULL DEFAULT 0;
 
 EOD;
         $this->addSql($sql);
