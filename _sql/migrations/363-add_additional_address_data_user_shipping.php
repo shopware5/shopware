11ce6803da05edc90862de2b7f83f76fac4f6ba0
Marcel Schmäing <ms@shopware.com>
2014-04-24 10:48:47 +0200

    SW-7715 change migration number

--- _sql/migrations/363-add_additional_address_data_user_shipping.php
+++ _sql/migrations/363-add_additional_address_data_user_shipping.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration358 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration363 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
