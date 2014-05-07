c4e6815c5fb95139483e507308b35a85fb6a87f2
Marcel Schmäing <ms@shopware.com>
2014-05-07 11:07:58 +0200

    SW-8360 fix migration number

--- _sql/migrations/305-add-product-feed-caching.php
+++ _sql/migrations/305-add-product-feed-caching.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration303 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration305 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
