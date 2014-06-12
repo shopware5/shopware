1490883b472e955beb956a660b4817c59d41e9d4
Dieter Giesler <dg@shopware.com>
2014-06-12 10:40:09 +0200

    SW-8569 - Fixed: Replaced loading indicator icon - Fixed: Loading indicator now updates correctly

--- engine/Shopware/Themes/Frontend/Responsive/frontend/_public/src/js/jquery.overlay.js
+++ engine/Shopware/Themes/Frontend/Responsive/frontend/_public/src/js/jquery.overlay.js
@@ -1,9 +1,28 @@
 ;(function ($) {
     "use strict";
 
+    /**
+     * jQuery overlay component.
+     * Displays/Hides a fullscreen overlay with a certain color and opacity.
+     *
+     * @type {Object}
+     */
     $.overlay = {
+        /**
+         * The overlay jQuery element.
+         * Will be created when opening the overlay.
+         *
+         * @type {Null|jQuery}
+         * @private
+         */
         $overlay: null,
 
+        /**
+         * The default options for the overlay.
+         * When certain options were not passed, these will be used instead.
+         *
+         * @type {Object}
+         */
         defaults: {
             color: '#555555',
             opacity: 0.8,
@@ -11,8 +30,19 @@
             closeOnClick: false
         },
 
+        /**
+         * The extended options for the current opened overlay.
+         *
+         * @type {Object}
+         */
         options: {},
 
+        /**
+         * Opens/Shows the fullscreen overlay element.
+         * If it's not available, it will be created and appended to the document body.
+         *
+         * @param {Object} options
+         */
         open: function (options) {
             var me = this;
 
@@ -31,6 +61,9 @@
             me.$overlay.fadeIn(me.options.animationSpeed);
         },
 
+        /**
+         * Closes/Hides the overlay element when it's available
+         */
         close: function () {
             var me = this;
 
@@ -39,6 +72,34 @@
             }
         },
 
+        /**
+         * Adds an event listener to the overlay element.
+         *
+         * @param {String} event
+         * @param {Function} func
+         */
+        addListener: function (event, func) {
+            var me = this;
+
+            me.$overlay.on(event, func.bind(me));
+        },
+
+        /**
+         * Removes an event listener from the overlay element.
+         *
+         * @param {String} event
+         */
+        removeListener: function (event) {
+            var me = this;
+
+            me.$overlay.off(event);
+        },
+
+        /**
+         * Updates the overlay by setting new style options and listeners.
+         *
+         * @private
+         */
         _updateOverlay: function () {
             var me = this,
                 opts = me.options;
@@ -49,10 +110,10 @@
                 cursor: opts.closeOnClick ? 'pointer' : 'default'
             });
 
+            me.removeListener('click.overlay');
+
             if (opts.closeOnClick) {
-                me.$overlay.on('click', me.close.bind(me));
-            } else {
-                me.$overlay.off('click');
+                me.addListener('click.overlay', me.close.bind(me));
             }
         }
     };
