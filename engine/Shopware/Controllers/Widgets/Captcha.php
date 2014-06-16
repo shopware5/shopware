90776c1de49faeabfaa4b7a56e8143d4034b8dcd
Ralf Hoefer <rh@shopware.com>
2014-06-16 09:03:13 +0200

    SW-7890 Fixed: Indentation

--- engine/Shopware/Controllers/Widgets/Captcha.php
+++ engine/Shopware/Controllers/Widgets/Captcha.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Shopware 4
- * Copyright Â© shopware AG
+ * Copyright © shopware AG
  *
  * According to our dual licensing model, this program can be used either
  * under the terms of the GNU Affero General Public License, version 3,
