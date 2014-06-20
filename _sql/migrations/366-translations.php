6041d38294310c637c76042af4202d06245165ae
Tiago <tg@shopware.com>
2014-06-20 11:56:15 +0200

    SW-7389 - Rename migration file

--- _sql/migrations/366-translations.php
+++ _sql/migrations/366-translations.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration365 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration366 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
