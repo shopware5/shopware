950e76f2307140a0550fec734b36f9a9490ba1f7
Ralf Hoefer <rh@shopware.com>
2014-06-24 17:36:53 +0200

    SW-7892 Added: Newsletter jQuery Plugin

---
+++ engine/Shopware/Themes/Frontend/Responsive/frontend/_public/src/js/jquery.newsletter.js
@@ -0,0 +1,31 @@
+;(function ($) {
+    "use strict";
+
+    $.plugin('newsletter', {
+	init: function () {
+	    var me = this;
+
+	    me.$checkMail = me.$el.find('#newsletter--checkmail');
+	    me.$addionalForm = me.$el.find('.newsletter--additional-form');
+
+	    me._on(me.$checkMail, 'change', $.proxy(me.refreshAction, me));
+	    me.$checkMail.trigger('change');
+	},
+
+	refreshAction: function (event) {
+	    var me = this,
+		$el = $(event.currentTarget),
+		val = $el.val();
+
+	    if(val == -1) {
+		me.$addionalForm.hide();
+	    } else {
+		me.$addionalForm.show();
+	    }
+	},
+
+	destroy: function () {
+	    this._destroy();
+	}
+    });
+}( jQuery ));
