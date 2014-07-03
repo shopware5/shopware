9b01953ec1b4071ff92b20a3169ea67182fb6933
Tino Vogel <tv@shopware.com>
2014-07-03 17:38:01 +0200

    SW-8637 - Converted failed Mink-Test for responsive template

--- tests/Mink/features/bootstrap/Page/Responsive/Account.php
+++ tests/Mink/features/bootstrap/Page/Responsive/Account.php
@@ -17,4 +17,85 @@ class Account extends \Emotion\Account
     {
 
     }
+
+    public function checkOrder($orderNumber, $articles, $position = 1)
+    {
+        $this->open();
+
+        $this->clickLink('Meine Bestellungen');
+
+        $locator_prefix = sprintf(
+            'div.account--orders-overview > div.panel--table > div:nth-of-type(%d)',
+            $position * 2 + 1
+        );
+
+        $locators = array();
+        $check = array();
+        $esd = array();
+
+        //Check positions
+        foreach ($articles as $key => $article) {
+            $locators['name' . $key] = sprintf(
+                '%s > div.panel--tr:nth-of-type(%d) .order--name',
+                $locator_prefix,
+                $key + 2
+            );
+            $locators['quantity' . $key] = sprintf(
+                '%s > div.panel--tr:nth-of-type(%d) > div:nth-of-type(2)',
+                $locator_prefix,
+                $key + 2
+            );
+            $locators['price' . $key] = sprintf(
+                '%s > div.panel--tr:nth-of-type(%d) > div:nth-of-type(3)',
+                $locator_prefix,
+                $key + 2
+            );
+            $locators['sum' . $key] = sprintf(
+                '%s > div.panel--tr:nth-of-type(%d) > div:nth-of-type(4)',
+                $locator_prefix,
+                $key + 2
+            );
+
+            $check['name' . $key] = array('', $article['product']);
+            $check['quantity' . $key] = array('', $article['quantity']);
+            $check['price' . $key] = array('', $article['price']);
+            $check['sum' . $key] = array('', $article['sum']);
+
+            if (!empty($article['esd'])) {
+                $esd[] = $article['product'];
+            }
+        }
+
+        $locators['orderDate'] = $locator_prefix . ' > div.is--odd > div.column--info-data > p:nth-of-type(1)';
+        $locators['orderNumber'] = $locator_prefix . ' > div.is--odd > div.column--info-data > p:nth-of-type(2)';
+
+        $check['orderNumber'] = array('', $orderNumber);
+
+        $elements = \Helper::findElements($this, null, $locators);
+
+        foreach ($check as $key => &$checkStep) {
+            $checkStep[0] = $elements[$key]->getText();
+
+            if (strpos($key, 'name') === false) {
+                $checkStep = \Helper::toFloat($checkStep);
+            }
+        }
+
+        if (!empty($esd)) {
+            $date = $elements['orderDate']->getText();
+
+            $downloads = $this->getEsdArray($date);
+
+            foreach ($downloads as $key => $download) {
+                $check['esd' . $key] = array($download, $date .' '. $esd[$key]);
+            }
+        }
+
+        $result = \Helper::checkArray($check);
+
+        if ($result !== true) {
+            $message = sprintf('There was a different value of the order! (%s: %s instead of %s)', $result, $check[$result][0], $check[$result][1]);
+            \Helper::throwException(array($message));
+        }
+    }
 }
