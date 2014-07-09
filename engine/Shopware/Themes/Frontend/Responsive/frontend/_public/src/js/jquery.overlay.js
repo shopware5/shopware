2cbd026a0c3050c483cddfb0d52e25d59a9690e0
Dieter Giesler <dg@shopware.com>
2014-07-09 16:40:37 +0200

    SW-8569 - Refactored the autosubmit, captcha and collapse panel plugin to the new base and added documentation (cherry picked from commit 5df9c74)

--- engine/Shopware/Themes/Frontend/Responsive/frontend/_public/src/js/jquery.overlay.js
+++ engine/Shopware/Themes/Frontend/Responsive/frontend/_public/src/js/jquery.overlay.js
@@ -1,9 +1,24 @@
-;(function ($) {
-    "use strict";
+;
+(function ($, modernizr) {
+    'use strict';
 
     /**
-     * jQuery overlay component.
+     * Shopware Overlay Module.
+     *
      * Displays/Hides a fullscreen overlay with a certain color and opacity.
+     *
+     * @example
+     *
+     * Open th overlay:
+     *
+     * $.overlay.open({
+     *     color: '#FF0000' //red
+     *     opacity: 0.5
+     * });
+     *
+     * Closing the overlay:
+     *
+     * $.overlay.close();
      *
      * @type {Object}
      */
@@ -12,35 +27,73 @@
          * The overlay jQuery element.
          * Will be created when opening the overlay.
          *
-         * @type {Null|jQuery}
          * @private
+         * @property _$overlay
+         * @type {jQuery}
          */
-        $overlay: null,
+        _$overlay: null,
 
         /**
          * The default options for the overlay.
          * When certain options were not passed, these will be used instead.
          *
+         * @public
+         * @property defaults
          * @type {Object}
          */
         defaults: {
-            color: '#555555',
+            /**
+             * Override for the overlay color.
+             * If no color is passed, the default template overlay color will be used.
+             *
+             * @type {String}
+             */
+            color: '',
+
+            /**
+             * The overlay alpha value ranges from 0 to 1.
+             *
+             * @type {Number}
+             */
             opacity: 0.8,
+
+            /**
+             * The animation duration for all animations.
+             *
+             * @type {Number}
+             */
             animationSpeed: 500,
-            closeOnClick: false
+
+            /**
+             * Whether or not the overlay should be closed when clicked on.
+             *
+             * @type {Boolean}
+             */
+            closeOnClick: false,
+
+            /**
+             * Function that will be called when the overlay was clicked on.
+             *
+             * @type {Function}
+             */
+            onClick: function () { }
         },
 
         /**
          * The extended options for the current opened overlay.
          *
+         * @public
+         * @property options
          * @type {Object}
          */
-        options: {},
+        options: { },
 
         /**
          * Opens/Shows the fullscreen overlay element.
          * If it's not available, it will be created and appended to the document body.
          *
+         * @public
+         * @method open
          * @param {Object} options
          */
         open: function (options) {
@@ -48,73 +101,119 @@
 
             me.options = $.extend({}, me.defaults, options);
 
-            if (me.$overlay === null) {
-                me.$overlay = $('<div>', {
+            if (!me._$overlay) {
+                me._$overlay = $('<div>', {
                     'class': 'js--overlay'
                 });
 
-                $('body').append(me.$overlay);
+                $('body').append(me._$overlay);
+
+                me._$overlay.on('click touchstart MSPointerDown', me.onClick.bind(me));
             }
 
-            me._updateOverlay();
+            me.updateOverlay();
 
-            me.$overlay.fadeIn(me.options.animationSpeed);
+            if (modernizr.csstransitions) {
+                me._$overlay.stop(true).transition({
+                    opacity: me.options.opacity
+                }, me.options.animationSpeed);
+            } else {
+                me._$overlay.stop(true).animate({
+                    opacity: me.options.opacity
+                }, me.options.animationSpeed);
+            }
         },
 
         /**
          * Closes/Hides the overlay element when it's available
+         *
+         * @public
+         * @method close
          */
         close: function () {
-            var me = this;
+            var me = this,
+                css = {
+                    opacity: 0.01
+                },
+                duration = me.options.animationSpeed || me.defaults.animationSpeed,
+                easing = 'ease',
+                callback = function () {
+                    me._$overlay.css({
+                            display: 'none'
+                        }
+                    );
+                };
 
-            if (me.$overlay !== null) {
-                me.$overlay.fadeOut(me.options.animationSpeed || me.defaults.animationSpeed);
+            if (me._$overlay !== null) {
+                if (modernizr.csstransitions) {
+                    me._$overlay.stop(true).transition(css, duration, easing, callback);
+                } else {
+                    me._$overlay.stop(true).animate(css, {
+                        duration: duration,
+                        complete: callback
+                    });
+                }
             }
-        },
-
-        /**
-         * Adds an event listener to the overlay element.
-         *
-         * @param {String} event
-         * @param {Function} func
-         */
-        addListener: function (event, func) {
-            var me = this;
-
-            me.$overlay.on(event, func.bind(me));
-        },
-
-        /**
-         * Removes an event listener from the overlay element.
-         *
-         * @param {String} event
-         */
-        removeListener: function (event) {
-            var me = this;
-
-            me.$overlay.off(event);
         },
 
         /**
          * Updates the overlay by setting new style options and listeners.
          *
-         * @private
+         * @public
+         * @method updateOverlay
          */
-        _updateOverlay: function () {
+        updateOverlay: function () {
             var me = this,
                 opts = me.options;
 
-            me.$overlay.css({
-                background: opts.color,
-                opacity: opts.opacity,
+            me._$overlay.css({
+                opacity: 0.01,
+                display: 'block',
+                background: opts.color ? opts.color : '',
                 cursor: opts.closeOnClick ? 'pointer' : 'default'
             });
+        },
 
-            me.removeListener('click.overlay');
+        /**
+         * Will be called when the overlay was clicked on.
+         * Calls the onClick() function which was passed by the options.
+         *
+         * @public
+         * @method onClick
+         */
+        onClick: function (event) {
+            var me = this;
 
-            if (opts.closeOnClick) {
-                me.addListener('click.overlay', me.close.bind(me));
+            event.preventDefault();
+
+            me.options.onClick.call(me);
+
+            if (me.options.closeOnClick) {
+                me.close();
+            }
+        },
+
+        /**
+         * Removes the current overlay element from the DOM and destroys it.
+         * Also clears the options.
+         *
+         * @public
+         * @method destroy
+         */
+        destroy: function () {
+            var me = this,
+                p;
+
+            me._$overlay.remove();
+
+            me._$overlay = null;
+
+            for (p in me.options) {
+                if (!me.options.hasOwnProperty(p)) {
+                    continue;
+                }
+                delete me.options[p];
             }
         }
     };
-})(jQuery);
+})(jQuery, Modernizr);
