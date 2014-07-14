d5f45562766107a4b192658d08b732079e864692
Tino Vogel <tv@shopware.com>
2014-07-14 09:05:33 +0200

    SW-8695 - Styled Datepicker

--- engine/Shopware/Themes/Frontend/Responsive/frontend/_public/vendors/jquery.ui.datepicker/jquery.ui.datepicker.js
+++ engine/Shopware/Themes/Frontend/Responsive/frontend/_public/vendors/jquery.ui.datepicker/jquery.ui.datepicker.js
@@ -1958,7 +1958,7 @@
                 this._getFormatConfig(inst)));
 
             prev = (this._canAdjustMonth(inst, -1, drawYear, drawMonth) ?
-                "<a class='ui-datepicker-prev ui-corner-all' data-handler='prev' data-event='click'" +
+                "<a class='ui-datepicker-prev ui-corner-all icon--arrow-left' data-handler='prev' data-event='click'" +
                     " title='" + prevText + "'><span class='ui-icon ui-icon-circle-triangle-" + ( isRTL ? "e" : "w") + "'>" + prevText + "</span></a>" :
                 (hideIfNoPrevNext ? "" : "<a class='ui-datepicker-prev ui-corner-all ui-state-disabled' title='"+ prevText +"'><span class='ui-icon ui-icon-circle-triangle-" + ( isRTL ? "e" : "w") + "'>" + prevText + "</span></a>"));
 
@@ -1968,7 +1968,7 @@
                 this._getFormatConfig(inst)));
 
             next = (this._canAdjustMonth(inst, +1, drawYear, drawMonth) ?
-                "<a class='ui-datepicker-next ui-corner-all' data-handler='next' data-event='click'" +
+                "<a class='ui-datepicker-next ui-corner-all icon--arrow-right' data-handler='next' data-event='click'" +
                     " title='" + nextText + "'><span class='ui-icon ui-icon-circle-triangle-" + ( isRTL ? "w" : "e") + "'>" + nextText + "</span></a>" :
                 (hideIfNoPrevNext ? "" : "<a class='ui-datepicker-next ui-corner-all ui-state-disabled' title='"+ nextText + "'><span class='ui-icon ui-icon-circle-triangle-" + ( isRTL ? "w" : "e") + "'>" + nextText + "</span></a>"));
 
