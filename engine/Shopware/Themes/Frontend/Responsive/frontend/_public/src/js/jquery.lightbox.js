629617e3739af926c6f98d82b7531bc206e04539
Philipp Schuch <psc@shopware.com>
2014-07-29 11:00:54 +0200

    SW-8569 Added documentation

--- engine/Shopware/Themes/Frontend/Responsive/frontend/_public/src/js/jquery.lightbox.js
+++ engine/Shopware/Themes/Frontend/Responsive/frontend/_public/src/js/jquery.lightbox.js
@@ -1,9 +1,23 @@
 ;(function ($) {
     'use strict';
 
+    /**
+     * Shopware Lightbox Plugin.
+     *
+     * This plugin is based on the modal plugin.
+     * It opens images in a modal window and sets the width and height
+     * of the modal box automatically to the image size. If the image
+     * size is bigger than the window size, the modal will be set to
+     * 90% of the window size so there is little margin between the modal
+     * and the window edge. It calculates always the correct aspect.
+     *
+     * Usage:
+     * $.lightbox.open('http://url.to.my.image.de');
+     *
+     */
     $.lightbox = {
 
-        open: function(imageURL, options) {
+        open: function(imageURL) {
             var me = this,
                 width, height,
                 maxWidth = window.innerWidth * 0.9,
