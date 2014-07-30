2ccccdc52fbef5cf8875ba6b49141a6aa188ea2f
Tino Vogel <tv@shopware.com>
2014-07-30 15:35:01 +0200

    SW-8554 - Increased migration number

--- _sql/migrations/375-display-shipping-calculations-in-basket.php
+++ _sql/migrations/375-display-shipping-calculations-in-basket.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration374 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration375 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
