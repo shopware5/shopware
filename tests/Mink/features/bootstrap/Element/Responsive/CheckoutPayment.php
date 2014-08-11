0b46d9d75e23a34101ddef30b86a36ada2193edd
Tino Vogel <tv@shopware.com>
2014-08-11 08:50:40 +0200

    SW-7864 - Fixed failed Mink Tests

--- tests/Mink/features/bootstrap/Element/Responsive/CheckoutPayment.php
+++ tests/Mink/features/bootstrap/Element/Responsive/CheckoutPayment.php
@@ -4,5 +4,8 @@ namespace Responsive;
 
 class CheckoutPayment extends \Emotion\CheckoutPayment
 {
-
+    /**
+     * @var array $selector
+     */
+    protected $selector = array('css' => 'div.panel--group > div.panel:nth-of-type(3)');
 }
--- tests/Mink/features/bootstrap/Element/Responsive/CheckoutShipping.php
+++ tests/Mink/features/bootstrap/Element/Responsive/CheckoutShipping.php
@@ -4,5 +4,8 @@ namespace Responsive;
 
 class CheckoutShipping extends \Emotion\CheckoutShipping
 {
-
+    /**
+     * @var array $selector
+     */
+    protected $selector = array('css' => 'div.panel--group > div.panel:nth-of-type(2)');
 }
