8a110981ec2003ff010500f0f87a28046ca05a08
Linus Holtstiege <lh@shopware.com>
2014-08-19 15:56:36 +0200

    SW-8429 - increased migration number

--- _sql/migrations/379-add-device-column.php
+++ _sql/migrations/379-add-device-column.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration365 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration379 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up()
     {
