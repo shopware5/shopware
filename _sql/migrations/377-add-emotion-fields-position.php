3d802c07131a62676f51aa06517d079332e795cf
Tiago Garcia <tg@shopware.com>
2014-08-27 14:26:15 +0200

    SW-8808 - Add position calculation

--- _sql/migrations/377-add-emotion-fields-position.php
+++ _sql/migrations/377-add-emotion-fields-position.php
@@ -3,7 +3,7 @@ class Migrations_Migration377 Extends Shopware\Components\Migrations\AbstractMig
 {
     public function up($modus)
     {
-        $this->addSql("ALTER TABLE s_library_component_field ADD position INT NOT NULL;");
+        $this->addSql("ALTER TABLE s_library_component_field ADD position INT NULL;");
         $this->addSql("UPDATE s_library_component_field SET position = id;");
         $this->addSql("SET @parent = (SELECT id FROM `s_library_component` WHERE `x_type`='emotion-components-article-slider' AND template = 'component_article_slider' AND pluginID IS NULL LIMIT 1);");
         $this->addSql("SET @maxNumberPosition = (SELECT id FROM `s_library_component_field` WHERE `name`='article_slider_max_number' AND componentID = @parent LIMIT 1);");
