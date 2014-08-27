74986eb197a00e05114fa212d257b68b2f527bea
Tiago Garcia <tg@shopware.com>
2014-08-27 16:08:31 +0200

    SW-8715 - fix migration number

--- _sql/migrations/382-add-404-page-config-options.php
+++ _sql/migrations/382-add-404-page-config-options.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration381 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration382 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up($modus)
     {
