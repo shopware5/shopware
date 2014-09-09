0fdcaf04db5452e1142445c38c7da77ac867d4b7
Oliver Denter <dr@shopware.com>
2014-09-09 08:18:03 +0200

    NTR - Add JsonSerializable implementation to all context classes.

--- engine/Shopware/Bundle/StoreFrontBundle/Struct/LocationContext.php
+++ engine/Shopware/Bundle/StoreFrontBundle/Struct/LocationContext.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Shopware 4
+ * Copyright © shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 namespace Shopware\Bundle\StoreFrontBundle\Struct;
 
@@ -6,12 +28,13 @@ use Shopware\Bundle\StoreFrontBundle\Struct\Country\State;
 use Shopware\Bundle\StoreFrontBundle\Struct\Country\Area;
 
 /**
- * Class LocationContext
- * @package Shopware\Bundle\StoreFrontBundle\Struct
+ * @category  Shopware
+ * @package   Shopware\Bundle\StoreFrontBundle\Struct
+ * @copyright Copyright (c) shopware AG (http://www.shopware.de)
  */
 class LocationContext
     extends Extendable
-    implements LocationContextInterface
+    implements LocationContextInterface, \JsonSerializable
 {
     /**
      * @var Area
@@ -62,5 +85,14 @@ class LocationContext
     public function getState()
     {
         return $this->state;
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function jsonSerialize()
+    {
+        return get_object_vars($this);
     }
 }
