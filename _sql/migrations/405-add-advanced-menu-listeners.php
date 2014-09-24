5e2fe4672cddd00f67adf5654889adbeb1dcfe17
Dieter Giesler <dg@shopware.com>
2014-09-24 09:12:58 +0200

    SW-8570 - Changed migration number back from 406 to 405

--- _sql/migrations/405-add-advanced-menu-listeners.php
+++ _sql/migrations/405-add-advanced-menu-listeners.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration406 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration405 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up($modus)
     {
