e77498e12c24252a9e3ce1c6e103dd6ff0f5605f
Benjamin Cremer <bc@shopware.com>
2014-10-08 13:06:40 +0200

    Always return successful return code
    Always return a successful return in clear cache script, even if no
    cache files were removed.
    
    This is to not break build systems depending on successful return codes.

--- cache/clear_cache.sh
+++ cache/clear_cache.sh
@@ -2,5 +2,5 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Clearing caches"
-find $DIR -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 rm -R
+find $DIR -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 rm -R || true
 
