5bd4c900f45f5dda466ecae26fa8f8227efa8285
Tino Vogel <tv@shopware.com>
2014-10-20 15:45:02 +0200

    SW-9154 - Fixed mink tests

--- tests/Mink/features/bootstrap/Element/Responsive/Article.php
+++ tests/Mink/features/bootstrap/Element/Responsive/Article.php
@@ -4,5 +4,59 @@ namespace Element\Responsive;
 
 class Article extends \Element\Emotion\Article
 {
+    /**
+     * @var array $selector
+     */
+    protected $selector = array('css' => 'div.emotion--element.article-element');
 
+    public $cssLocator = array(
+        'name' => 'a.product--title',
+        'link' => 'a.box--image',
+        'container' =>  'a.box--image > .image--element',
+        'image' => 'a.box--image > .image--element > span:nth-of-type(2)',
+        'price' => 'div.price--default > .price--content'
+    );
+
+    /**
+     * @return array
+     */
+    public function getNamesToCheck()
+    {
+        $locators = array('name', 'link', 'container');
+        $elements = \Helper::findElements($this, $locators);
+
+        return array(
+            $elements['name']->getText(),
+            $elements['name']->getAttribute('title'),
+            $elements['link']->getAttribute('title'),
+            $elements['container']->getAttribute('data-alt')
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getImagesToCheck()
+    {
+        $locators = array('image');
+        $elements = \Helper::findElements($this, $locators);
+
+        return array(
+            'image' => $elements['image']->getAttribute('data-src')
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getLinksToCheck()
+    {
+        $locators = array('name', 'link');
+        $elements = \Helper::findElements($this, $locators);
+
+        return array(
+            $elements['name']->getAttribute('href'),
+            $elements['link']->getAttribute('href')
+        );
+    }
 }
--- tests/Mink/features/bootstrap/Element/Responsive/ArticleSlider.php
+++ tests/Mink/features/bootstrap/Element/Responsive/ArticleSlider.php
@@ -4,5 +4,53 @@ namespace Element\Responsive;
 
 class ArticleSlider extends \Element\Emotion\ArticleSlider
 {
+    /**
+     * @var array $selector
+     */
+    protected $selector = array('css' => 'div.emotion--element.article-slider-element');
 
+    public $cssLocator = array(
+        'slideImage' => 'div.article-slider--item a.product--image > .image--element > span:nth-of-type(2)',
+        'slideContainer' => 'div.article-slider--item a.product--image > .image--element',
+        'slideLink' => 'div.article-slider--item a.product--image',
+        'slideName' => 'div.article-slider--item a.product--title'
+    );
+
+    /**
+     * @return array
+     */
+    public function getImagesToCheck()
+    {
+        $locators = array('slideImage');
+        $elements = \Helper::findElements($this, $locators, null, true);
+
+        $images = array();
+
+        foreach ($elements['slideImage'] as $image) {
+            $images[] = array($image->getAttribute('data-src'));
+        }
+
+        return $images;
+    }
+
+    /**
+     * @return array
+     */
+    public function getNamesToCheck()
+    {
+        $locators = array('slideContainer', 'slideLink', 'slideName');
+        $elements = \Helper::findElements($this, $locators, null, true);
+
+        $names = array();
+
+        foreach ($elements['slideContainer'] as $key => $container) {
+            $names[] = array(
+                $container->getAttribute('data-alt'),
+                $elements['slideLink'][$key]->getAttribute('title'),
+                $elements['slideName'][$key]->getAttribute('title'),
+            );
+        }
+
+        return $names;
+    }
 }
