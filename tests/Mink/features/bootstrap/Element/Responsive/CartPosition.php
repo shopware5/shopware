d3ca2d13fba42dc311146701c4e12be4aeabdb6b
Tino Vogel <tv@shopware.com>
2014-11-10 15:19:37 +0100

    SW-9605 - Added test for graduated prices

--- tests/Mink/features/bootstrap/Element/Responsive/CartPosition.php
+++ tests/Mink/features/bootstrap/Element/Responsive/CartPosition.php
@@ -13,6 +13,9 @@ class CartPosition extends \Element\Emotion\CartPosition
         'name' => 'div.table--content > a.content--title',
         'number' => 'div.table--content > p.content--sku',
         'thumbnailLink' => 'div.table--media > a.table--media-link',
-        'thumbnailImage' => 'div.table--media > a.table--media-link > img'
+        'thumbnailImage' => 'div.table--media > a.table--media-link > img',
+        'quantity' => 'div.column--quantity > select > option',
+        'itemPrice' => 'div.column--unit-price',
+        'sum' => 'div.column--total-price'
     );
 }
