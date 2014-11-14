a2d369de49c9502d018d5ba67b85fe3ab5949cbc
Tino Vogel <tv@shopware.com>
2014-11-14 11:56:21 +0100

    NTR - Fixed failed mink tests

--- tests/Mink/features/bootstrap/Element/Responsive/CartPosition.php
+++ tests/Mink/features/bootstrap/Element/Responsive/CartPosition.php
@@ -7,13 +7,13 @@ class CartPosition extends \Element\Emotion\CartPosition
     /**
      * @var array $selector
      */
-    protected $selector = array('css' => 'div.table--row');
+    protected $selector = array('css' => 'div.row--product');
 
     public $cssLocator = array(
         'name' => 'div.table--content > a.content--title',
         'number' => 'div.table--content > p.content--sku',
-        'thumbnailLink' => 'div.table--media > a.table--media-link',
-        'thumbnailImage' => 'div.table--media > a.table--media-link > img',
+        'thumbnailLink' => 'div.table--media a.table--media-link',
+        'thumbnailImage' => 'div.table--media a.table--media-link > img',
         'quantity' => 'div.column--quantity > select > option',
         'itemPrice' => 'div.column--unit-price',
         'sum' => 'div.column--total-price'
