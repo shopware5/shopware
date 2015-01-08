279595157e0fde95ea40f7b665958b97998cf38b
Marcel Schmäing <ms@shopware.com>
2015-01-08 17:01:06 +0100

    NTR fix migration numbers

--- _sql/migrations/432-html5-tags-for-snippets.php
+++ _sql/migrations/432-html5-tags-for-snippets.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration427 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration432 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up($modus)
     {
