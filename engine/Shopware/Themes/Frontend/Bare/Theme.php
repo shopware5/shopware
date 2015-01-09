61a041c431b9965ddf90d1ca5e72405d5aba0ebb
Tobias Berge <tb@shopware.com>
2015-01-09 17:02:07 +0100

    SW-9360 Fixed: Normalized labelWidth, Changed infinite scrolling snippets

--- engine/Shopware/Themes/Frontend/Bare/Theme.php
+++ engine/Shopware/Themes/Frontend/Bare/Theme.php
@@ -42,7 +42,7 @@ class Theme extends \Shopware\Components\Theme
     {
         $tab = $this->createTab('bareMain', '__bare_tab_header__', array('attributes' => array('layout' => 'anchor', 'autoScroll' => true, 'padding' => '0')));
 
-        $fieldSet = $this->createFieldSet('bareGlobal', '__global_configuration__', array('attributes' => array('padding' => '10', 'margin'=> '5', 'layout' => 'anchor', 'defaults' => array('anchor' => '100%'))));
+        $fieldSet = $this->createFieldSet('bareGlobal', '__global_configuration__', array('attributes' => array('padding' => '10', 'margin'=> '5', 'layout' => 'anchor', 'defaults' => array('labelWidth' => 155, 'anchor' => '100%'))));
         $fieldSet->addElement($this->createCheckboxField('offcanvasCart', '__offcanvas_cart__', true, $this->getLabelAttribute('offcanvas_cart_description')));
         $fieldSet->addElement($this->createCheckboxField('focusSearch', '__focus_search__', false, $this->getLabelAttribute('focus_search_description')));
         $fieldSet->addElement($this->createCheckboxField('displaySidebar', '__display_sidebar__', true, $this->getLabelAttribute('display_sidebar_description')));
@@ -58,14 +58,14 @@ class Theme extends \Shopware\Components\Theme
         ), $this->getLabelAttribute('lightbox_zoom_factor_description')));
         $tab->addElement($fieldSet);
 
-        $fieldSet = $this->createFieldSet('bareLogos', '__logos__', array('attributes' => array('padding' => '10', 'margin'=> '5', 'layout' => 'anchor', 'defaults' => array('anchor' => '100%'))));
+        $fieldSet = $this->createFieldSet('bareLogos', '__logos__', array('attributes' => array('padding' => '10', 'margin'=> '5', 'layout' => 'anchor', 'defaults' => array('labelWidth' => 155, 'anchor' => '100%'))));
         $fieldSet->addElement($this->createMediaField('mobileLogo', '__smartphone__', 'frontend/_public/src/img/logos/logo--mobile.png'));
         $fieldSet->addElement($this->createMediaField('tabletLogo', '__tablet__', 'frontend/_public/src/img/logos/logo--tablet.png'));
         $fieldSet->addElement($this->createMediaField('tabletLandscapeLogo', '__tablet_landscape__', 'frontend/_public/src/img/logos/logo--tablet.png'));
         $fieldSet->addElement($this->createMediaField('desktopLogo', '__desktop__', 'frontend/_public/src/img/logos/logo--tablet.png'));
         $tab->addElement($fieldSet);
 
-        $fieldSet = $this->createFieldSet('Icons', '__icons__', array('attributes' => array('padding' => '10', 'margin'=> '5', 'layout' => 'anchor', 'defaults' => array('labelWidth' => 280, 'anchor' => '100%'))));
+        $fieldSet = $this->createFieldSet('Icons', '__icons__', array('attributes' => array('padding' => '10', 'margin'=> '5', 'layout' => 'anchor', 'defaults' => array('labelWidth' => 155, 'anchor' => '100%'))));
         $fieldSet->addElement($this->createMediaField('appleTouchIcon', '__apple_touch_icon__', 'frontend/_public/src/img/apple-touch-icon-precomposed.png'));
         $fieldSet->addElement($this->createMediaField('win8TileImage', '__win8_tile_image__', 'frontend/_public/src/img/win-tile-image.png'));
         $fieldSet->addElement($this->createMediaField('favicon', '__favicon__', 'frontend/_public/src/img/favicon.ico'));
