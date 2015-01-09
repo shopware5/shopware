34eb5f9ff784d32a88e1faa201165d7eab3f1dc2
Tino Vogel <tv@shopware.com>
2015-01-09 11:51:12 +0100

    SW-9947 - Increased migration number

--- _sql/migrations/436-update-html5-video-fields.php
+++ _sql/migrations/436-update-html5-video-fields.php
@@ -1,5 +1,5 @@
 <?php
-class Migrations_Migration432 Extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration436 Extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up($modus)
     {
