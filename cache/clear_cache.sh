221c4e7559847b48186e533b89da23f0be17927a
Benjamin Cremer <bc@shopware.com>
2015-01-09 10:47:17 +0100

    Remove web caches in clear_cache.sh script

--- cache/clear_cache.sh
+++ cache/clear_cache.sh
@@ -2,5 +2,7 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Clearing caches"
-find $DIR -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 rm -R || true
-
+find $DIR -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 rm -Rf
+rm -f $DIR/../web/cache/*.js > /dev/null
+rm -f $DIR/../web/cache/*.css
+rm -f $DIR/../web/cache/*.txt
