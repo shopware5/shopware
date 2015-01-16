350cb215cfb441cc2917cf1d1ae5e48db9a11135
Tino Vogel <tv@shopware.com>
2015-01-16 08:01:39 +0100

    SW-9941 - Enabled 'scope' for the configuration, added info to UPGRADE.md

--- _sql/migrations/441-add-hide-downloads-in-account.php
+++ _sql/migrations/441-add-hide-downloads-in-account.php
@@ -7,7 +7,7 @@ class Migrations_Migration441 Extends Shopware\Components\Migrations\AbstractMig
 SET @parent = (SELECT id FROM `s_core_config_forms` WHERE `name`='Esd');
 INSERT INTO `s_core_config_elements`
 (`id`, `form_id`, `name`, `value`, `label`, `description`, `type`, `required`, `position`, `scope`, `filters`, `validators`, `options`)
-VALUES (NULL, @parent, 'showEsd', 'b:1;', 'Sofortdownloads im Account anzeigen', 'Sofortdownloads können weiterhin über die Bestellübersicht heruntergeladen werden.', 'boolean', '1', '5', '', NULL, NULL, NULL);
+VALUES (NULL, @parent, 'showEsd', 'b:1;', 'Sofortdownloads im Account anzeigen', 'Sofortdownloads können weiterhin über die Bestellübersicht heruntergeladen werden.', 'boolean', '1', '5', '1', NULL, NULL, NULL);
 
 SET @elementId = (SELECT id FROM `s_core_config_elements` WHERE `name` = 'showEsd' LIMIT 1);
 SET @localeID = (SELECT id FROM s_core_locales WHERE locale='en_GB');
