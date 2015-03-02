e8b9f92e94504820ea1d02437cc639c274ce9a2f
Tiago Garcia <tg@shopware.com>
2015-03-02 09:42:42 +0100

    SW-9850 - CR fixes for SofortPayment dummy plugin

--- engine/Shopware/Plugins/Default/Frontend/SofortPayment/BootstrapDummy.php
+++ engine/Shopware/Plugins/Default/Frontend/SofortPayment/BootstrapDummy.php
@@ -1,14 +1,13 @@
 <?php
-
 /**
- * Shopware 4.0
- * Copyright © 2012 shopware AG
+ * Shopware 4
+ * Copyright © shopware AG
  *
  * According to our dual licensing model, this program can be used either
- * under the terms of the GNU LESSER GENERAL PUBLIC LICENSE, version 3,
+ * under the terms of the GNU Affero General Public License, version 3,
  * or under a proprietary license.
  *
- * The texts of the GNU LESSER GENERAL PUBLIC LICENSE with an additional
+ * The texts of the GNU Affero General Public License with an additional
  * permission and of our proprietary license can be found at and
  * in the LICENSE file you have received along with this program.
  *
@@ -21,8 +20,10 @@
  * The licensing of the program under the AGPLv3 does not imply a
  * trademark license. Therefore any rights, title and interest in
  * our trademarks remain entirely with us.
- *
- * @category   Shopware
+ */
+
+/**
+ * @category  Shopware
  * @package   Shopware\Plugins\Frontend\SofortPayment
  * @copyright Copyright (c) shopware AG (http://www.shopware.de)
  */
