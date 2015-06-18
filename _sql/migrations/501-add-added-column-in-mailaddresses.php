98aaf61f49f846de7891b11e5358a8cfa70a8267
Simon Bäumer <bs@shopware.com>
2015-06-18 09:16:59 +0200

    SW-8835 - fixed mysql migration

--- _sql/migrations/501-add-added-column-in-mailaddresses.php
+++ _sql/migrations/501-add-added-column-in-mailaddresses.php
@@ -10,16 +10,8 @@ class Migrations_Migration501 Extends Shopware\Components\Migrations\AbstractMig
 EOD;
         $this->addSql($sql);
 
-        //set default value to current_timestamp this can't be done directly because this is only supported in mysql 5.6
-        $sql = <<<'EOD'
-            UPDATE s_campaigns_mailaddresses
-            SET added = CURRENT_TIMESTAMP();
-EOD;
-        $this->addSql($sql);
-
-
         $sql = "UPDATE s_campaigns_mailaddresses ca
-SET added = (SELECT cm.added FROM s_campaigns_maildata cm WHERE cm.email = ca.email LIMIT 1)";
+                SET added = (SELECT cm.added FROM s_campaigns_maildata cm WHERE cm.email = ca.email LIMIT 1)";
         $this->addSql($sql);
     }
 }
