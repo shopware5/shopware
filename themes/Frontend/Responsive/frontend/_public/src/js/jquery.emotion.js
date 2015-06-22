c8960b87f37d401bb333ac8d98f74165fbab6e35
Florian Schlittenbauer <fschlittenbauer@1drop.de>
2015-06-22 13:05:19 +0200

    [BUGFIX] Slider initialization

--- themes/Frontend/Responsive/frontend/_public/src/js/jquery.emotion.js
+++ themes/Frontend/Responsive/frontend/_public/src/js/jquery.emotion.js
@@ -440,6 +440,7 @@
             me.$bannerElements = me.$elements.find(me.opts.bannerElSelector);
             me.$videoElements = me.$elements.find(me.opts.videoElSelector);
             me.$productSliderElements = me.$elements.find('*[data-product-slider="true"]');
+            me.$imageSliderElements = me.$elements.find('*[data-image-slider="true"]');
 
             me.remSpacing = ~~me.opts.cellSpacing / 16;
 
@@ -475,8 +476,13 @@
                 $(item).emotionVideo();
             });
 
-            StateManager.updatePlugin('*[data-product-slider="true"]', 'productSlider');
-            StateManager.updatePlugin('*[data-image-slider="true"]', 'imageSlider');
+            $.each(me.$productSliderElements, function(index, item){
+                StateManager.updatePlugin($(item), 'productSlider');
+            });
+
+            $.each(me.$imageSliderElements, function(index, item){
+                StateManager.updatePlugin($(item), 'imageSlider');
+            });
 
             window.picturefill();
 
