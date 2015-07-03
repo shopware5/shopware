d7b3226dfaecf99db86beab6f07b00231a688f72
Simon Bäumer <bs@shopware.com>
2015-07-03 11:02:42 +0200

    SW-11583 - fix migration 372 and 476

--- _sql/migrations/476-add-less-compatible-flag.php
+++ _sql/migrations/476-add-less-compatible-flag.php
@@ -4,6 +4,17 @@ class Migrations_Migration476 Extends Shopware\Components\Migrations\AbstractMig
 {
     public function up($modus)
     {
+        $statement = $this->getConnection()->prepare('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = "s_core_templates_config_elements";');
+        $statement->execute();
+        $result = $statement->fetchAll(\PDO::FETCH_COLUMN);
+
+        if (!in_array('less_compatible', $result)) {
+            $this->addLessCompatibleFlag();
+        }
+    }
+
+    private function addLessCompatibleFlag()
+    {
         $sql = <<<SQL
 ALTER TABLE `s_core_templates_config_elements` ADD `less_compatible` INT(1) NOT NULL DEFAULT '1' ;
 SQL;
