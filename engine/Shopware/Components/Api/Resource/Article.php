29771505b6a3f457f05c028da14babf3a22658d2
Jan Bücker <jb@shopware.com>
2015-07-15 12:34:52 +0200

    SW-11342 - Fixes REST Api configuratorSet group position

--- engine/Shopware/Components/Api/Resource/Article.php
+++ engine/Shopware/Components/Api/Resource/Article.php
@@ -992,7 +992,9 @@ class Article extends Resource implements BatchInterface
 
                 $option->fromArray($optionData);
                 $option->setGroup($group);
-                $option->setPosition($optionPosition++);
+                if (!isset($optionData['position'])) {
+                    $option->setPosition($optionPosition++);
+                }
                 $allOptions[] = $option;
                 $groupOptions[] = $option;
             }
