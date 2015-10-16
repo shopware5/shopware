bb18a0de234d1458dae200e4cd8d6931f430f998
haruka <haruka@sunaru.de>
2015-10-16 17:11:37 +0200

    Fixed wrong default tax rate
    Line: 964
    Changed: $tax = 119;
    To: $tax = 19;
    
    Like it's done in Line 369 and Line 863 before.

--- engine/Shopware/Core/sBasket.php
+++ engine/Shopware/Core/sBasket.php
@@ -961,7 +961,7 @@ class sBasket
             }
 
             if (!$tax) {
-                $tax = 119;
+                $tax = 19;
             }
 
             if ((!$this->sSYSTEM->sUSERGROUPDATA["tax"] && $this->sSYSTEM->sUSERGROUPDATA["id"])) {
