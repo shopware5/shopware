7a7a5c8a2dc50d875d8c393ec12aa220cb7f80d4
Timo Helmke <thelmke@byte-fuer-byte.de>
2016-01-27 11:26:13 +0100

    Removed array conversion in the Property Group model. Should be more consistent to the other models. This should fix #SW-13924. As far as I see there are no sideeffects with other parts that rely on an array but I'm not sure about that. Please verify.

--- engine/Shopware/Models/Property/Group.php
+++ engine/Shopware/Models/Property/Group.php
@@ -214,11 +214,11 @@ class Group extends ModelEntity
     /**
      * Returns Array of associated Options
      *
-     * @return \Shopware\Models\Property\Option[]
+     * @return \Doctrine\Common\Collections\ArrayCollection
      */
     public function getOptions()
     {
-        return $this->options->toArray();
+        return $this->options;
     }
 
     /**
