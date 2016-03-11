876a65ed9264b8bbef016bc87adcc4d1d66297e1
edwint88 <tuzaredwin@gmail.com>
2016-03-11 01:07:43 +0100

    SW-14505 - Add {block} tags for all .js files in the PluginManager

--- engine/Shopware/Plugins/Default/Backend/PluginManager/Views/backend/plugin_manager/view/detail/prices.js
+++ engine/Shopware/Plugins/Default/Backend/PluginManager/Views/backend/plugin_manager/view/detail/prices.js
@@ -1,5 +1,34 @@
-
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ *
+ * @category   Shopware
+ * @package    PluginManager
+ * @subpackage Detail
+ * @version    $Id$
+ * @author shopware AG
+ */
 //{namespace name=backend/plugin_manager/translation}
+
+//{block name="backend/plugin_manager/view/detail/prices"}
 Ext.define('Shopware.apps.PluginManager.view.detail.Prices', {
     extend: 'PluginManager.tab.Panel',
     cls: 'store-plugin-detail-prices-tab shopware-plugin-manager-tab',
@@ -217,3 +246,4 @@ Ext.define('Shopware.apps.PluginManager.view.detail.Prices', {
     }
 
 });
+//{/block}
