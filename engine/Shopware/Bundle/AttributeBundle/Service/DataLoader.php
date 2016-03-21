e99ae2df0cb291ae7dd38657a887ea6f9ab663c6
Oliver Skroblin <dr@shopware.com>
2016-03-21 15:25:48 +0100

    SW-13438 - Implement ext js components which performs crud operations for all attribute tables.

---
+++ engine/Shopware/Bundle/AttributeBundle/Service/DataLoader.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+namespace Shopware\Bundle\AttributeBundle\Service;
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * @category  Shopware
+ * @package   Shopware\Bundle\AttributeBundle\Service
+ * @copyright Copyright (c) shopware AG (http://www.shopware.com)
+ */
+class DataLoader
+{
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @var TableMapping
+     */
+    private $mapping;
+
+    /**
+     * @param Connection $connection
+     * @param TableMapping $mapping
+     */
+    public function __construct(Connection $connection, TableMapping $mapping)
+    {
+        $this->connection = $connection;
+        $this->mapping = $mapping;
+    }
+
+    /**
+     * @param string $table
+     * @param string $foreignKey
+     * @return array
+     * @throws \Exception
+     */
+    public function load($table, $foreignKey)
+    {
+        if (!$this->mapping->isAttributeTable($table)) {
+            throw new \Exception(sprintf("Table %s is no attribute table", $table));
+        }
+
+        if (!$foreignKey) {
+            throw new \Exception("No foreign key provided");
+        }
+
+        /** @var TableMapping $mapping */
+        $foreignKeyColumn = $this->mapping->getTableForeignKey($table);
+
+        $query = $this->connection->createQueryBuilder();
+        $query->select('alias.*')
+            ->from($table, 'alias')
+            ->where('alias.' . $foreignKeyColumn . ' = :foreignKey')
+            ->setParameter(':foreignKey', $foreignKey)
+            ->setFirstResult(0)
+            ->setMaxResults(1);
+
+        return $query->execute()->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @param string $table
+     * @param int $foreignKey
+     * @return array[]
+     * @throws \Exception
+     */
+    public function loadTranslations($table, $foreignKey)
+    {
+        if (!$foreignKey) {
+            throw new \Exception("No foreign key provided");
+        }
+
+        $query = $this->connection->createQueryBuilder();
+        $query->select('translation.*');
+        $query->from('s_core_translations', 'translation');
+        $query->where('translation.objecttype = :type');
+        $query->setParameter(':type', $table);
+        $query->andWhere('objectkey = :key');
+        $query->setParameter(':key', $foreignKey);
+
+        $data = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
+        return $data;
+    }
+}
---
+++ themes/Backend/ExtJs/backend/base/attribute/Shopware.attribute.Window.js
@@ -0,0 +1,157 @@
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ *
+ * @category    Shopware
+ * @package     Base
+ * @subpackage  Attribute
+ * @version     $Id$
+ * @author      shopware AG
+ */
+
+//{namespace name="backend/attributes/main"}
+//{block name="backend/base/attribute/window"}
+Ext.define('Shopware.attribute.Window', {
+    extend: 'Enlight.app.Window',
+    table: null,
+    layout: 'border',
+    iconCls: 'sprite-attributes',
+    allowTranslation: true,
+
+    initComponent: function() {
+        var me = this;
+
+        me.items = me.createItems();
+        me.dockedItems = me.createDockedItems();
+        me.callParent(arguments);
+
+        if (me.record) {
+            me.loadAttribute(me.record);
+        }
+    },
+
+    loadAttribute: function(record) {
+        var me = this;
+        me.record = record;
+        me.attributeForm.loadAttribute(record.get('id'));
+        me.setTitle(this.getRecordTitle(record));
+    },
+
+    saveAttribute: function() {
+        var me = this;
+        me.attributeForm.saveAttribute(
+            me.record.get('id'),
+            function() {
+                me.destroy();
+            }
+        );
+    },
+
+    createItems: function() {
+        return [this.createForm()];
+    },
+
+    createDockedItems: function() {
+        return [this.createToolbar()];
+    },
+
+    createToolbar: function() {
+        return Ext.create('Ext.toolbar.Toolbar', {
+            dock: 'bottom',
+            items: this.createToolbarItems()
+        });
+    },
+
+    createToolbarItems: function() {
+        return [
+            '->',
+            this.createCancelButton(),
+            this.createSaveButton()
+        ];
+    },
+
+    createSaveButton: function() {
+        var me = this;
+        me.saveButton = Ext.create('Ext.button.Button', {
+            text: '{s name="save_button"}{/s}',
+            cls: 'primary',
+            handler: function() {
+                me.saveAttribute();
+            }
+        });
+        return me.saveButton;
+    },
+
+    createCancelButton: function() {
+        var me = this;
+        me.cancelButton = Ext.create('Ext.button.Button', {
+            text: '{s name="cancel_button"}{/s}',
+            cls: 'secondary',
+            handler: function() {
+                me.destroy();
+            }
+        });
+        return me.cancelButton;
+    },
+
+    createForm: function() {
+        var me = this;
+
+        me.attributeForm = Ext.create('Shopware.attribute.Form', {
+            table: me.table,
+            bodyPadding: 20,
+            region: 'center',
+            autoScroll: true,
+            allowTranslation: me.allowTranslation
+        });
+
+        return me.attributeForm;
+    },
+
+    getRecordTitle: function(record) {
+        var me = this;
+
+        if (me.title) {
+            return me.title;
+        }
+
+        var plain = '{s name="attribute_window_title"}{/s}';
+        if (!record) {
+            return plain;
+        }
+
+        var prefix = '{s name="attribute_window_title"}{/s}' + ': ';
+        if (record.get('name')) {
+            return prefix + record.get('name');
+        }
+        if (record.get('label')) {
+            return prefix + record.get('label');
+        }
+        if (record.get('description')) {
+            return prefix + record.get('description');
+        }
+        if (record.get('number')) {
+            return prefix + record.get('number');
+        }
+        return plain;
+    }
+});
+//{/block}
