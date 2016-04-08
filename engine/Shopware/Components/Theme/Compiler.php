be890afd7537864de3f212f5031551f4ef52f6d5
Thomas Christiansen <t.christiansen@reply.de>
2016-04-08 10:38:29 +0000

    SW-15129 - Added filter events to collect javascript and less methods before compiling
    Fixes https://github.com/shopware/shopware/pull/513

--- engine/Shopware/Components/Theme/Compiler.php
+++ engine/Shopware/Components/Theme/Compiler.php
@@ -329,6 +329,15 @@ class Compiler
             $this->collectInheritanceCss($inheritances['custom'])
         );
 
+        $definitions = $this->eventManager->filter(
+            'Theme_Compiler_Collect_Less_Definitions_FilterResult',
+            $definitions,
+            array(
+                'shop' => $shop,
+                'template' => $template
+            )
+        );
+
         return $definitions;
     }
 
@@ -372,6 +381,15 @@ class Compiler
         $files = array_merge(
             $files,
             $this->collectInheritanceJavascript($inheritances['custom'])
+        );
+
+        $files = $this->eventManager->filter(
+            'Theme_Compiler_Collect_Javascript_Files_FilterResult',
+            $files,
+            array(
+                'shop' => $shop,
+                'template' => $template
+            )
         );
 
         return $files;
