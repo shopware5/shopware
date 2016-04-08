5cc29777c7d06cbcc99c3652b70b4add924e468b
Benjamin Cremer <bc@shopware.com>
2016-04-08 07:03:19 +0200

    Set sql_mode during build

--- build/ApplyDeltas.php
+++ build/ApplyDeltas.php
@@ -56,6 +56,9 @@ try {
     );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    // Reset sql_mode "STRICT_TRANS_TABLES" that will be default in MySQL 5.6
+    $conn->exec('SET @@session.sql_mode = ""');
 } catch(PDOException $e) {
     echo 'Could not connect to database: ' . $e->getMessage();
     exit(1);
