53dcb696f05f31ab8925024b4f115fcc82e25d82
Jan Bücker <jb@shopware.com>
2016-07-28 15:08:16 +0200

    SW-15138 - Fix product query factory implementation docs

--- engine/Shopware/Bundle/ESIndexingBundle/Product/ProductQueryFactoryInterface.php
+++ engine/Shopware/Bundle/ESIndexingBundle/Product/ProductQueryFactoryInterface.php
@@ -31,7 +31,6 @@ use Shopware\Bundle\ESIndexingBundle\LastIdQuery;
  */
 interface ProductQueryFactoryInterface
 {
-
     /**
      * @param $categoryId
      * @param null|int $limit
@@ -40,71 +39,71 @@ interface ProductQueryFactoryInterface
     public function createCategoryQuery($categoryId, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $priceIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createPriceIdQuery($priceIds, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $unitIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createUnitIdQuery($unitIds, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $voteIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createVoteIdQuery($voteIds, $limit = null);
 
     /**
      * @param int[] $productIds
-     * @param null $limit
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createProductIdQuery($productIds, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $variantIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createVariantIdQuery($variantIds, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $taxIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createTaxQuery($taxIds, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $manufacturerIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createManufacturerQuery($manufacturerIds, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $categoryIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createProductCategoryQuery($categoryIds, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $groupIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createPropertyGroupQuery($groupIds, $limit = null);
 
     /**
-     * @param int|null $limit
      * @param int[] $optionIds
+     * @param int|null $limit
      * @return LastIdQuery
      */
     public function createPropertyOptionQuery($optionIds, $limit = null);
