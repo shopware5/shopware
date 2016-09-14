4d32e60b2363e45bf0c9c537e8e4f77fabe5db2e
Tobias Wilming <tobias_wilming_1993@web.de>
2016-09-14 09:18:51 +0200

    SW-14482 - update description and label to one query

--- _sql/migrations/788-change-Seo-Router-description.php
+++ _sql/migrations/788-change-Seo-Router-description.php
@@ -9,14 +9,8 @@ class Migrations_Migration788 extends Shopware\Components\Migrations\AbstractMig
     public function up($modus)
     {
         $sql = <<<'EOD'
-           UPDATE `s_core_config_elements` SET `label` = 'prev/next-Tag auf paginierten Seiten benutzen' 
-           WHERE `s_core_config_elements`.`name` = 'seoIndexPaginationLinks';
-EOD;
-        $this->addSql($sql);
-
-
-        $sql = <<<'EOD'
-           UPDATE `s_core_config_elements` SET `description` = 'Wenn aktiv, wird auf paginierten Seiten anstatt des Canoncial-Tags der prev/next-Tag benutzt.' 
+           UPDATE `s_core_config_elements` SET `label` = 'prev/next-Tag auf paginierten Seiten benutzen',
+           `description` = 'Wenn aktiv, wird auf paginierten Seiten anstatt des Canoncial-Tags der prev/next-Tag benutzt.'
            WHERE `s_core_config_elements`.`name` = 'seoIndexPaginationLinks';
 EOD;
         $this->addSql($sql);
@@ -27,13 +21,8 @@ EOD;
         $this->addSql($sql);
 
         $sql = <<<'EOD'
-           UPDATE `s_core_config_element_translations` SET `label` = 'Use prev/next-tag on paginated sites' 
-           WHERE `element_id` = @elementId;
-EOD;
-        $this->addSql($sql);
-
-        $sql = <<<'EOD'
-           UPDATE `s_core_config_element_translations` SET `description` = 'If active, use prev/next-tag instead of the Canoncial-tag on paginated sites' 
+           UPDATE `s_core_config_element_translations` SET `label` = 'Use prev/next-tag on paginated sites',
+           `description` = 'If active, use prev/next-tag instead of the Canoncial-tag on paginated sites'
            WHERE `element_id` = @elementId;
 EOD;
         $this->addSql($sql);
