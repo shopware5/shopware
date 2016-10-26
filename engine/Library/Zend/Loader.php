a111bf0f13f66236e800bbf49c7808be1e131e37
Benjamin Cremer <b.cremer@shopware.com>
2016-10-26 11:00:03 +0200

    Remove unused Zend_Loader_* autoloaders

--- engine/Library/Zend/Loader.php
+++ engine/Library/Zend/Loader.php
@@ -230,13 +230,7 @@ class Zend_Loader
      */
     public static function autoload($class)
     {
-        trigger_error(__CLASS__ . '::' . __METHOD__ . ' is deprecated as of 1.8.0 and will be removed with 2.0.0; use Zend_Loader_Autoloader instead', E_USER_NOTICE);
-        try {
-            @self::loadClass($class);
-            return $class;
-        } catch (Exception $e) {
-            return false;
-        }
+        throw new \RuntimeException(__CLASS__ . '::' . __METHOD__ . ' was removed.');
     }
 
     /**
@@ -251,27 +245,7 @@ class Zend_Loader
      */
     public static function registerAutoload($class = 'Zend_Loader', $enabled = true)
     {
-        trigger_error(__CLASS__ . '::' . __METHOD__ . ' is deprecated as of 1.8.0 and will be removed with 2.0.0; use Zend_Loader_Autoloader instead', E_USER_NOTICE);
-        require_once 'Zend/Loader/Autoloader.php';
-        $autoloader = Zend_Loader_Autoloader::getInstance();
-        $autoloader->setFallbackAutoloader(true);
-
-        if ('Zend_Loader' != $class) {
-            self::loadClass($class);
-            $methods = get_class_methods($class);
-            if (!in_array('autoload', (array) $methods)) {
-                require_once 'Zend/Exception.php';
-                throw new Zend_Exception("The class \"$class\" does not have an autoload() method");
-            }
-
-            $callback = array($class, 'autoload');
-
-            if ($enabled) {
-                $autoloader->pushAutoloader($callback);
-            } else {
-                $autoloader->removeAutoloader($callback);
-            }
-        }
+        throw new \RuntimeException(__CLASS__ . '::' . __METHOD__ . ' was removed.');
     }
 
     /**
@@ -338,6 +312,6 @@ class Zend_Loader
             $file      = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
         }
         $file .= str_replace('_', DIRECTORY_SEPARATOR, $fileName) . '.php';
-        return $file;    
+        return $file;
     }
 }
