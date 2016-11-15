57b22d084256ccfdc74bfa0590f73cbd3cd68a80
OliverSkroblin <o.skroblin@shopware.com>
2016-11-15 12:11:04 +0100

    SW-16959 - Fix salutation snippet migration

--- _sql/migrations/794-remove-salutation-snippets.php
+++ _sql/migrations/794-remove-salutation-snippets.php
@@ -4,9 +4,10 @@ class Migrations_Migration794 extends Shopware\Components\Migrations\AbstractMig
 {
     public function up($modus)
     {
-        $sql = <<<'EOD'
-        DELETE FROM s_core_snippets WHERE dirty = 0 AND namespace = 'frontend/salutation' AND value = '';
-EOD;
+        $statement = $this->connection->query("SELECT id, locale_id FROM s_core_shops WHERE `default` = 1 LIMIT 1");
+        $shop = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $shop = array_shift($shop);
+        $sql = "DELETE FROM s_core_snippets WHERE dirty = 0 AND namespace = 'frontend/salutation' AND value = '' AND shopID != " . (int) $shop['id'] . " AND localeID != " . (int) $shop['locale_id'];
         $this->addSql($sql);
     }
 }
