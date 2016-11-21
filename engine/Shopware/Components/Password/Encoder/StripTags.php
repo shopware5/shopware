0b681380d83fe4377c979c0fe1fffd1d8b1dd7da
Julian Krzefski <julian.krzefski@gmail.com>
2016-11-21 16:17:18 +0100

    StyleCI again

--- engine/Shopware/Components/Password/Encoder/StripTags.php
+++ engine/Shopware/Components/Password/Encoder/StripTags.php
@@ -2,47 +2,47 @@
 
 class StripTags implements PasswordEncoderInterface
 {
-	/**
-	 * @return string
-	 */
-	public function getName()
-	{
-		return 'StripTags';
-	}
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'StripTags';
+    }
 
-	/**
-	 * @return boolean
-	 */
-	public function isCompatible()
-	{
-		return version_compare(PHP_VERSION, '5.3.7', '>=');
-	}
+    /**
+     * @return boolean
+     */
+    public function isCompatible()
+    {
+        return version_compare(PHP_VERSION, '5.3.7', '>=');
+    }
 
-	/**
-	 * @param  string $password
-	 * @param  string $hash
-	 * @return bool
-	 */
-	public function isPasswordValid($password, $hash)
-	{
-		return password_verify(strip_tags($password), $hash);
-	}
+    /**
+     * @param  string $password
+     * @param  string $hash
+     * @return bool
+     */
+    public function isPasswordValid($password, $hash)
+    {
+        return password_verify(strip_tags($password), $hash);
+    }
 
-	/**
-	 * @param  string $password
-	 * @return string
-	 */
-	public function encodePassword($password)
-	{
-		throw new Exception('Shoud never be called.');
-	}
+    /**
+     * @param  string $password
+     * @return string
+     */
+    public function encodePassword($password)
+    {
+        throw new Exception('Shoud never be called.');
+    }
 
-	/**
-	 * @param  string $hash
-	 * @return bool
-	 */
-	public function isReencodeNeeded($hash)
-	{
-		return true;
-	}
+    /**
+     * @param  string $hash
+     * @return bool
+     */
+    public function isReencodeNeeded($hash)
+    {
+        return true;
+    }
 }
