97b4194334fba32218ce1609b103793c10350a3a
C. Holst <ch@synergy-networks.de>
2016-11-22 16:36:27 +0100

    Update Shopware.form.field.ComboTree.js
    Beim Laden der Werte einer Form, werden diese über die setValue Methode an die Komponenten weitergegeben. Beim ComboTree wird der gesetzte Wert aber nicht an das Inputelement weitergegeben und deshalb dort nicht angezeigt. Dazu muß zusätzlich die setRawValue Methode aufgerufen werden.
    
    VG
    JM

--- themes/Backend/ExtJs/backend/base/component/Shopware.form.field.ComboTree.js
+++ themes/Backend/ExtJs/backend/base/component/Shopware.form.field.ComboTree.js
@@ -162,6 +162,14 @@ Ext.define('Shopware.form.field.ComboTree', {
             inputEl.removeCls(me.emptyCls);
         }
         me.value = value;
+        
+        /** 
+        * Setzen von rawValue, sonst wird der Wert im Inputelement nicht angezeigt - SN - 22.11.2016
+        */
+        if (inputEl && !Ext.isEmpty(value)){
+            me.setRawValue(value); 
+        }  
+
         me.applyEmptyText();
     },
 
