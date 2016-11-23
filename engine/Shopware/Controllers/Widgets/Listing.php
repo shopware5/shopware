d251eb0646cc16a172a6d5196dbe2840acab047d
OliverSkroblin <o.skroblin@shopware.com>
2016-11-23 13:43:57 +0100

    SW-16691 - Fix product stream filter for new filter mode

--- engine/Shopware/Controllers/Widgets/Listing.php
+++ engine/Shopware/Controllers/Widgets/Listing.php
@@ -334,7 +334,7 @@ class Shopware_Controllers_Widgets_Listing extends Enlight_Controller_Action
         return new ProductNumberSearchResult(
             $result->getProducts(),
             $result->getTotalCount(),
-            $facetFilter->filter($criteria->getFacets(), $criteria)
+            $facetFilter->filter($result->getFacets(), $criteria)
         );
     }
 
