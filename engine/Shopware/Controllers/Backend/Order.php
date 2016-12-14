7fca64f3cfb46f5b13132dabdf10a3dd4b87835c
Marcel Schmäing <ms@shopware.com>
2016-12-14 13:34:50 +0100

    SW-17209 - Add default sorting to the order and payment status query
    fixes https://github.com/shopware/shopware/pull/881

--- engine/Shopware/Controllers/Backend/Order.php
+++ engine/Shopware/Controllers/Backend/Order.php
@@ -263,21 +263,27 @@ class Shopware_Controllers_Backend_Order extends Shopware_Controllers_Backend_Ex
     }
 
     /**
-     * This class has its own OrderStatusQuery as we need to get rid of states with satus.id = -1
+     * This class has its own OrderStatusQuery as we need to get rid of states with status.id = -1
+     * @param array|null $filter
+     * @param array|null $order
+     * @param int|null $offset
+     * @param int|null $limit
+     * @return \Doctrine\ORM\Query
      */
     public function getOrderStatusQuery($filter = null, $order = null, $offset = null, $limit = null)
     {
         $builder = Shopware()->Models()->createQueryBuilder();
         $builder->select(array('status'))
             ->from('Shopware\Models\Order\Status', 'status')
-            ->andWhere("status.group = 'state'")
-            ->orderBy( "status.position", "ASC" );
+            ->andWhere("status.group = 'state'");
 
         if ($filter !== null) {
             $builder->addFilter($filter);
         }
         if ($order !== null) {
             $builder->addOrderBy($order);
+        } else {
+            $builder->orderBy('status.position', 'ASC');
         }
 
         if ($offset !== null) {
