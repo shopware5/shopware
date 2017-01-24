ce0c637f2c7b5c843ca61730078b7b959ecd2e69
OliverSkroblin <o.skroblin@shopware.com>
2017-01-24 12:34:02 +0100

    SW-16633 - Increase migration numbers

--- _sql/migrations/918-add-facet-module.php
+++ _sql/migrations/918-add-facet-module.php
@@ -7,7 +7,7 @@ use Shopware\Bundle\SearchBundle\Sorting\ReleaseDateSorting;
 use Shopware\Bundle\SearchBundle\Sorting\SearchRankingSorting;
 use Shopware\Components\Migrations\AbstractMigration;
 
-class Migrations_Migration917 extends AbstractMigration
+class Migrations_Migration918 extends AbstractMigration
 {
     /**
      * @inheritdoc
