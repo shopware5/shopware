8a8233058c33cb6f0686954ad658e3f1e19ea8e6
Jochen Manz <jochen.manz@gmx.de>
2017-01-30 16:46:45 +0100

    fix column exists check

--- _sql/migrations/815-remove-base-price-column.php
+++ _sql/migrations/815-remove-base-price-column.php
@@ -4,10 +4,12 @@ class Migrations_Migration815 extends Shopware\Components\Migrations\AbstractMig
 {
     public function up($modus)
     {
-        $statement = $this->connection->query('SELECT * FROM s_articles_prices');
-        $price = $statement->fetch(PDO::FETCH_ASSOC);
+        $statement = $this->connection->prepare('SHOW COLUMNS FROM s_articles_prices LIKE :fieldname');
+        $statement->execute([':fieldname' => 'baseprice']);
 
-        if (array_key_exists('baseprice', $price)) {
+        $columnExists = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if ($columnExists) {
             $sql = 'ALTER TABLE `s_articles_prices` DROP `baseprice`;';
             $this->addSql($sql);
         }
