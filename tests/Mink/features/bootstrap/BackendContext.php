65cf04166b874fee403f53d70319aa6146773401
Niklas Dzösch <nd@shopware.com>
2017-02-20 15:00:24 +0100

    SW-17850 - Fix backend login feature

--- tests/Mink/features/bootstrap/BackendContext.php
+++ tests/Mink/features/bootstrap/BackendContext.php
@@ -24,29 +24,93 @@
 
 namespace Shopware\Tests\Mink;
 
-use Behat\Behat\Tester\Exception\PendingException;
+use Shopware\Tests\Mink\Page\Backend;
 
 class BackendContext extends SubContext
 {
+    /**
+     * Based on Behat's own example
+     * @see http://docs.behat.org/en/v2.5/cookbook/using_spin_functions.html#adding-a-timeout
+     * @param $lambda
+     * @param int $wait
+     * @return bool
+     */
+    protected function spinWithNoException($lambda, $wait = 60)
+    {
+        $time = time();
+        $stopTime = $time + $wait;
+        while (time() < $stopTime) {
+            try {
+                if ($lambda($this)) {
+                    return true;
+                }
+            } catch (\Exception $e) {
+                // do nothing
+            }
+
+            usleep(250000);
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks via a string exists
+     * @param string $text
+     * @param SubContext $context
+     * @return bool
+     */
+    protected function checkIfThereIsText($text, SubContext $context)
+    {
+        $result = $context->getSession()->getPage()->findAll('xpath', "//*[contains(., '$text')]");
+        return !empty($result);
+    }
+
+    /**
+     * Checks via spin function if a string exists, with sleep at the beginning (default 2)
+     * @param string $text
+     * @param int $sleep
+     */
+    protected function waitForText($text, $sleep = 2)
+    {
+        sleep($sleep);
+        $this->spin(function (SubContext $context) use ($text) {
+            return $this->checkIfThereIsText($text, $context);
+        });
+    }
+
+    /**
+     * Checks via spin function if a string exists, with sleep at the beginning (default 2)
+     * @param string $text
+     * @param int $wait
+     * @return bool
+     */
+    protected function waitIfThereIsText($text, $wait = 5)
+    {
+        return $this->spinWithNoException(function (SubContext $context) use ($text) {
+            return $this->checkIfThereIsText($text, $context);
+        }, $wait);
+    }
+
     /**
      * @Given /^I am logged in to the backend as an admin user$/
      */
     public function iAmLoggedInToTheBackendAsAnAdminUser()
     {
+        /** @var Backend $page */
         $page = $this->getPage('Backend');
         $page->open();
 
-        $this->spin(function ($context) use ($page) {
-            return $page->verifyLogin();
-        });
+        // See if we already are logged in
+        if ($this->waitIfThereIsText('Marketing', 5)) {
+            return;
+        }
 
-        $this->spin(function ($context) use ($page) {
-            return $page->login('demo', 'demo');
-        });
+        // waitForText() is defined in Trait
+        $this->waitForText('Shopware Backend Login', 10);
 
-        $this->spin(function ($context) use ($page) {
-            return $page->verifyIsLoggedIn();
-        });
+        $page->login('demo', 'demo');
+        $this->waitForText('Marketing');
     }
 
     /**
