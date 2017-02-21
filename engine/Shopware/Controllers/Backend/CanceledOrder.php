8250a6698a19441e474c76eb1f7aeff09aa5f8e3
Gugiman <Gugiman@users.noreply.github.com>
2017-02-21 12:36:05 +0100

    Update CanceledOrder.php

--- engine/Shopware/Controllers/Backend/CanceledOrder.php
+++ engine/Shopware/Controllers/Backend/CanceledOrder.php
@@ -668,8 +668,7 @@ class Shopware_Controllers_Backend_CanceledOrder extends Shopware_Controllers_Ba
         $total = $paginator->count();
         $orders = $paginator->getIterator()->getArrayCopy();
 
-        if(strtolower($this->format) == "csv") {
-
+        if (strtolower($this->format) == "csv") {
             foreach ($orders as &$value) {
 
                 /* Filter irrelevant values for export*/
@@ -690,7 +689,7 @@ class Shopware_Controllers_Backend_CanceledOrder extends Shopware_Controllers_Ba
                 unset($value["details"]);
 
                 /* Parse data*/
-                if($value["orderTime"] instanceof \DateTime) {
+                if ($value["orderTime"] instanceof \DateTime) {
                     $value["orderTime"] = $value["orderTime"]->format("d.m.Y");
                 }
 
