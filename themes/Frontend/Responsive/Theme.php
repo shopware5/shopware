32e3a8bea06c7c8eaf09abbf60e0640d7b12b43f
Timo Altholtmann <T.Altholtmann@shopware.com>
2017-02-28 16:39:44 +0100

    SW-17882 - Move advanced menu views to shopware themes

--- themes/Frontend/Responsive/Theme.php
+++ themes/Frontend/Responsive/Theme.php
@@ -138,6 +138,7 @@ class Theme extends \Shopware\Components\Theme
         'src/js/jquery.address-editor.js',
         'src/js/jquery.cookie-permission.js',
         'src/js/jquery.shopware-responsive.js',
+        'src/js/jquery.advanced-menu.js'
     ];
 
     /**
