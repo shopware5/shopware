d8ed754f552dcdcd5a44fc30a08d47d334623676
Hendrik Söbbing <h.soebbing@shopware.com>
2017-03-03 09:42:11 +0100

    SW-17131 - Fixe duplicate migration ids, make test less rigid

--- _sql/migrations/818-make-some-fields-in-order-table-nullable.php
+++ _sql/migrations/818-make-some-fields-in-order-table-nullable.php
@@ -1,6 +1,6 @@
 <?php
 
-class Migrations_Migration816 extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration818 extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up($modus)
     {
