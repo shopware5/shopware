2a2c0cd567a8fc4b797c37d1bf96c0d9a92370ca
Oliver Skroblin <o.skroblin@shopware.com>
2017-03-16 10:38:28 +0100

    SW-17886 - Fix migration number

--- _sql/migrations/1006-remove-statistics-plugin.php
+++ _sql/migrations/1006-remove-statistics-plugin.php
@@ -22,7 +22,7 @@
  * our trademarks remain entirely with us.
  */
 
-class Migrations_Migration1005 extends Shopware\Components\Migrations\AbstractMigration
+class Migrations_Migration1006 extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up($modus)
     {
