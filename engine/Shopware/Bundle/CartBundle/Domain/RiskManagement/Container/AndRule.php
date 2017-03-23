a276b422498e6f95f3a529ad8d4869602cf31381
Oliver Skroblin <o.skroblin@shopware.com>
2017-03-23 16:08:05 +0100

    SW-18226 - Implement first rules for risk management

---
+++ engine/Shopware/Bundle/CartBundle/Domain/RiskManagement/Container/AndRule.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+namespace Shopware\Bundle\CartBundle\Domain\RiskManagement\Container;
+
+use Shopware\Bundle\CartBundle\Domain\Cart\CalculatedCart;
+use Shopware\Bundle\StoreFrontBundle\Struct\ShopContextInterface;
+
+/**
+ * AndRule returns true, if all child-rules are true
+ */
+class AndRule extends Container
+{
+    public function validate(CalculatedCart $cart, ShopContextInterface $context): bool
+    {
+        foreach ($this->rules as $rule) {
+            if (!$rule->validate($cart, $context)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
