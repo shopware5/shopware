7496c41b7f162de2410ab290c9ce723ad68409cf
Jens Küper <J.Kueper@shopware.com>
2017-03-30 15:50:51 +0200

    SW-18009 - Updated a few lables in step 6

--- recovery/install/data/lang/de.php
+++ recovery/install/data/lang/de.php
@@ -46,10 +46,10 @@ return [
     'language-selection_info_message' => 'Die hier zu wählende Sprache bezieht sich nur auf den Installationsassistenten, die Sprache Deines Shops kannst Du später definieren.',
     'language-selection_welcome_message' => <<<EOT
 <p>
-    Wir freuen uns, dass Du Teil unserer unserer großartigen, weltweiten Shopware Community werden möchtest.
+    Wir freuen uns, dass Du Teil unserer großartigen, weltweiten Shopware Community werden möchtest.
 </p>
 <p>
-    Schritt für Schritt begleiten wir Dich jetzt durch Deinen Installationsprozess. Solltest Du Fragen haben, schaue einfach in unser <a href="https://forum.shopware.com" target="_blank">Forum</a> oder kontaktiere uns telefonisch unter <a href="tel:+492555928850">(+49) 2555 928850</a> oder per <a href="mailto:info@shopware.com">Mail</a>.
+    Schritt für Schritt begleiten wir Dich jetzt durch Deinen Installationsprozess. Solltest Du Fragen haben, schaue einfach in unser <a href="https://forum.shopware.com" target="_blank">Forum</a> oder kontaktiere uns telefonisch unter <a href="tel:+492555928850">(+49) 2555 928850</a> oder per <a href="mailto:info@shopware.com">E-Mail</a>.
 </p>
 <p>
     <strong>Los geht's</strong>
@@ -59,13 +59,13 @@ EOT
     'requirements_header' => 'Systemvoraussetzungen',
     'requirements_header_files' => 'Dateien und Verzeichnisse',
     'requirements_header_system' => 'System',
-    'requirements_files_info' => 'Die nachfolgenden Dateien und Verzeichnisse müssen vorhanden sein und Schreibrechte besitzen',   // The following files and directories must exist and be writable
+    'requirements_files_info' => 'Die nachfolgenden Dateien und Verzeichnisse müssen vorhanden sein und Schreibrechte besitzen',
     'requirements_tablefiles_colcheck' => 'Datei/Verzeichnis',
     'requirements_tablefiles_colstatus' => 'Status',
-    'requirements_error' => '<h3 class="alert-heading">Achtung!</h3>Es wurden nicht alle Voraussetzungen für eine erfolgreiche Installation erfüllt',
-    'requirements_success'              => '<h3 class="alert-heading">Glückwunsch!</h3>Alle Voraussetzungen für eine erfolgreiche Installation erfüllt',
+    'requirements_error' => '<h3 class="alert-heading">Achtung!</h3>Es sind nicht alle Voraussetzungen für eine erfolgreiche Installation erfüllt',
+    'requirements_success'              => '<h3 class="alert-heading">Glückwunsch!</h3>Alle Voraussetzungen für eine erfolgreiche Installation sind erfüllt',
     'requirements_ioncube'              => '<small><strong>* Hinweis:</strong> Auf Deinem System ist die Codierungs-Software ionCube nicht installiert. Diese wird nur benötigt, wenn Du Dir später über unseren <a href="https://store.shopware.com" target="_blank">Shopware Community Store</a> Erweiterungen installieren möchtest, die mit ionCube verschlüsselt sind. Du kannst die Installation von ionCube auch jederzeit nachholen.</small>',
-    'requirements_php_info' => 'Ihr Server muss die folgenden Systemvoraussetzungen erfüllen, damit Shopware lauffähig ist', // Your server must meet the following requirements in order to run shopware
+    'requirements_php_info' => 'Ihr Server muss die folgenden Systemvoraussetzungen erfüllen, damit Shopware lauffähig ist',
     'requirements_system_colcheck' => 'Voraussetzung',
     'requirements_system_colrequired' => 'Erforderlich',
     'requirements_system_colfound' => 'Ihr System',
@@ -86,7 +86,7 @@ EOT
     'database-configuration_field_user' => 'Datenbank Benutzer:',
     'database-configuration_field_password' => 'Datenbank Passwort:',
     'database-configuration_field_database' => 'Datenbank Name:',
-    'database-configuration_info' => 'Um Shopware auf Deinem System zu installieren, werden die Zugangsdaten zur Datenbank benötigt. Wenn Du Dir nicht sicher bist, was du eintragen musst, kontaktiere Deinen Administrator / Hoster.', // Enter your database connection - if you are not sure what data must be entered here, please contact your provider.
+    'database-configuration_info' => 'Um Shopware auf Deinem System zu installieren, werden die Zugangsdaten zur Datenbank benötigt. Wenn Du Dir nicht sicher bist, was Du eintragen musst, kontaktiere Deinen Administrator / Hoster.',
     'database-configuration-create_new_database' => 'Neue Datenbank anlegen',
 
     'database-import_header'            => 'Installation',
@@ -97,22 +97,22 @@ EOT
     'migration_counter_text_snippets' => 'Textbausteine werden aktualisiert',
     'migration_update_success' => 'Datenbank erfolgreich importiert!',
 
-    'edition_header' => 'Wählen Sie die Lizenz unter der Sie Shopware verwenden möchten',
-    'edition_info' => 'Wenn Sie eine kommerzielle Shopware Lizenz erworben haben, so wählen Sie bitte die zutreffene Edition aus der Liste und geben Sie Ihren Lizenzschlüssel ein.',
-    'edition_ce' => 'Shopware Community Edition (Lizenz: Open-Source AGPL)',
-    'edition_cm' => 'Shopware kommerzielle Version (Lizenz: Kommerziell, Lizenzschlüssel erforderlich) Bspw. Professional, Professional Plus, Enterprise',
-    'edition_license' => 'Lizenz-Schlüssel:',
-    'edition_license_error' => 'Für die Installation einer kommerziellen Shopware Version ist eine gültige Lizenz erfordelich.',
+    'edition_header'        => 'Hast Du eine  Shopware -Lizenz erworben ?',
+    'edition_info'          => 'Shopware gibt es in einer kostenlosen <a href="https://de.shopware.com/versionen/" target="_blank">Community Edition </a> sowie in kostenpflichtigen <a href="https://de.shopware.com/versionen/" target="_blank">Professional, Professional Plus und Enterprise Editionen</a>.',
+    'edition_ce'            => 'Nein, ich möchte die kostenfreie <a href="https://de.shopware.com/versionen/" target="_blank"> Community Edition </a> verwenden.',
+    'edition_cm'            => 'Ja, ich habe eine kostenpflichtigeShopware -Lizenz(<a href="https://de.shopware.com/versionen/" target="_blank"> Professional, Professional Plusoder Enterprise</a>).',
+    'edition_license'       => 'Dann trage hier Deinen Lizenzschlüssel ein. Diesen findest Du in Deinem Shopware Account unter "Lizenzen" -> "Produktlizenzen" -> "Details / Download":',
+    'edition_license_error' => 'Für die Installation einer kostenpflichtigen Shopware Version ist eine gültige Lizenz erfordelich.',
 
     'configuration_header'                  => 'Shop Grundeinrichtung',
-    'configuration_sconfig_text'            => 'Es ist fast geschafft! Jetzt musst Du nur noch einige grundlegende Einstellungenfür Deinen Shop festlegen, dann ist die Installation abgeschlossen. Alles was Du hier einträgst, kannst Du natürlich nachträglich wieder ändern!',
-    'configuration_sconfig_name'            => 'Name des Shops:',
-    'configuration_sconfig_name_info'       => 'Bitte geben Sie den Namen Ihres Shops ein',
-    'configuration_sconfig_mail'            => 'Ihre E-Mail-Adresse:',
-    'configuration_sconfig_mail_info'       => 'Bitte geben Sie Ihre E-Mail-Adresse für ausgehende E-Mails ein',
+    'configuration_sconfig_text'            => 'Fast geschafft! Jetzt musst Du nur noch einige grundlegende Einstellungenfür Deinen Shop festlegen, dann ist die Installation abgeschlossen. Alles was Du hier einträgst, kannst Du natürlich nachträglich wieder ändern!',
+    'configuration_sconfig_name'            => 'Name Deines Shops:',
+    'configuration_sconfig_name_info'       => 'Bitte gib den Namen Deines Shops ein',
+    'configuration_sconfig_mail'            => 'E-Mail-Adresse des Shops:',
+    'configuration_sconfig_mail_info'       => 'Bitte gib Deine E-Mail-Adresse für ausgehende E-Mails ein',
     'configuration_sconfig_domain'          => 'Shop-Domain:',
-    'configuration_sconfig_language'        => 'Basissprache:',
-    'configuration_sconfig_currency'        => 'Shop-Default-Währung:',
+    'configuration_sconfig_language'        => 'Hauptsprache:',
+    'configuration_sconfig_currency'        => 'Standardwährung:',
     'configuration_sconfig_currency_info'   => 'Diese Währung wird standardmäßig genutzt, wenn Artikel Preise definiert werden',
     'configuration_admin_currency_eur'      => 'Euro',
     'configuration_admin_currency_usd'      => 'Dollar (US)',
@@ -134,9 +134,9 @@ EOT
     Herzlich Willkommen bei Shopware.
 </p>
 <p>
-    Wir freuen uns Sie in unserer Community begrüßen zu dürfen. Shopware ist nun erfolgreich installiert und ab sofort einsatzbereit. Gerne unterstützt Sie unser Assistent, welcher sich automatisch beim Aufruf Ihrer Shopware Administrationsoberfläche öffnet, bei der weiteren Einrichtung.
+    Wir freuen uns Dich in unserer Community begrüßen zu dürfen. Shopware ist nun erfolgreich installiert und ab sofort einsatzbereit. Gerne unterstützt Dich unser Assistent, welcher sich automatisch beim Aufruf Ihrer Shopware Administrationsoberfläche öffnet, bei der weiteren Einrichtung.
 </p>
 <p>
-    Darüber hinaus hilft Ihnen unser <a target="_blank" href="http://wiki.shopware.com/_detail_930.html">Guide</a> bei Ihren „ersten Schritten".
+    Darüber hinaus hilft Dir unser <a target="_blank" href="http://wiki.shopware.com/_detail_930.html">Guide</a> bei Deinen „ersten Schritten".
 </p>',
 ];
