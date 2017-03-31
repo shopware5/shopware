9d58a33ea7d10e8cdeb389f5217aa6ff55f7b1cf
Soner Sayakci <s.sayakci@gmail.com>
2017-03-31 20:30:52 +0200

    Fixed redirect of inactive categories

--- engine/Shopware/Controllers/Frontend/Listing.php
+++ engine/Shopware/Controllers/Frontend/Listing.php
@@ -246,9 +246,15 @@ class Shopware_Controllers_Frontend_Listing extends Enlight_Controller_Action
         if (!empty($categoryContent['external'])) {
             $location = $categoryContent['external'];
         } elseif (empty($categoryContent)) {
-            $location = array('controller' => 'index');
+            throw new \Enlight_Controller_Exception(
+                'Category not found',
+                Enlight_Controller_Exception::Controller_Dispatcher_Controller_Not_Found
+            );
         } elseif ($this->isShopsBaseCategoryPage($categoryContent['id'])) {
-            $location = array('controller' => 'index');
+            throw new \Enlight_Controller_Exception(
+                'Category not found',
+                Enlight_Controller_Exception::Controller_Dispatcher_Controller_Not_Found
+            );
         } elseif ($this->get('config')->get('categoryDetailLink') && $checkRedirect) {
             /**@var $context ShopContextInterface*/
             $context = $this->get('shopware_storefront.context_service')->getShopContext();
