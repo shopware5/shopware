c29420cdfbc4895e38c05ae554a559d665d9b2bf
Jens Küper <J.Kueper@shopware.com>
2017-04-27 10:36:12 +0200

    SW-18391 - Bugfix

--- engine/Shopware/Kernel.php
+++ engine/Shopware/Kernel.php
@@ -28,7 +28,7 @@ use Enlight_Controller_Request_RequestHttp as EnlightRequest;
 use Enlight_Controller_Response_ResponseHttp as EnlightResponse;
 use Shopware\Bundle\AttributeBundle\DependencyInjection\Compiler\SearchRepositoryCompilerPass;
 use Shopware\Bundle\ControllerBundle\DependencyInjection\Compiler\RegisterControllerCompilerPass;
-use Shopware\Bundle\CustomerSearchBundle\DependencyInjection\Compiler\HandlerRegistryCompilerPass;
+use Shopware\Bundle\CustomerSearchBundleDBAL\DependencyInjection\Compiler\HandlerRegistryCompilerPass;
 use Shopware\Bundle\EmotionBundle\DependencyInjection\Compiler\EmotionComponentHandlerCompilerPass;
 use Shopware\Bundle\ESIndexingBundle\DependencyInjection\CompilerPass\DataIndexerCompilerPass;
 use Shopware\Bundle\ESIndexingBundle\DependencyInjection\CompilerPass\MappingCompilerPass;
@@ -609,7 +609,7 @@ class Kernel implements HttpKernelInterface
         $loader->load('AttributeBundle/services.xml');
         $loader->load('EmotionBundle/services.xml');
         $loader->load('SearchBundleES/services.xml');
-        $loader->load('CustomerSearchBundle/services.xml');
+        $loader->load('CustomerSearchBundleDBAL/services.xml');
 
         if (is_file($file = __DIR__ . '/Components/DependencyInjection/services_local.xml')) {
             $loader->load($file);
