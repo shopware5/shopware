074f83064c382f4b41a501eaefaf101c2d99c57d
Paul Hörig <paul_hoerig@egoist.de>
2017-05-16 17:35:18 +0200

    convert the doctrine model to array

--- engine/Shopware/Controllers/Frontend/Forms.php
+++ engine/Shopware/Controllers/Frontend/Forms.php
@@ -218,7 +218,7 @@ class Shopware_Controllers_Frontend_Forms extends Enlight_Controller_Action
             'metaTitle' => $form->getMetaTitle(),
             'metaDescription' => $form->getMetaDescription(),
             'metaKeywords' => $form->getMetaKeywords(),
-            'attribute' => $form->getAttribute(),
+            'attribute' => (array)$form->getAttribute(),
         ];
 
         return array_merge($formData, [
