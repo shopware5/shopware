cb9355113550988176a48ab4290757a2e866c01c
Philipp Schuch <psc@shopware.com>
2017-05-18 10:38:47 +0200

    SW-18506 - Fixed mime type of statistics call

--- engine/Shopware/Controllers/Widgets/Index.php
+++ engine/Shopware/Controllers/Widgets/Index.php
@@ -48,6 +48,9 @@ class Shopware_Controllers_Widgets_Index extends Enlight_Controller_Action
         /** @var $plugin Shopware_Plugins_Frontend_Statistics_Bootstrap */
         $plugin = Shopware()->Plugins()->Frontend()->Statistics();
         $plugin->updateLog($request, $response);
+
+        /** Setting mime type for jsonp request */
+        $this->Response()->setHeader('Content-type', 'application/javascript', true);
     }
 
     /**
