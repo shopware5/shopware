d791bf8dfd65be5c315a345c0025343e98c44d45
Jens Küper <J.Kueper@shopware.com>
2017-06-23 11:47:09 +0200

    SW-18956 - Add migration to prevent two Import/Export entries in the backend menu

--- _sql/migrations/903-add-menu-import-export-advanced.php
+++ _sql/migrations/903-add-menu-import-export-advanced.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 class Migrations_Migration903 extends Shopware\Components\Migrations\AbstractMigration
 {
@@ -25,7 +47,7 @@ SQL;
     private function addNewMenuEntry()
     {
         $sql = <<<'EOD'
-INSERT INTO `s_core_menu` (`id`, `parent`, `name`, `onclick`, `class`, `position`, `active`, `pluginID`, `controller`, `shortcut`, `action`)
+INSERT IGNORE INTO `s_core_menu` (`id`, `parent`, `name`, `onclick`, `class`, `position`, `active`, `pluginID`, `controller`, `shortcut`, `action`)
 VALUES (NULL, @parentId, 'Import/Export', NULL, 'sprite-arrow-circle-double-135 contents--import-export', '3', '1', NULL, 'PluginManager', NULL, 'ImportExport');
 EOD;
         $this->addSql($sql);
