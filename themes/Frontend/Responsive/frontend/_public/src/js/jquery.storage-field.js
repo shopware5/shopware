1a0771b60fbe935843c736504ab5f985d80aa1b3
Philipp Schuch <psc@shopware.com>
2017-07-05 09:10:39 +0200

    SW-15949 - Refactored plugin for storing form fields in local storage

---
+++ themes/Frontend/Responsive/frontend/_public/src/js/jquery.storage-field.js
@@ -0,0 +1,119 @@
+;(function($, window) {
+    'use strict';
+
+    /**
+     * Storage Field Plugin
+     *
+     * The plugin stores the content of a form field in the local storage of the browser.
+     * This is in case the user performs an action that causes a page reload.
+     * The Plugin will then populate the field when the page is reloaded
+     */
+    $.plugin('swStorageField', {
+
+        defaults: {
+
+            storageType: 'session',
+
+            storageKeyPrefix: 'sw-local-',
+
+            storageKeyName: null,
+
+            storeEvent: 'blur'
+        },
+
+        init: function () {
+            var me = this;
+
+            me.applyDataAttributes();
+
+            me.storage = window.StorageManager.getStorage(me.opts.storageType);
+
+            me.storageKey = me.getStorageKey();
+
+            me.$form = me.getParentForm();
+
+            me.setFieldValueFromStorage();
+            me.registerEvents();
+
+            $.publish('plugin/swStorageField/init', [ me ]);
+        },
+
+        getStorageKey: function () {
+            var me = this,
+                fieldName = me.$el.attr('name'),
+                key = me.opts.storageKeyPrefix;
+
+            if (me.opts.storageKeyName !== null) {
+                key += me.opts.storageKeyName.toLowerCase();
+
+            } else if (fieldName && fieldName.length) {
+                key += fieldName.toLowerCase();
+            }
+
+            $.publish('plugin/swStorageField/getStorageKey', [ me, key ]);
+
+            return key;
+        },
+
+        getParentForm: function () {
+            var me = this,
+                $form = me.$el.parents('form');
+
+            // The field is just a pseudo field for another field
+            if (me.$el.is('[data-selector]')) {
+                $form = $(me.$el.attr('data-selector')).parents('form');
+            }
+
+            $.publish('plugin/swStorageField/getParentForm', [ me, $form ]);
+
+            return ($form.length > 0) ? $form : null;
+        },
+
+        setFieldValueFromStorage: function () {
+            var me = this,
+                value = me.storage.getItem(me.storageKey);
+
+            if (value && value.length) {
+                me.$el.val(value);
+            }
+
+            $.publish('plugin/swStorageField/setFieldValueFromStorage', [ me ]);
+        },
+
+        registerEvents: function () {
+            var me = this;
+
+            me._on(me.$el, me.opts.storeEvent, $.proxy(me.storeValue, me));
+
+            if (me.$form && me.$form !== null) {
+                me._on(me.$form, 'submit', $.proxy(me.onFormSubmit, me));
+            }
+
+            $.publish('plugin/swStorageField/onRegisterEvents', [ me ]);
+        },
+
+        storeValue: function () {
+            var me = this,
+                value = me.$el.val();
+
+            me.storage.setItem(me.storageKey, value);
+
+            $.publish('plugin/swStorageField/storeValue', [ me ]);
+        },
+
+        onFormSubmit: function () {
+            var me = this;
+
+            me.storage.removeItem(me.storageKey);
+
+            $.publish('plugin/swStorageField/onFormSubmit', [ me ]);
+        },
+
+        destroy: function() {
+            var me = this;
+
+            me._destroy();
+        }
+    });
+
+})(jQuery, window);
