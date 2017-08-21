4acce35a364a1f8b63120082f75e0f4294abb973
Thomas Eiling <teiling88@gmail.com>
2017-08-21 14:34:24 +0200

    SW-19241 - add nexus-dev.sh

--- dev-ops/common/actions/nexus-init.sh
+++ dev-ops/common/actions/nexus-init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#DESCRIPTION: init nexus dependencies
+#DESCRIPTION: init nexus dependencies for docker setups
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo bash
 sudo apt-get install nodejs -y
