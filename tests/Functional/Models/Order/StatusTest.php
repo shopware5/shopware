8239bfe7a3326f79fbde64bb0bf787c8aa555c45
Joshua Behrens <code@joshua-behrens.de>
2017-10-29 14:05:22 +0100

    SW-20376 - Update Status-model, made id writable as it is not an autoincrement column on the database fixes 1355

---
+++ tests/Functional/Models/Order/StatusTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+/**
+ * @category  Shopware
+ *
+ * @copyright Copyright (c) shopware AG (http://www.shopware.de)
+ */
+class Shopware_Tests_Models_Order_StatusTest extends Enlight_Components_Test_TestCase
+{
+    public function testStatusIsPersistableViaOrm()
+    {
+        $order = new \Shopware\Models\Order\Status();
+        $order->setId(123456789);
+        $order->setPosition(123456789);
+        $order->setSendMail(0);
+        $order->setName('prettyIgnorableNameForThisTest');
+        $order->setDescription('prettyIgnorableNameForThisTest');
+        $order->setGroup(\Shopware\Models\Order\Status::GROUP_PAYMENT);
+
+        Shopware()->Models()->persist($order);
+        Shopware()->Models()->flush($order);
+
+        // cleanup
+        Shopware()->Models()->remove($order);
+        Shopware()->Models()->flush($order);
+        Shopware()->Models()->clear();
+    }
+}
