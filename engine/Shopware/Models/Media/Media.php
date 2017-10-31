8bffcedb6f91960a54502583a96fd7f06de87d92
wbob <wbob@buerotiger.de>
2017-10-31 22:31:18 +0100

    Fix regex in removeSpecialCharacters to filter all symbols

--- engine/Shopware/Models/Media/Media.php
+++ engine/Shopware/Models/Media/Media.php
@@ -1249,7 +1249,7 @@ class Media extends ModelEntity
     private function removeSpecialCharacters($name)
     {
         $name = iconv('utf-8', 'ascii//translit', $name);
-        $name = preg_replace('#[^A-z0-9\-_]#', '-', $name);
+        $name = preg_replace('#[^A-Za-z0-9\-_]#', '-', $name);
         $name = preg_replace('#-{2,}#', '-', $name);
         $name = trim($name, '-');
 
