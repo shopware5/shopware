a2a70090b08aef98fb5a28c9a2d5f7c0a6f5d968
hanneswernery <hannes.wernery@viison.com>
2017-11-14 15:59:03 +0100

    Add safeguard to only write snippet entry to db if locale was found. Add test for Components/Snippet/DatabaseHandle::loadToDatabase with missing locale

---
+++ tests/Functional/Components/Snippet/DatabaseHandlerTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+/**
+ * @category  Shopware
+ *
+ * @copyright Copyright (c) shopware AG (http://www.shopware.com)
+ */
+class Shopware_Tests_Components_Snippet_DatabaseHandlerTest extends Enlight_Components_Test_TestCase
+{
+    /**
+     * Tests the import of snippets with missing locale.
+     * Asserts that all remaining snippets of a plugin are imported correctly even if some
+     * of the snippets are skipped because the respective locale is missing.
+     */
+    public function testLoadToDatabaseWithMissingLocale()
+    {
+        $em = Shopware()->Container()->get('models');
+
+        // Delete necessary locale
+        $germanLocale = $em->getRepository(\Shopware\Models\Shop\Locale::class)->findOneBy([
+            'locale' => 'de_DE',
+        ]);
+        $em->remove($germanLocale);
+        // Delete snippets of the AdvancedMenu plugin
+        $advancedMenuSnippets = $em->getRepository(\Shopware\Models\Snippet\Snippet::class)->findBy([
+            'namespace' => 'frontend/plugins/advanced_menu/advanced_menu',
+        ]);
+        foreach ($advancedMenuSnippets as $advancedMenuSnippet) {
+            $em->remove($advancedMenuSnippet);
+        }
+        $em->flush();
+
+        // No snippets are remaining
+        $advancedMenuSnippets = $em->getRepository(\Shopware\Models\Snippet\Snippet::class)->findBy([
+            'namespace' => 'frontend/plugins/advanced_menu/advanced_menu',
+        ]);
+        $this->assertCount(0, $advancedMenuSnippets);
+
+        // (partial) import snippets of the AdvancedMenu plugin
+        $namespace = Shopware()->Plugins()->Frontend();
+        $pluginBootstrap = $namespace->get('AdvancedMenu');
+        Shopware()->Container()->get('shopware.snippet_database_handler')
+            ->loadToDatabase($pluginBootstrap->Path() . 'Snippets/');
+
+        // Check that all snippets of the remaining locale are installed
+        $advancedMenuSnippets = $em->getRepository(\Shopware\Models\Snippet\Snippet::class)->findBy([
+            'namespace' => 'frontend/plugins/advanced_menu/advanced_menu',
+        ]);
+        $this->assertCount(3, $advancedMenuSnippets);
+
+        // Restore locale
+        $sql = "INSERT INTO s_core_locales values (1, 'de_DE', 'Deutsch', 'Deutschland');";
+        Shopware()->Db()->exec($sql);
+
+        // Import all remaining snippets
+        Shopware()->Container()->get('shopware.snippet_database_handler')
+            ->loadToDatabase($pluginBootstrap->Path() . 'Snippets/');
+    }
+}
