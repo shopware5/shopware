fd5252322d3ed03d14925ea31921fbe5e08acac0
Moritz Naczenski <mnaczenski@users.noreply.github.com>
2017-11-20 13:40:06 +0100

    Update prices.js

--- themes/Backend/ExtJs/backend/article/view/detail/prices.js
+++ themes/Backend/ExtJs/backend/article/view/detail/prices.js
@@ -254,6 +254,10 @@ Ext.define('Shopware.apps.Article.view.detail.Prices', {
                 xtype: 'numbercolumn',
                 header: me.snippets.grid.columns.price,
                 dataIndex: 'price',
+                renderer: function(val) {
+                    val = window.parseFloat(val);
+                    return Ext.Number.toFixed(val, window.parseInt('{s name=detail/price/precision}2{/s}'));
+                },
                 editor: {
                     xtype: 'numberfield',
                     decimalPrecision: '{s name=detail/price/precision}2{/s}',
