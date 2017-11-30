b61c5439fd983d2bd52f49606ece1c2665403644
Gugiman <Gugiman@users.noreply.github.com>
2017-11-30 13:43:55 +0100

    removal of unnecessary joins

--- engine/Shopware/Models/Document/Order.php
+++ engine/Shopware/Models/Document/Order.php
@@ -572,11 +572,10 @@ class Shopware_Models_Document_Order extends Enlight_Class implements Enlight_Ho
     {
         $this->_billing = new ArrayObject(Shopware()->Db()->fetchRow('
         SELECT sob.*,u.customernumber FROM s_order_billingaddress AS sob
-        LEFT JOIN s_user_addresses AS sub ON sub.id = ?
-        LEFT JOIN s_user u ON u.id = sub.user_id
+        LEFT JOIN s_user u ON u.id = sob.userID
         WHERE sob.userID = ? AND
         sob.orderID = ?
-        ', [$this->_user['default_billing_address_id'], $this->_userID, $this->_id]), ArrayObject::ARRAY_AS_PROPS);
+        ', [$this->_userID, $this->_id]), ArrayObject::ARRAY_AS_PROPS);
 
         $this->_billing['country'] = new ArrayObject(Shopware()->Db()->fetchRow('
         SELECT * FROM s_core_countries
