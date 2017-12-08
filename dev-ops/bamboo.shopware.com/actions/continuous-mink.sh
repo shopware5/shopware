1bf347940f803f7b474abd4c082b0e34d77fad25
Jan Bücker <j.buecker@shopware.com>
2017-12-08 15:34:17 +0100

    Simplify docker setup

--- dev-ops/bamboo.shopware.com/actions/continuous-mink.sh
+++ dev-ops/bamboo.shopware.com/actions/continuous-mink.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker exec __APP_ID__ /tmp/wait.sh
+docker exec -u __USERKEY__ __APP_ID__ /usr/local/bin/wait-for-it.sh --timeout=120 mysql:3306 -- echo "ready!"
 docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:init
 I: docker exec -u __USERKEY__ __APP_ID__ ./psh.phar bamboo:mink
-docker exec -u __USERKEY__ __APP_ID__ sudo chown -R app-shell:app-shell .
