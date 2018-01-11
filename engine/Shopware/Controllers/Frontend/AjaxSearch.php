2459fca4e18cc8b2a455eddc199083eea322a3b8
Moritz Naczenski <mnaczenski@users.noreply.github.com>
2018-01-11 09:30:58 +0100

    removed shopware() singleton

--- engine/Shopware/Controllers/Frontend/AjaxSearch.php
+++ engine/Shopware/Controllers/Frontend/AjaxSearch.php
@@ -61,11 +61,11 @@ class Shopware_Controllers_Frontend_AjaxSearch extends Enlight_Controller_Action
         $result = $this->get('shopware_search.product_search')->search($criteria, $context);
 
         //check if search-tearm is a valid product-number
-        if ((Shopware()->Config()->get('activateNumberSearch') == "1")) {
-        $directhit = Shopware()->Container()->get('shopware_storefront.list_product_service')
-            ->get($term, $context);
+        if (($this->get("config")->get('activateNumberSearch') == "1")) {
+            $directhit = $this->get('shopware_storefront.list_product_service')
+                ->get($term, $context);
         }
-        if ((Shopware()->Config()->get('activateNumberSearch') == "1") && $directhit) {
+        if (($this->get("config")->get('activateNumberSearch') == "1") && $directhit) {
             $result = new ProductSearchResult([$directhit], 1, []);
             $data = $this->get('legacy_struct_converter')->convertListProductStruct($directhit);
             //change name for compatibility reasons
@@ -77,6 +77,16 @@ class Shopware_Controllers_Frontend_AjaxSearch extends Enlight_Controller_Action
                 'sArticlesCount' => 1,
             ]);
             return;
+        }
+
+        if ($result->getTotalCount() > 0) {
+            $articles = $this->convertProducts($result);
+            $this->View()->assign('searchResult', $result);
+            $this->View()->assign('sSearchRequest', ['sSearch' => $term]);
+            $this->View()->assign('sSearchResults', [
+                'sResults' => $articles,
+                'sArticlesCount' => $result->getTotalCount(),
+            ]);
         }
         
         if ($result->getTotalCount() > 0) {
