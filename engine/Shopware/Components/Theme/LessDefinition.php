bd6291acddd2d6ac86f8043aa47e4ddc339763f5
Shyim <shyim@posteo.de>
2018-02-27 18:44:45 +0100

    Update LessDefinition.php

--- engine/Shopware/Components/Theme/LessDefinition.php
+++ engine/Shopware/Components/Theme/LessDefinition.php
@@ -108,7 +108,7 @@ class LessDefinition
      * @param array $config          contains the less variables, has to be a key value array
      * @param array $files           contains the full file name paths
      * @param null  $importDirectory Full path to the import directory for less @import commands
-     * @param Theme $theme           the corresponding theme
+     * @param Theme|null $theme           the corresponding theme
      */
     public function __construct(array $config = [], array $files = [], $importDirectory = null, Theme $theme = null)
     {
