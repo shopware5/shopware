56f83b18b05f3a448dc649913d9100a28716c19d
Soner Sayakci <shyim@posteo.de>
2018-04-12 19:14:51 +0200

    Extracted config to a new function

--- engine/Shopware/Plugins/Default/Backend/PluginManager/Views/backend/plugin_manager/view/plugin_helper.js
+++ engine/Shopware/Plugins/Default/Backend/PluginManager/Views/backend/plugin_manager/view/plugin_helper.js
@@ -328,30 +328,34 @@ Ext.define('Shopware.apps.PluginManager.view.PluginHelper', {
     },
 
     confirmMessage: function(title, message, callback, declineCallback) {
-        var me = this,
-            cfg = {
-                title: title,
-                icon: Ext.Msg.QUESTION,
-                msg: message,
-                buttons: Ext.Msg.YESNOCANCEL,
-                callback: function (apply) {
-                    if (apply === 'cancel') {
-                        return;
-                    }
-                    if (apply !== 'yes') {
+        var me = this;
+        var confirmationCallback = function (apply) {
+            if (apply === 'cancel') {
+                return;
+            }
+            if (apply !== 'yes') {
 
-                        me.hideLoadingMask();
-                        if (Ext.isFunction(declineCallback)) {
-                            declineCallback();
+                me.hideLoadingMask();
+                if (Ext.isFunction(declineCallback)) {
+                    declineCallback();
 
-                        }
-                        return;
-                    }
-                    callback();
                 }
-            };
+                return;
+            }
+            callback();
+        };
 
-        Ext.MessageBox.confirm(cfg);
+        Ext.MessageBox.confirm(me.getMessageBoxConfig(title, message, confirmationCallback));
+    },
+
+    getMessageBoxConfig: function(title, message, callback) {
+        return {
+            title: title,
+            icon: Ext.Msg.QUESTION,
+            msg: message,
+            buttons: Ext.Msg.YESNOCANCEL,
+            callback: callback
+        };
     },
 
     displayErrorMessage: function(response, callback) {
