fe9f3aadf23ae00123cf5fdc8fd318a07159b0f1
Soner Sayakci <s.sayakci@shopware.com>
2018-07-31 09:27:41 +0200

    NTR - Remove unneeded files

--- build/composer-post-install-cmd.sh
+++ build/composer-post-install-cmd.sh
@@ -8,3 +8,16 @@ root_dir="$script_dir"/..
 set -x
 
 rm -rf -- "$root_dir"/vendor/mpdf/mpdf/ttfonts
+rm -rf -- "$root_dir"/vendor/google/protobuf/src
+rm -rf -- "$root_dir"/vendor/google/protobuf/java
+rm -rf -- "$root_dir"/vendor/google/protobuf/objectivec
+rm -rf -- "$root_dir"/vendor/google/protobuf/csharp
+rm -rf -- "$root_dir"/vendor/google/protobuf/python
+rm -rf -- "$root_dir"/vendor/google/protobuf/ruby
+rm -rf -- "$root_dir"/vendor/google/protobuf/js
+rm -rf -- "$root_dir"/vendor/google/protobuf/javanano
+rm -rf -- "$root_dir"/vendor/google/protobuf/php/ext
+rm -rf -- "$root_dir"/vendor/google/protobuf/php/tests
+rm -rf -- "$root_dir"/vendor/google/cloud/tests
+rm -rf -- "$root_dir"/vendor/google/cloud/dev
+rm -rf -- "$root_dir"/vendor/google/cloud/docs
