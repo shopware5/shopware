38219a7f0efa26858c68a130938917ea2b986d40
Moritz Naczenski <mnaczenski@users.noreply.github.com>
2018-08-16 15:38:55 +0200

    SW-22339 - Add forwarding to manufacturer listing for old supplier urls

--- engine/Shopware/Plugins/Default/Core/ViewportForward/Bootstrap.php
+++ engine/Shopware/Plugins/Default/Core/ViewportForward/Bootstrap.php
@@ -61,8 +61,10 @@ class Shopware_Plugins_Core_ViewportForward_Bootstrap extends Shopware_Component
                 }
                 break;
             case 'cat':
-            case 'supplier':
                 $request->setControllerName('listing')->setDispatched(false);
+                break;
+            case 'supplier':
+                $request->setControllerName('listing')->setActionName('manufacturer')->setDispatched(false);
                 break;
             case 'captcha':
                 $request->setModuleName('widgets')->setControllerName('captcha')->setDispatched(false);
