3873b16b219d5f5eaa1af4e54bc9a434440a9d47
Soner Sayakci <s.sayakci@shopware.com>
2018-08-29 15:58:49 +0200

    NTR - Enable code style check in CI

--- dev-ops/common/actions/unit-fast.sh
+++ dev-ops/common/actions/unit-fast.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 #DESCRIPTION: execute unit tests without coverage
 
+php vendor/bin/php-cs-fixer fix --dry-run --stop-on-violation --verbose --show-progress=dots
+
 php vendor/bin/phpunit -c tests/phpunit.xml.dist --stop-on-failure --stop-on-error
