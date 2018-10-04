efda74c4c558718428c2562972b20899941c14ba
Hendrik Söbbing <h.soebbing@shopware.com>
2018-10-04 15:56:01 +0200

    SW-22525 - Improve codestyle

--- _sql/migrations/705-rename-category-template-column.php
+++ _sql/migrations/705-rename-category-template-column.php
@@ -1,5 +1,26 @@
 <?php
-
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 class Migrations_Migration705 extends Shopware\Components\Migrations\AbstractMigration
 {
     public function up($modus)
@@ -26,7 +47,7 @@ class Migrations_Migration705 extends Shopware\Components\Migrations\AbstractMig
             'article_listing_1col.tpl',
             'article_listing_2col.tpl',
             'article_listing_3col.tpl',
-            'article_listing_4col.tpl'
+            'article_listing_4col.tpl',
         ];
         $templates = $this->connection->query("SELECT vals.id, vals.value FROM `s_core_config_values` as vals INNER JOIN `s_core_config_elements` as elems ON elems.id = vals.element_id WHERE elems.name = 'categorytemplates'")
             ->fetchAll(\PDO::FETCH_KEY_PAIR);
@@ -35,32 +56,32 @@ class Migrations_Migration705 extends Shopware\Components\Migrations\AbstractMig
             $cleanedTemplates = [];
             $templates = unserialize($serializedValue);
 
-            foreach (explode(";", $templates) as $template) {
-                if (strpos($template, ":") === false) {
+            foreach (explode(';', $templates) as $template) {
+                if (strpos($template, ':') === false) {
                     continue;
                 }
-                
-                list($file, $name) = explode(":", $template);
+
+                list($file, $name) = explode(':', $template);
 
                 if (in_array($file, $templateBlacklist) || empty($name)) {
                     continue;
                 }
 
-                $cleanedTemplates[] = $file . ":" . $name;
+                $cleanedTemplates[] = $file . ':' . $name;
             }
 
             if (empty($cleanedTemplates)) {
-                $this->connection->exec("DELETE FROM `s_core_config_values` WHERE id = " . $valueId);
+                $this->connection->exec('DELETE FROM `s_core_config_values` WHERE id = ' . $valueId);
             } else {
-                $this->connection->prepare("UPDATE `s_core_config_values` SET `value` = :newTemplates WHERE id = :valueId")
+                $this->connection->prepare('UPDATE `s_core_config_values` SET `value` = :newTemplates WHERE id = :valueId')
                     ->execute([
-                        ':newTemplates' => serialize(join(";", $cleanedTemplates)),
-                        ':valueId' => $valueId
+                        ':newTemplates' => serialize(implode(';', $cleanedTemplates)),
+                        ':valueId' => $valueId,
                     ]);
             }
         }
 
-        $this->addSql("UPDATE `s_categories` SET template = NULL WHERE template IN ('" . join("','", $templateBlacklist) . "')");
+        $this->addSql("UPDATE `s_categories` SET template = NULL WHERE template IN ('" . implode("','", $templateBlacklist) . "')");
     }
 
     private function emptyCustomTemplates()
