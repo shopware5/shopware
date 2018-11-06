f690b67c68000435a0b8742e73a30bcbbd464d55
Johannes Vallo <j.vallo@shopware.com>
2018-11-06 17:13:32 +0100

    SW-22900 - Changed legacy groupKey for custom pages to new groupKey

--- engine/Shopware/Controllers/Widgets/Listing.php
+++ engine/Shopware/Controllers/Widgets/Listing.php
@@ -230,7 +230,7 @@ class Shopware_Controllers_Widgets_Listing extends Enlight_Controller_Action
     public function getCustomPageAction()
     {
         $pageId = (int) $this->Request()->getParam('pageId', 0);
-        $groupKey = $this->Request()->getParam('groupKey', 'gLeft');
+        $groupKey = $this->Request()->getParam('groupKey', 'left');
 
         $customPage = Shopware()->Modules()->Cms()->sGetStaticPageChildrensById($pageId, $groupKey);
 
