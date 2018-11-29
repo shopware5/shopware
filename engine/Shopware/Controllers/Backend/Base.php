74216c4a3817a905ac794e43fedb2aa811e0600c
Soner Sayakci <s.sayakci@shopware.com>
2018-11-29 15:30:41 +0100

    SW-22151 - Remove reference

--- engine/Shopware/Controllers/Backend/Base.php
+++ engine/Shopware/Controllers/Backend/Base.php
@@ -1100,9 +1100,9 @@ class Shopware_Controllers_Backend_Base extends Shopware_Controllers_Backend_Ext
         $tmpVariant = [];
 
         // Checks if an additional text is available
-        foreach ($data as &$variantData) {
+        foreach ($data as $key => $variantData) {
             if (!empty($variantData['additionalText'])) {
-                $variantData['name'] = $variantData['name'] . ' ' . $variantData['additionalText'];
+                $data[$key]['name'] = $variantData['name'] . ' ' . $variantData['additionalText'];
             } else {
                 $variantIds[$variantData['id']] = $variantData['id'];
             }
