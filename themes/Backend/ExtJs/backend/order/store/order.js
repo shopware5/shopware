4511519e06a48f2a90fac714c1ce47296b90f564
Soner Sayakci <s.sayakci@shopware.com>
2019-01-24 08:11:52 +0100

    SW-23365 - Add default sorting for order module in elastic search

--- themes/Backend/ExtJs/backend/order/store/order.js
+++ themes/Backend/ExtJs/backend/order/store/order.js
@@ -39,36 +39,53 @@ Ext.define('Shopware.apps.Order.store.Order', {
      * @string
      */
     extend:'Ext.data.Store',
+
     /**
      * Auto load the store after the component is initialized
      * @boolean
      */
     autoLoad:false,
+
     /**
      * Enable remote sort.
      * @boolean
      */
     remoteSort:true,
+
     /**
      * Enable remote filtering
      * @boolean
      */
     remoteFilter:true,
+
     /**
      * Amount of data loaded at once
      * @integer
      */
     pageSize:20,
+
     /**
      * to upload all selected items in one request
      * @boolean
      */
     batch:true,
+
     /**
      * Define the used model for this store
      * @string
      */
-    model:'Shopware.apps.Order.model.Order'
+    model:'Shopware.apps.Order.model.Order',
+
+    /**
+     * Default sorters
+     * @array
+     */
+    sorters: [
+        {
+            property: 'orderTime',
+            direction: 'DESC'
+        }
+    ]
 });
 //{/block}
 
