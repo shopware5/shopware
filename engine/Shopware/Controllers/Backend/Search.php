e322e3750145b6a70a4ce244fb99c339ffa1d9b0
Pascal Thesing <pascal@thesing.me>
2019-03-27 16:02:42 +0100

    SW-23800 - show variants in backend

--- engine/Shopware/Controllers/Backend/Search.php
+++ engine/Shopware/Controllers/Backend/Search.php
@@ -108,6 +108,8 @@ class Shopware_Controllers_Backend_Search extends Shopware_Controllers_Backend_E
     /**
      * Queries the articles from the database based on the passed search term
      *
+     * @deprecated since version 5.5.8, to be removed in 5.7, use the ProductRepository instead
+     *
      * @param string $search
      *
      * @return array
@@ -150,6 +152,8 @@ class Shopware_Controllers_Backend_Search extends Shopware_Controllers_Backend_E
     /**
      * Queries the customers from the database based on the passed search term
      *
+     * @deprecated since version 5.5.8, to be removed in 5.7, use the CustomerRepository instead
+     *
      * @param string $search
      *
      * @return array
@@ -184,6 +188,8 @@ class Shopware_Controllers_Backend_Search extends Shopware_Controllers_Backend_E
 
     /**
      * Queries the orders from the database based on the passed search term
+     *
+     * @deprecated since version 5.5.8, to be removed in 5.7, use the OrderRepository instead
      *
      * @param string $search
      *
