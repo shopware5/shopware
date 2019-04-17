26ff810286b1e5d55b6f212d2b8fdd25e8cdd90b
Pascal Thesing <pascal@thesing.me>
2019-04-17 10:18:26 +0200

    NTR - add es lint for backend

--- themes/.eslintrc.js
+++ themes/.eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-    'extends': 'standard',
     'root': true,
     'env': {
         'browser': true,
@@ -15,33 +14,4 @@ module.exports = {
         'Shopware': true,
         'Ext': true
     },
-    'rules': {
-        'arrow-parens': 0,
-        'space-before-function-paren': 0,
-        'keyword-spacing': [
-            'warn'
-        ],
-        'padded-blocks': [
-            'warn'
-        ],
-        'space-in-parens': [
-            'warn'
-        ],
-        'generator-star-spacing': 0,
-        'no-shadow-restricted-names': 0,
-        'eqeqeq': 0,
-        'no-debugger': 0,
-        'semi': [
-            'error',
-            'always'
-        ],
-        'one-var': 0,
-        'indent': [
-            'error',
-            4,
-            { 'SwitchCase': 1 }
-        ],
-
-        'standard/no-callback-literal': 0
-    }
 };
