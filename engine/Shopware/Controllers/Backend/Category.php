2d46c00d658c75612c2d804297aeb8b0e5c7e2c6
Bastian Neumann <b.neumann@shopware.com>
2019-05-17 11:49:47 +0200

    SW-22983 - Fix category teaser

--- engine/Shopware/Controllers/Backend/Category.php
+++ engine/Shopware/Controllers/Backend/Category.php
@@ -191,7 +191,7 @@ class Shopware_Controllers_Backend_Category extends Shopware_Controllers_Backend
             }
         } else {
             $query = $this->Request()->getParam('query');
-            $parents = (bool) $this->Request()->getParam('parents', false);
+            $parents = (bool) $this->Request()->getParam('parents', true);
             $result = $this->getPathByQuery($query, $separator, $parents);
         }
 
