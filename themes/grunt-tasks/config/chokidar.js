8ba8cd06edc091ee08f3d082ff659cdf3acbc7fe
Julian Krzefski <krzefski@heptacom.de>
2019-06-03 16:07:34 +0200

    SW-24218 - Adds project plugins into grunt configuration fixes #2129

--- themes/grunt-tasks/config/chokidar.js
+++ themes/grunt-tasks/config/chokidar.js
@@ -6,7 +6,9 @@ module.exports = {
             '../themes/Frontend/**/*.less',
             '../themes/Frontend/**/*.css',
             '../custom/plugins/**/*.less',
-            '../custom/plugins/**/*.css'
+            '../custom/plugins/**/*.css',
+            '../custom/project/**/*.less',
+            '../custom/project/**/*.css'
         ],
         tasks: ['less:development'],
         options: {
@@ -19,7 +21,9 @@ module.exports = {
             '../themes/Frontend/**/_public/src/js/*.js',
             '../engine/Shopware/Plugins/**/frontend/**/src/js/**/*.js',
             '../custom/plugins/**/frontend/**/src/js/**/*.js',
-            '../custom/plugins/**/frontend/js/**/*.js'
+            '../custom/plugins/**/frontend/js/**/*.js',
+            '../custom/project/**/frontend/**/src/js/**/*.js',
+            '../custom/project/**/frontend/js/**/*.js'
         ],
         tasks: ['uglify:development'],
         options: {
