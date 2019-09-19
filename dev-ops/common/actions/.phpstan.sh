5e5b74f7fba75cd090b8246d90bede4bfaf286d5
Hendrik Söbbing <h.soebbing@shopware.com>
2019-09-19 10:20:48 +0200

    NTR - Update PHPStan to 0.11.16

--- dev-ops/common/actions/.phpstan.sh
+++ dev-ops/common/actions/.phpstan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -e phpstan.phar ]; then
-    curl -s -L "https://github.com/phpstan/phpstan/releases/download/0.11.15/phpstan.phar" > phpstan.phar
+    curl -s -L "https://github.com/phpstan/phpstan/releases/download/0.11.16/phpstan.phar" > phpstan.phar
     chmod +x phpstan.phar
 fi
 
