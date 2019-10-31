33e71e693766fa27c30208f43dabfa1abd1dc10b
erolschel <kontakt@erolschel.de>
2019-10-31 15:07:36 +0100

    added ignoredExceptionClasses also to testkernel config due to fatal through non-existent parameter

--- tests/Functional/config.php
+++ tests/Functional/config.php
@@ -30,6 +30,17 @@ return array_replace_recursive($this->loadConfig($this->AppPath() . 'Configs/Def
     ],
     'errorhandler' => [
         'throwOnRecoverableError' => true,
+        'ignoredExceptionClasses' => [
+            // Disable logging for defined exceptions by class, eg. to disable any logging for CSRF exceptions add this:
+            // \Shopware\Components\CSRFTokenValidationException::class
+            \Shopware\Components\Api\Exception\BatchInterfaceNotImplementedException::class,
+            \Shopware\Components\Api\Exception\CustomValidationException::class,
+            \Shopware\Components\Api\Exception\NotFoundException::class,
+            \Shopware\Components\Api\Exception\OrmException::class,
+            \Shopware\Components\Api\Exception\ParameterMissingException::class,
+            \Shopware\Components\Api\Exception\PrivilegeException::class,
+            \Shopware\Components\Api\Exception\ValidationException::class,
+        ],
     ],
     'session' => [
         'unitTestEnabled' => true,
