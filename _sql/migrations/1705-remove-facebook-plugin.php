da2344b10a53027115501fc4c87f28b521646560
Joshua Behrens <behrens@heptacom.de>
2019-11-11 04:04:44 +0100

    SW-24863 - Remove plugin like migration 630 fixes #2252

---
+++ _sql/migrations/1705-remove-facebook-plugin.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Shopware 5
+ * Copyright (c) shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+class Migrations_Migration1705 extends Shopware\Components\Migrations\AbstractMigration
+{
+    public function up($modus)
+    {
+        $sql = <<<'EOD'
+SET @pluginId = (SELECT id FROM s_core_plugins WHERE name = 'Facebook' and namespace = 'Frontend' and `source` = 'Default' LIMIT 1);
+SET @formId = (SELECT id FROM s_core_config_forms WHERE plugin_id = @pluginId);
+
+DELETE FROM s_core_config_elements WHERE form_id = @formId;
+DELETE FROM s_core_config_forms WHERE id = @formId;
+DELETE FROM s_core_plugins WHERE id = @pluginId'
+EOD;
+        $this->addSql($sql);
+    }
+}
