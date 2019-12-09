0fe975b5a6754e1eeab7fc6fbf7148bd8f3b5bf3
pascal.guhl <pascal.guhl@datenwerk.de>
2019-12-09 15:19:18 +0100

    fix export bug - all items available at getRating

--- engine/Shopware/Controllers/Backend/Analytics.php
+++ engine/Shopware/Controllers/Backend/Analytics.php
@@ -276,11 +276,17 @@ class Shopware_Controllers_Backend_Analytics extends Shopware_Controllers_Backen
             }
         }
 
+        // Sets the correct limit
+        $limit = 25;
+        if (strtolower($this->format) === 'csv') {
+            $limit = count($data);
+        }
+
         $values = array_values($data);
         $splice = array_splice(
             $values,
             (int) $this->Request()->getParam('start', 0),
-            (int) $this->Request()->getParam('limit', 25)
+            $limit
         );
 
         $this->send($splice, count($data));
