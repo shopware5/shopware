51b0b09e606c25577a85f4d388b680d841edb375
Felix Brucker <felix@felixbrucker.com>
2020-11-19 13:32:07 +0100

    SW-26032 - Fix session id when switching between sessions fixes #2400

--- engine/Library/Enlight/Components/Session/Namespace.php
+++ engine/Library/Enlight/Components/Session/Namespace.php
@@ -140,4 +140,58 @@ class Enlight_Components_Session_Namespace extends Session implements ArrayAcces
         parent::clear();
         $this->set('sessionId', $this->getId());
     }
+
+    public function start()
+    {
+        // Generally `start()` is called by Shopware's session factories
+        // - Shopware_Plugins_Backend_Auth_Bootstrap::onInitResourceBackendSession()
+        // - Shopware\Components\DependencyInjection\Bridge\Session::createSession()
+        // In case the session is started somewhere else we need to ensure no other session is active. The same logic as
+        // in Shopware's factories is used here.
+        // This behaviour is analogue to Zend: https://github.com/shopware/shopware/blob/cbc212ca4642878cac62193d3a2f41e08f4849a2/engine/Library/Zend/Session.php#L413
+        $container = Shopware()->Container();
+        self::ensureFrontendSessionClosed($container);
+        self::ensureBackendSessionClosed($container);
+
+        // When the session cookie is present but no session id is currently set, set its value as session id as
+        // otherwise a new session id is generated when a session was active before. This is required to resume a
+        // previously active session after switching back from another session. This behaviour is analogue to Zend:
+        // https://github.com/shopware/shopware/blob/cbc212ca4642878cac62193d3a2f41e08f4849a2/engine/Library/Zend/Session.php#L421-L423
+        // The session id can be empty on first start or when it has been set to an empty string, for example by
+        // `ensureSessionClosed()`.
+        if (!$this->getId() && ini_get('session.use_cookies') === '1' && !empty($_COOKIE[$this->getName()])) {
+            $this->setId($_COOKIE[$this->getName()]);
+        }
+
+        return parent::start();
+    }
+
+    public static function ensureFrontendSessionClosed($container)
+    {
+        self::ensureSessionClosed($container, 'session');
+    }
+
+    public static function ensureBackendSessionClosed($container)
+    {
+        self::ensureSessionClosed($container, 'backendsession');
+    }
+
+    /**
+     * Saves the session and ensures it can be reused.
+     *
+     * This method can be used to ensure other sessions are closed before starting a new session, because the other
+     * sessions would use the session id of the new session and thus write their data into the wrong session.
+     *
+     * @param $container
+     * @param $sessionServiceName
+     */
+    private static function ensureSessionClosed($container, $sessionServiceName)
+    {
+        $session = $container->initialized($sessionServiceName) ? $container->get($sessionServiceName) : null;
+        if ($session && $session->isStarted()) {
+            $session->save();
+            // The empty session id signals that upon starting a session the session cookie is used.
+            $session->setId('');
+        }
+    }
 }
