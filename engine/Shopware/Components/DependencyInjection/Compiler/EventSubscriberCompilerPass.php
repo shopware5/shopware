ed18cb5f4b94e6143b8b2ecadb47d7392e83a542
Soner Sayakci <s.sayakci@shopware.com>
2020-12-14 11:27:42 +0100

    NTR - Set event listener, event subscriber always as public

--- engine/Shopware/Components/DependencyInjection/Compiler/EventSubscriberCompilerPass.php
+++ engine/Shopware/Components/DependencyInjection/Compiler/EventSubscriberCompilerPass.php
@@ -45,12 +45,6 @@ class EventSubscriberCompilerPass implements CompilerPassInterface
 
         foreach ($taggedServices as $id => $tagAttributes) {
             $def = $container->getDefinition($id);
-
-            if (!$def->isPublic()) {
-                throw new \InvalidArgumentException(sprintf('The service "%s" must be public as event subscribers are lazy-loaded.', $id));
-            }
-
-            // isPublic default value is true when no public is specified. This should be fixed in Symfony
             $def->setPublic(true);
 
             if ($def->isAbstract()) {
