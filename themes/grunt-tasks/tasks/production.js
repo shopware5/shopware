29cef7f63a58f56edfbb0a0475eea51a01a45d3a
Soner Sayakci <s.sayakci@shopware.com>
2021-05-10 11:33:06 +0000

    NTR - Update npm dependencies

--- themes/grunt-tasks/tasks/production.js
+++ themes/grunt-tasks/tasks/production.js
@@ -1,3 +1,3 @@
 module.exports = (grunt) => {
-    grunt.registerTask('production', ['eslint', 'less:production', 'uglify:production']);
+    grunt.registerTask('production', ['less:production', 'uglify:production']);
 };
