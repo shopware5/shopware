19214ffe3ba617aeb5a68aa92bd3b435316d0f9f
Daniel Schumacher <info@favor-it.net>
2021-06-16 16:26:55 +0200

    Revert "SW-24297 - Use mousemove listener only then needed and remove afterwards"
    This reverts commit 06eb4d0b23548848da0b1ed011aee1790ab67408.

--- engine/Shopware/Plugins/Default/Frontend/AdvancedMenu/Views/frontend/_public/src/js/jquery.advanced-menu.js
+++ engine/Shopware/Plugins/Default/Frontend/AdvancedMenu/Views/frontend/_public/src/js/jquery.advanced-menu.js
@@ -157,6 +157,8 @@
                 me._on($el, 'click', $.proxy(me.onClick, me, i, $el));
             });
 
+            $body.on('mousemove touchstart', $.proxy(me.onMouseMove, me));
+
             me._on(me._$closeButton, 'click', $.proxy(me.onCloseButtonClick, me));
         },
 
@@ -312,8 +314,6 @@
         openMenu: function () {
             var me = this;
 
-            $body.on('mousemove touchstart', $.proxy(me.onMouseMove, me));
-
             me.$el.show();
 
             $.publish('plugin/swAdvancedMenu/onOpenMenu', [ me ]);
@@ -332,8 +332,6 @@
             me._$list.find('.' + opts.itemHoverClass).removeClass(opts.itemHoverClass);
 
             me.$el.hide();
-
-            $body.off('mousemove touchstart', $.proxy(me.onMouseMove, me));
 
             me._targetIndex = -1;
 
