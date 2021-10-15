de49e68c5dc5dd5b118ac18733c4d1f8e6ff3a4c
Tobias Bachert <tobiasbachert@horn-gmbh.com>
2021-10-15 11:46:51 +0200

    Remove customer id limitation from shipping address

--- engine/Shopware/Core/sAdmin.php
+++ engine/Shopware/Core/sAdmin.php
@@ -4335,10 +4335,6 @@ SQL;
             return null;
         }
 
-        if ($shippingAddress->getCustomer()->getId() !== $customer->getId()) {
-            throw new \UnexpectedValueException('Address did not match the user');
-        }
-
         $shippingAddressArray = $this->convertToLegacyAddressArray($shippingAddress);
 
         $shippingAddressArray['attributes'] = $this->attributeLoader->load(
