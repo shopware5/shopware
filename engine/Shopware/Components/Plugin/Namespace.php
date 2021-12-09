a90046c765c57a46c4399dce17bd174253c32886
Michael Telgmann <mt@shopware.com>
2021-12-09 09:04:16 +0100

    SW-26435 - Improve routing

--- engine/Shopware/Components/Plugin/Namespace.php
+++ engine/Shopware/Components/Plugin/Namespace.php
@@ -39,6 +39,7 @@ use Shopware\Models\Widget\Widget;
  * @method Shopware_Plugins_Core_HttpCache_Bootstrap          HttpCache()
  * @method Shopware_Plugins_Core_MarketingAggregate_Bootstrap MarketingAggregate()
  * @method Shopware_Plugins_Core_PostFilter_Bootstrap         PostFilter()
+ * @method Shopware_Plugins_Core_Router_Bootstrap             Router()
  * @method Shopware_Plugins_Frontend_Statistics_Bootstrap     Statistics()
  * @method Shopware_Plugins_Backend_SwagUpdate_Bootstrap      SwagUpdate()
  * @method Shopware_Plugins_Frontend_TagCloud_Bootstrap       TagCloud()
