1f63ae46edc0f78508eeb92ab24d94fb1a85a222
Dominik Rathmer <d.rathmer@shopware.com>
2022-03-14 13:57:18 +0100

    SW-21011 - Fixed secure flag not set

--- themes/Frontend/Responsive/frontend/_public/src/js/jquery.csrf-protection.js
+++ themes/Frontend/Responsive/frontend/_public/src/js/jquery.csrf-protection.js
@@ -26,7 +26,7 @@
      */
     $.removeCookie = function(name) {
         var basePath = window.csrfConfig.basePath || '/';
-        document.cookie = name + '=; path=' + basePath + '; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        document.cookie = name + '=; path=' + basePath + '; expires=Thu, 01 Jan 1970 00:00:01 GMT;' + ($.isSecure() ? 'secure;' : '');
     };
 
     var CSRF = {
